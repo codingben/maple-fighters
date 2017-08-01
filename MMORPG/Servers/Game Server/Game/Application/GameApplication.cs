05907935bbdd282dc762c50bdf9c9650c822b315
Ben Ukhanov <ben1zuk321@gmail.com>
2017-08-02 00:41:57 +0300

    Created game application and peer logic

---
+++ MMORPG/Servers/Game Server/Game/Application/GameApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Photon.SocketServer;
+
+namespace Game.Application
+{
+    internal class GameApplication : ApplicationBase
+    {
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new PeerLogic.PeerLogic(initRequest);
+        }
+
+        protected override void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void TearDown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MMORPG/Servers/Game Server/Game/Application/PeerLogic/PeerLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace Game.Application.PeerLogic
+{
+    internal class PeerLogic : ClientPeer
+    {
+        public PeerLogic(InitRequest initRequest)
+            : base(initRequest)
+        {
+
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
