efe20a3665ca0cf257509261325d99ba33a1e9ab
Ben Ukhanov <ben1zuk321@gmail.com>
2017-08-06 19:11:22 +0300

    Created a GameConfiguration and ServiceBase

---
+++ MMORPG/Assets/MMORPG/Scripts/GameConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using ExitGames.Client.Photon;
+using UnityEngine;
+
+namespace Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "GameConfiguration", menuName = "Scriptable Objects/Game Configuration", order = 1)]
+    public class GameConfiguration : ScriptableObject
+    {
+        public ConnectionProtocol ConnectionProtocol = ConnectionProtocol.Udp;
+    }
+}
