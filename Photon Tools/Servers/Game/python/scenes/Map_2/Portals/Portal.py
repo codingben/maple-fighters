c42f3f2e85a0a8363bc0b702e4cbfa866c2284e4
Ben Ukhanov <ben1zuk321@gmail.com>
2017-10-28 15:35:22 +0200

    A changed python script.

--- Photon Tools/Servers/Game/python/scenes/Map_2/Portals/Portal.py
+++ Photon Tools/Servers/Game/python/scenes/Map_2/Portals/Portal.py
@@ -2,7 +2,19 @@ from MathematicsHelper import *
 from Game.InterestManagement import *
 from Game.Application.SceneObjects import *
 
-position = Vector2(12.5, -1.125)
+import os
+import json
+
+path = os.path.abspath(__file__)
+jsonPath = os.path.splitext(path)[0] + ".json"
+
+with open(jsonPath) as data_file:
+    data = json.load(data_file)
+
+x = data["Position"]["x"]
+y = data["Position"]["y"]
+
+position = Vector2(x, y)
 playerPosition = Vector2(18, -1.75)
 map = 2
 
