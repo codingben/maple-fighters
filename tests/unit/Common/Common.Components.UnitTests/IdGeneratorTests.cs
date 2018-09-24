249d9a5555ecede44fb34e9fc0bb82c39c96aca7
Ben Ukhanov <benzuk@users.noreply.github.com>
2018-09-24 16:13:11 +0300

    Added unit tests for the common components.

---
+++ tests/unit/Common/Common.Components.UnitTests/IdGeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using Common.ComponentModel;
+using Common.UnitTestsBase;
+using Shouldly;
+using Xunit;
+
+namespace Common.Components.UnitTests
+{
+    public class IdGeneratorTests
+    {
+        private readonly IComponentsProvider componentsProvider;
+
+        public IdGeneratorTests()
+        {
+            componentsProvider = new ComponentsProvider();
+            componentsProvider.Add(new IdGenerator());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        private void GenerateId(int id)
+        {
+            // Arrange
+            var idGenerator = componentsProvider.Get<IIdGenerator>().AssertNotNull();
+
+            // Act
+            var generateId = idGenerator.GenerateId();
+
+            // Assert
+            id.Equals(generateId).ShouldBeTrue();
+        }
+    }
+}
---
+++ tests/unit/Common/Common.Components.UnitTests/RandomNumberGeneratorTests.cs
@@ -0,0 +1,45 @@
+﻿using Common.ComponentModel;
+using Shouldly;
+using Xunit;
+
+namespace Common.Components.UnitTests
+{
+    public class RandomNumberGeneratorTests
+    {
+        private readonly IComponentsProvider componentsProvider;
+
+        public RandomNumberGeneratorTests()
+        {
+            componentsProvider = new ComponentsProvider();
+            componentsProvider.Add(new RandomNumberGenerator());
+        }
+
+        [Fact]
+        private void GenerateRandomNumber()
+        {
+            // Arrange
+            var randomNumberGenerator = componentsProvider.Get<IRandomNumberGenerator>();
+
+            // Act
+            var number = randomNumberGenerator.GenerateRandomNumber();
+
+            // Assert
+            number.Equals(0).ShouldBeFalse();
+        }
+
+        [Theory]
+        [InlineData(100, 200)]
+        [InlineData(1000, 2000)]
+        private void GenerateRandomNumber_WithParameters(int min, int max)
+        {
+            // Arrange
+            var randomNumberGenerator = componentsProvider.Get<IRandomNumberGenerator>();
+
+            // Act
+            var number = randomNumberGenerator.GenerateRandomNumber(min, max);
+
+            // Assert
+            Assert.InRange(number, min, max);
+        }
+    }
+}
