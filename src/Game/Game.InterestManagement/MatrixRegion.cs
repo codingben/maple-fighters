1dad8cc31b7fd5187d28e238a303c626b07731b2
Ben Ukhanov <ben.ukhanov@gmail.com>
2018-11-15 23:18:24 +0200

    Code refactoring.

--- src/Game/Game.InterestManagement/MatrixRegion.cs
+++ src/Game/Game.InterestManagement/MatrixRegion.cs
@@ -7,46 +7,37 @@ namespace Game.InterestManagement
 {
     public class MatrixRegion : IMatrixRegion
     {
-        public Vector2 SceneSize { get; }
-
-        public Vector2 RegionSize { get; }
-
+        private readonly Vector2 sceneSize;
+        private readonly Vector2 regionSize;
         private readonly int rows;
         private readonly int columns;
         private readonly IRegion[,] regions;
 
         public MatrixRegion(Vector2 sceneSize, Vector2 regionSize)
         {
-            if (sceneSize.Equals(Vector2.Zero)
-                || regionSize.Equals(Vector2.Zero))
-            {
-                throw new InvalidMatrixRegionSize();
-            }
+            this.sceneSize = sceneSize;
+            this.regionSize = regionSize;
 
-            SceneSize = sceneSize;
-            RegionSize = regionSize;
+            rows = (int)(sceneSize.X / regionSize.X);
+            columns = (int)(sceneSize.Y / regionSize.Y);
 
-            rows = (int)(SceneSize.X / SceneSize.X);
-            columns = (int)(RegionSize.Y / RegionSize.Y);
-            regions = new IRegion[rows, columns];
-
-            InitializeRegions();
-        }
-
-        private void InitializeRegions()
-        {
-            var x = -(SceneSize.X / 2) + (RegionSize.X / 2);
-            var y = -(SceneSize.Y / 2) + (RegionSize.Y / 2);
+            var x1 = -(sceneSize.X / 2) + (regionSize.X / 2);
+            var y1 = -(sceneSize.Y / 2) + (regionSize.Y / 2);
 
             for (var row = 0; row < rows; row++)
             {
                 for (var column = 0; column < columns; column++)
                 {
-                    var position = new Vector2(
-                        x: x + (row * RegionSize.X),
-                        y: y + (column * RegionSize.Y));
+                    if (regions == null)
+                    {
+                        regions = new IRegion[rows, columns];
+                    }
 
-                    regions[row, column] = new Region(position, RegionSize);
+                    var x2 = x1 + (row * regionSize.X);
+                    var y2 = y1 + (column * regionSize.Y);
+                    var position = new Vector2(x2, y2);
+
+                    regions[row, column] = new Region(position, regionSize);
                 }
             }
         }
@@ -72,11 +63,11 @@ namespace Game.InterestManagement
 
             foreach (var position in positions)
             {
-                var x = Math.Abs(position.X - (-(SceneSize.X / 2)));
-                var y = Math.Abs(position.Y - (-(SceneSize.Y / 2)));
+                var x = Math.Abs(position.X - (-(sceneSize.X / 2)));
+                var y = Math.Abs(position.Y - (-(sceneSize.Y / 2)));
 
-                var row = (int)Math.Floor(x / RegionSize.X);
-                var column = (int)Math.Floor(y / RegionSize.Y);
+                var row = (int)Math.Floor(x / regionSize.X);
+                var column = (int)Math.Floor(y / regionSize.Y);
 
                 // Make sure it is not out of bounds.
                 if ((row < 0 || row >= rows)
