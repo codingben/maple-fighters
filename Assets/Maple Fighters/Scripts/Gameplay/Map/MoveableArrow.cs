d008ad3b941b50c2e38cb49fd9c1152cc9e91663
Ben Ukhanov <ben1zuk321@gmail.com>
2018-12-23 21:43:37 +0200

    :alien: Code refactoring.

--- Assets/Maple Fighters/Scripts/Gameplay/Map/MoveableArrow.cs
+++ Assets/Maple Fighters/Scripts/Gameplay/Map/MoveableArrow.cs
@@ -7,17 +7,21 @@ namespace Scripts.Gameplay.Map
 
     public class MoveableArrow : MonoBehaviour
     {
-        private const string MINI_CAMERA_TAG = "Minimap Camera";
+        private const string MiniCameraTag = "Minimap Camera";
 
-        [SerializeField] private float moveTime;
-        [SerializeField] private float moveSpeed;
+        [SerializeField]
+        private float moveTime;
+
+        [SerializeField]
+        private float moveSpeed;
 
         private Camera minimapCamera;
         private SpriteRenderer spriteRenderer;
 
         private void Awake()
         {
-            minimapCamera = GameObject.FindGameObjectWithTag(MINI_CAMERA_TAG).GetComponent<Camera>();
+            minimapCamera = GameObject.FindGameObjectWithTag(MiniCameraTag)
+                ?.GetComponent<Camera>();
             spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
@@ -28,42 +32,49 @@ namespace Scripts.Gameplay.Map
 
         private void Update()
         {
-            spriteRenderer.enabled = Utils.IsInLayerMask(gameObject.layer, minimapCamera.cullingMask);
+            spriteRenderer.enabled = Utils.IsInLayerMask(
+                gameObject.layer,
+                minimapCamera.cullingMask);
         }
 
         private IEnumerator MoveableArrowCoroutine()
         {
             while (true)
             {
-                while (Utils.IsInLayerMask(gameObject.layer, minimapCamera.cullingMask))
+                while (Utils.IsInLayerMask(
+                    gameObject.layer,
+                    minimapCamera.cullingMask))
                 {
                     yield return StartCoroutine(MoveArrowUp());
                     yield return StartCoroutine(MoveArrowDown());
                 }
+
                 yield return null;
             }
         }
 
         private IEnumerator MoveArrowUp()
         {
-            var curTime = Time.time;
+            var currentTime = Time.time;
 
             do
             {
                 transform.position += Vector3.up * moveSpeed * Time.deltaTime;
                 yield return null;
-            } while (Time.time < curTime + moveTime);
+            }
+            while (Time.time < currentTime + moveTime);
         }
 
         private IEnumerator MoveArrowDown()
         {
-            var curTime = Time.time;
+            var currentTime = Time.time;
 
             do
             {
                 transform.position += Vector3.down * moveSpeed * Time.deltaTime;
                 yield return null;
-            } while (Time.time < curTime + moveTime);
+            }
+            while (Time.time < currentTime + moveTime);
         }
     }
 }
--- Assets/Maple Fighters/Scripts/Gameplay/Map/Utils.cs
+++ Assets/Maple Fighters/Scripts/Gameplay/Map/Utils.cs
@@ -5,6 +5,18 @@ namespace Scripts.Gameplay
 {
     public static class Utils
     {
+        /// <summary>
+        /// Checks if the layer exists.
+        /// </summary>
+        /// <param name="layer">
+        /// The layer.
+        /// </param>
+        /// <param name="layermask">
+        /// The layermask.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
         public static bool IsInLayerMask(int layer, LayerMask layermask)
         {
             return layermask == (layermask | (1 << layer));
@@ -13,19 +25,29 @@ namespace Scripts.Gameplay
         /// <summary>
         /// Making a space between words (For instance, "AaaBbb" will be "Aaa Bbb")
         /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
         public static string MakeSpaceBetweenWords(this string value)
         {
-            var result = Regex.Replace(value, "[A-Z]", " $0").Trim();
-            return result;
+            return Regex.Replace(value, "[A-Z]", " $0").Trim();
         }
 
         /// <summary>
         /// Removing a "(Clone)" from a name of the created game object.
         /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
         public static string RemoveCloneFromName(this string value)
         {
-            var result = value.Replace("(Clone)", string.Empty);
-            return result;
+            return value.Replace("(Clone)", string.Empty);
         }
     }
 }
