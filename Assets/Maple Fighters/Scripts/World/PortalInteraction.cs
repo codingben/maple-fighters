9fac90187a56d6bea56db95c0a99bb6ec307d7bf
Ben Ukhanov <ben1zuk321@gmail.com>
2018-12-26 21:32:56 +0200

    :alien: Removed old interaction code with the map objects.

--- Assets/Maple Fighters/Scripts/World/PortalInteraction.cs
+++ Assets/Maple Fighters/Scripts/World/PortalInteraction.cs
@@ -4,24 +4,24 @@ namespace Scripts.World
 {
     public class PortalInteraction : MonoBehaviour
     {
-        private const string PORTAL_TAG = "Portal";
+        private const string PortalTag = "Portal";
 
         private void OnTriggerEnter2D(Collider2D collider)
         {
-            var portalGameObject = collider.transform;
-            if (portalGameObject.CompareTag(PORTAL_TAG))
+            if (collider.transform.CompareTag(PortalTag))
             {
-                var portalController = portalGameObject.GetComponent<IPortalControllerBase>();
+                var portalController = 
+                    collider.transform.GetComponent<IPortalControllerBase>();
                 portalController.StartInteraction();
             }
         }
 
         private void OnTriggerExit2D(Collider2D collider)
         {
-            var portalGameObject = collider.transform;
-            if (portalGameObject.CompareTag(PORTAL_TAG))
+            if (collider.transform.CompareTag(PortalTag))
             {
-                var portalController = portalGameObject.GetComponent<IPortalControllerBase>();
+                var portalController = 
+                    collider.transform.GetComponent<IPortalControllerBase>();
                 portalController.StopInteraction();
             }
         }
