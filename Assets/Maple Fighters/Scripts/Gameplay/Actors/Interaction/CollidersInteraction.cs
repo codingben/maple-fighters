da969251f0889685539fd6663c0001b61f63ee6b
Ben Ukhanov <ben1zuk321@gmail.com>
2018-12-28 19:23:20 +0200

    :alien: Created new interaction scripts with Rope object.

---
+++ Assets/Maple Fighters/Scripts/Gameplay/Actors/Interaction/CollidersInteraction.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Scripts.Gameplay.Actors.Interaction
+{
+    public class CollidersInteraction
+    {
+        private readonly Collider2D baseCollider;
+
+        private Collider2D overlappingCollider;
+        private Collider2D ignoredCollider;
+
+        public CollidersInteraction(Collider2D collider)
+        {
+            baseCollider = collider;
+        }
+
+        public void SetOverlappingCollider(Collider2D collider)
+        {
+            overlappingCollider = collider;
+        }
+
+        public void SetIgnoredCollider(Collider2D collider)
+        {
+            ignoredCollider = collider;
+        }
+
+        public void EnableCollisionWithIgnoredCollider()
+        {
+            if (ignoredCollider != null)
+            {
+                Physics2D.IgnoreCollision(baseCollider, ignoredCollider, false);
+            }
+        }
+
+        public void DisableCollisionWithIgnoredCollider()
+        {
+            if (ignoredCollider != null)
+            {
+                Physics2D.IgnoreCollision(baseCollider, ignoredCollider, true);
+            }
+        }
+
+        public bool HasOverlappingCollider()
+        {
+            return overlappingCollider != null;
+        }
+
+        public bool HasOverlappingColliderPosition(out Vector2 position)
+        {
+            var isOverlappingWithCollider = overlappingCollider != null;
+            if (isOverlappingWithCollider)
+            {
+                position = overlappingCollider.bounds.center;
+            }
+            else
+            {
+                position = Vector2.zero;
+            }
+
+            return isOverlappingWithCollider;
+        }
+
+        public Rigidbody2D GetAttachedRigidbody()
+        {
+            return baseCollider.attachedRigidbody;
+        }
+    }
+}
---
+++ Assets/Maple Fighters/Scripts/Gameplay/Actors/Interaction/RopeInteraction.cs
@@ -0,0 +1,146 @@
+﻿using System.Linq;
+using Game.Common;
+using UnityEngine;
+
+namespace Scripts.Gameplay.Actors.Interaction
+{
+    [RequireComponent(typeof(PlayerController), typeof(Collider2D))]
+    public class RopeInteraction : MonoBehaviour
+    {
+        private const string RopeTag = "Rope";
+        private const string FloorTag = "Ground";
+        private const string FloorLayerName = "Floor";
+
+        [SerializeField]
+        private KeyCode interactionKey = KeyCode.LeftControl;
+
+        private PlayerController playerController;
+        private CollidersInteraction collidersInteraction;
+
+        private bool isInInteraction;
+
+        private void Awake()
+        {
+            playerController = GetComponent<PlayerController>();
+
+            var collider = GetComponent<Collider2D>();
+            collidersInteraction = new CollidersInteraction(collider);
+        }
+
+        private void Update()
+        {
+            if (IsInInteraction())
+            {
+                return;
+            }
+
+            if (Input.GetKeyDown(interactionKey) && IsPlayerStateSuitable())
+            {
+                if (collidersInteraction.HasOverlappingCollider())
+                {
+                    StartInteraction();
+                }
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.transform.CompareTag(RopeTag))
+            {
+                collidersInteraction.SetOverlappingCollider(collider);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            if (collider.transform.CompareTag(RopeTag))
+            {
+                if (IsInInteraction())
+                {
+                    collidersInteraction.EnableCollisionWithIgnoredCollider();
+                    collidersInteraction.SetIgnoredCollider(null);
+
+                    StopInteraction();
+                }
+
+                collidersInteraction.SetOverlappingCollider(null);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (IsInInteraction() && collision.transform.CompareTag(FloorTag))
+            {
+                collidersInteraction.SetIgnoredCollider(collision.collider);
+                collidersInteraction.DisableCollisionWithIgnoredCollider();
+            }
+        }
+
+        private void StartInteraction()
+        {
+            var ground = GetGroundedCollider();
+            if (ground != null)
+            {
+                collidersInteraction.SetIgnoredCollider(ground);
+                collidersInteraction.DisableCollisionWithIgnoredCollider();
+            }
+
+            ChangePositionToRopeCenter();
+
+            playerController.ChangePlayerState(PlayerState.Rope);
+        }
+
+        private void StopInteraction()
+        {
+            collidersInteraction.EnableCollisionWithIgnoredCollider();
+            collidersInteraction.SetIgnoredCollider(null);
+
+            playerController.ChangePlayerState(PlayerState.Idle);
+        }
+
+        private void ChangePositionToRopeCenter()
+        {
+            var rigidbody = collidersInteraction.GetAttachedRigidbody();
+            rigidbody.velocity = Vector2.zero;
+
+            Vector2 center;
+            if (collidersInteraction.HasOverlappingColliderPosition(out center))
+            {
+                transform.parent.position = 
+                    new Vector3(center.x, transform.parent.position.y);
+            }
+        }
+
+        private bool IsPlayerStateSuitable()
+        {
+            return playerController.PlayerState == PlayerState.Idle
+                             || playerController.PlayerState
+                             == PlayerState.Jumping
+                             || playerController.PlayerState
+                             == PlayerState.Falling;
+        }
+
+        private bool IsInInteraction()
+        {
+            return playerController.PlayerState == PlayerState.Rope;
+        }
+
+        private Collider2D GetGroundedCollider()
+        {
+            var hit = Physics2D.RaycastAll(
+                transform.parent.position,
+                Vector2.down,
+                1,
+                1 << LayerMask.NameToLayer(FloorLayerName));
+
+            Collider2D collider = null;
+
+            if (hit.Any())
+            {
+                collider = hit.First().collider;
+            }
+
+            return collider;
+        }
+    }
+}
