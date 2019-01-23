b2320ec57fec3e9bad313a50e232f9cdde533316
Ben Ukhanov <ben1zuk321@gmail.com>
2019-01-23 22:23:36 +0200

    :alien: A new controller for the screen fade image.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/ScreenFadeController.cs
@@ -0,0 +1,64 @@
+﻿using Scripts.Utils;
+using UI.Manager;
+
+namespace Scripts.UI.Controllers
+{
+    public class ScreenFadeController : MonoSingleton<ScreenFadeController>
+    {
+        private ScreenFadeImage screenFadeImage;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+
+            Hide();
+        }
+
+        protected override void OnDestroying()
+        {
+            base.OnDestroying();
+
+            if (screenFadeImage != null)
+            {
+                Destroy(screenFadeImage.gameObject);
+            }
+        }
+
+        public void Show()
+        {
+            if (screenFadeImage == null)
+            {
+                screenFadeImage = UIElementsCreator.GetInstance()
+                    .Create<ScreenFadeImage>(UILayer.Foreground, UIIndex.End);
+                screenFadeImage.Show();
+            }
+        }
+
+        public void Hide()
+        {
+            if (screenFadeImage == null)
+            {
+                screenFadeImage = UIElementsCreator.GetInstance()
+                    .Create<ScreenFadeImage>(UILayer.Foreground, UIIndex.End);
+                screenFadeImage.Hide();
+            }
+            else
+            {
+                screenFadeImage.Hide();
+            }
+
+            var uiFadeAnimation =
+                screenFadeImage.GetComponent<UIFadeAnimation>();
+            uiFadeAnimation.FadeOutCompleted += OnFadeOutCompleted;
+        }
+
+        private void OnFadeOutCompleted()
+        {
+            var uiFadeAnimation =
+                screenFadeImage.GetComponent<UIFadeAnimation>();
+            uiFadeAnimation.FadeOutCompleted -= OnFadeOutCompleted;
+
+            Destroy(screenFadeImage.gameObject);
+        }
+    }
+}
