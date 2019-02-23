05f47d63529eef0a6a5e8d7beca9f8f3dee9d4a8
Ben Ukhanov <ben1zuk321@gmail.com>
2019-02-23 14:43:30 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/CharacterSelectionController.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/CharacterSelectionController.cs
@@ -11,38 +11,62 @@ namespace Scripts.UI.Controllers
 
         public event Action CharacterCancelled;
 
+        [Header("Configuration")]
+        [SerializeField]
+        private int characterNameLength;
+
         private UICharacterDetails uiCharacterDetails;
+
         private CharacterSelectionWindow characterSelectionWindow;
+        private CharacterNameWindow characterNameWindow;
 
         public void SetCharacterDetails(UICharacterDetails uiCharacterDetails)
         {
             this.uiCharacterDetails = uiCharacterDetails;
         }
 
-        public void ShowCharacterSelectionWindow()
+        public UICharacterDetails GetCharacterDetails()
         {
-            if (characterSelectionWindow == null)
-            {
-                characterSelectionWindow = UIElementsCreator.GetInstance()
-                    .Create<CharacterSelectionWindow>();
-                characterSelectionWindow.ChooseButtonClicked +=
-                    OnChooseButtonClicked;
-                characterSelectionWindow.CancelButtonClicked +=
-                    OnCancelButtonClicked;
-                characterSelectionWindow.KnightSelected += OnKnightSelected;
-                characterSelectionWindow.ArrowSelected += OnArrowSelected;
-                characterSelectionWindow.WizardSelected += OnWizardSelected;
-            }
-
-            characterSelectionWindow.Show();
+            return uiCharacterDetails;
         }
 
         private void Awake()
         {
-            SubscribeToCharacterNameEvents();
+            CreateCharacterSelectionWindow();
+            CreateCharacterNameWindow();
+        }
+
+        private void CreateCharacterSelectionWindow()
+        {
+            characterSelectionWindow = UIElementsCreator.GetInstance()
+                .Create<CharacterSelectionWindow>();
+            characterSelectionWindow.ChooseButtonClicked +=
+                OnChooseButtonClicked;
+            characterSelectionWindow.CancelButtonClicked +=
+                OnCancelButtonClicked;
+            characterSelectionWindow.KnightSelected += OnKnightSelected;
+            characterSelectionWindow.ArrowSelected += OnArrowSelected;
+            characterSelectionWindow.WizardSelected += OnWizardSelected;
+        }
+
+        private void CreateCharacterNameWindow()
+        {
+            characterNameWindow = UIElementsCreator.GetInstance()
+                .Create<CharacterNameWindow>();
+            characterNameWindow.ConfirmButtonClicked +=
+                OnConfirmButtonClicked;
+            characterNameWindow.BackButtonClicked += OnBackButtonClicked;
+            characterNameWindow.NameInputFieldChanged +=
+                OnNameInputFieldChanged;
         }
 
         private void OnDestroy()
+        {
+            DestroyCharacterSelectionWindow();
+            DestroyCharacterNameWindow();
+        }
+
+        private void DestroyCharacterSelectionWindow()
         {
             if (characterSelectionWindow != null)
             {
@@ -56,40 +80,44 @@ namespace Scripts.UI.Controllers
 
                 Destroy(characterSelectionWindow.gameObject);
             }
-
-            UnsubscribeToCharacterNameEvents();
         }
 
-        private void SubscribeToCharacterNameEvents()
+        private void DestroyCharacterNameWindow()
         {
-            // TODO: Use event bus system
-            var characterNameController =
-                FindObjectOfType<CharacterNameController>();
-            if (characterNameController != null)
+            if (characterNameWindow != null)
             {
-                characterNameController.ConfirmButtonClicked +=
+                characterNameWindow.ConfirmButtonClicked -=
                     OnConfirmButtonClicked;
-                characterNameController.BackButtonClicked +=
-                    OnBackButtonClicked;
+                characterNameWindow.BackButtonClicked -= OnBackButtonClicked;
+                characterNameWindow.NameInputFieldChanged -=
+                    OnNameInputFieldChanged;
+
+                Destroy(characterNameWindow.gameObject);
             }
         }
 
-        private void UnsubscribeToCharacterNameEvents()
+        private void OnNameInputFieldChanged(string characterName)
         {
-            // TODO: Use event bus system
-            var characterNameController =
-                FindObjectOfType<CharacterNameController>();
-            if (characterNameController != null)
+            if (characterName.Length >= characterNameLength)
             {
-                characterNameController.ConfirmButtonClicked -=
-                    OnConfirmButtonClicked;
-                characterNameController.BackButtonClicked -=
-                    OnBackButtonClicked;
+                if (characterNameWindow != null)
+                {
+                    characterNameWindow.EnableConfirmButton();
+                }
+            }
+            else
+            {
+                if (characterNameWindow != null)
+                {
+                    characterNameWindow.DisableConfirmButton();
+                }
             }
         }
 
         private void OnConfirmButtonClicked(string characterName)
         {
+            HideCharacterNameWindow();
+
             uiCharacterDetails.SetCharacterName(characterName);
 
             CharacterChosen?.Invoke();
@@ -97,25 +125,20 @@ namespace Scripts.UI.Controllers
 
         private void OnBackButtonClicked()
         {
-            if (characterSelectionWindow != null)
-            {
-                characterSelectionWindow.Show();
-            }
+            HideCharacterNameWindow();
+            ShowCharacterSelectionWindow();
         }
 
         private void OnChooseButtonClicked()
         {
-            // TODO: Use event bus system
-            var characterNameController =
-                FindObjectOfType<CharacterNameController>();
-            if (characterNameController != null)
-            {
-                characterNameController.ShowCharacterNameWindow();
-            }
+            HideCharacterSelectionWindow();
+            ShowCharacterNameWindow();
         }
 
         private void OnCancelButtonClicked()
         {
+            HideCharacterSelectionWindow();
+
             CharacterCancelled?.Invoke();
         }
 
@@ -134,9 +157,36 @@ namespace Scripts.UI.Controllers
             uiCharacterDetails.SetCharacterClass(UICharacterClass.Wizard);
         }
 
-        public UICharacterDetails GetCharacterDetails()
+        private void ShowCharacterNameWindow()
         {
-            return uiCharacterDetails;
+            if (characterNameWindow != null)
+            {
+                characterNameWindow.Show();
+            }
+        }
+
+        private void HideCharacterNameWindow()
+        {
+            if (characterNameWindow != null)
+            {
+                characterNameWindow.Hide();
+            }
+        }
+
+        public void ShowCharacterSelectionWindow()
+        {
+            if (characterSelectionWindow != null)
+            {
+                characterSelectionWindow.Show();
+            }
+        }
+
+        private void HideCharacterSelectionWindow()
+        {
+            if (characterSelectionWindow != null)
+            {
+                characterSelectionWindow.Hide();
+            }
         }
     }
 }
