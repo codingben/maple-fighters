80c758268a479c4e90b8ca336fa90ecdc722bf10
Ben Ukhanov <ben1zuk321@gmail.com>
2019-02-26 21:18:43 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/MenuBackgroundController.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/MenuBackgroundController.cs
@@ -5,27 +5,10 @@ namespace Scripts.UI.Controllers
 {
     public class MenuBackgroundController : MonoBehaviour
     {
-        private IMenuBackgroundView backgroundView;
-
         private void Awake()
         {
-            CreateMenuBackgroundImage();
-        }
-
-        private void CreateMenuBackgroundImage()
-        {
-            backgroundView = UIElementsCreator.GetInstance()
+            UIElementsCreator.GetInstance()
                 .Create<MenuBackgroundImage>(UILayer.Background);
-        }
-
-        private void OnDestroy()
-        {
-            DestroyMenuBackgroundImage();
-        }
-
-        private void DestroyMenuBackgroundImage()
-        {
-            // TODO: Implement
         }
     }
 }
