cbc9409701d3d8d5111e353252c0d74f48a214c0
Ben Ukhanov <ben1zuk321@gmail.com>
2019-02-28 23:46:00 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/FocusStateController.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/FocusStateController.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Scripts.UI.Controllers
 {
     public class FocusStateController : MonoBehaviour
     {
-        public event Action<FocusState> FocusStateChanged;
-
         private FocusState focusState = FocusState.Game;
 
         public void ChangeFocusState(FocusState focusState)
         {
             this.focusState = focusState;
-
-            FocusStateChanged?.Invoke(focusState);
         }
 
         public FocusState GetFocusState()
