41864230977d2cda23f6c272984b752a7c069d17
Ben Ukhanov <ben1zuk321@gmail.com>
2019-03-03 21:25:24 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/CharacterViewInteractor.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/CharacterViewInteractor.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using CommonTools.Coroutines;
-using Game.Common;
 using Scripts.Containers;
 using Scripts.Network.APIs;
 using Scripts.UI.Models;
@@ -50,100 +49,14 @@ namespace Scripts.UI.Controllers
             {
                 var characterDetails = new CharacterDetails(
                     character.Name,
-                    ConvertToUiCharacterIndex(character.Index),
-                    ConvertToUiCharacterClass(character.CharacterType),
-                    ConvertToUiMapIndex(character.LastMap),
+                    character.Index.ConvertToUiCharacterIndex(),
+                    character.CharacterType.ConvertToUiCharacterClass(),
+                    character.LastMap.ConvertToUiMapIndex(),
                     character.HasCharacter);
 
                 onCharacterReceivedListener.OnCharacterReceived(
                     characterDetails);
             }
-        }
-
-        private UICharacterIndex ConvertToUiCharacterIndex(
-            CharacterIndex characterIndex)
-        {
-            var uiCharacterIndex = UICharacterIndex.Zero;
-
-            switch (characterIndex)
-            {
-                case CharacterIndex.Zero:
-                {
-                    uiCharacterIndex = UICharacterIndex.Zero;
-                    break;
-                }
-
-                case CharacterIndex.First:
-                {
-                    uiCharacterIndex = UICharacterIndex.First;
-                    break;
-                }
-
-                case CharacterIndex.Second:
-                {
-                    uiCharacterIndex = UICharacterIndex.Second;
-                    break;
-                }
-
-                case CharacterIndex.Third:
-                {
-                    uiCharacterIndex = UICharacterIndex.Third;
-                    break;
-                }
-            }
-
-            return uiCharacterIndex;
-        }
-
-        private UICharacterClass ConvertToUiCharacterClass(
-            CharacterClasses characterClasses)
-        {
-            var uiCharacterClass = UICharacterClass.Knight;
-
-            switch (characterClasses)
-            {
-                case CharacterClasses.Knight:
-                {
-                    uiCharacterClass = UICharacterClass.Knight;
-                    break;
-                }
-
-                case CharacterClasses.Arrow:
-                {
-                    uiCharacterClass = UICharacterClass.Arrow;
-                    break;
-                }
-
-                case CharacterClasses.Wizard:
-                {
-                    uiCharacterClass = UICharacterClass.Wizard;
-                    break;
-                }
-            }
-
-            return uiCharacterClass;
-        }
-
-        private UIMapIndex ConvertToUiMapIndex(Maps map)
-        {
-            var uiMapIndex = UIMapIndex.Map_1;
-
-            switch (map)
-            {
-                case Maps.Map_1:
-                {
-                    uiMapIndex = UIMapIndex.Map_1;
-                    break;
-                }
-
-                case Maps.Map_2:
-                {
-                    uiMapIndex = UIMapIndex.Map_2;
-                    break;
-                }
-            }
-
-            return uiMapIndex;
         }
     }
 }
---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/ExtensionMethods.cs
@@ -0,0 +1,93 @@
+﻿using Game.Common;
+
+namespace Scripts.UI.Controllers
+{
+    public static class ExtensionMethods
+    {
+        public static UICharacterIndex ConvertToUiCharacterIndex(
+            this CharacterIndex characterIndex)
+        {
+            var uiCharacterIndex = UICharacterIndex.Zero;
+
+            switch (characterIndex)
+            {
+                case CharacterIndex.Zero:
+                {
+                    uiCharacterIndex = UICharacterIndex.Zero;
+                    break;
+                }
+
+                case CharacterIndex.First:
+                {
+                    uiCharacterIndex = UICharacterIndex.First;
+                    break;
+                }
+
+                case CharacterIndex.Second:
+                {
+                    uiCharacterIndex = UICharacterIndex.Second;
+                    break;
+                }
+
+                case CharacterIndex.Third:
+                {
+                    uiCharacterIndex = UICharacterIndex.Third;
+                    break;
+                }
+            }
+
+            return uiCharacterIndex;
+        }
+
+        public static UICharacterClass ConvertToUiCharacterClass(
+            this CharacterClasses characterClasses)
+        {
+            var uiCharacterClass = UICharacterClass.Knight;
+
+            switch (characterClasses)
+            {
+                case CharacterClasses.Knight:
+                {
+                    uiCharacterClass = UICharacterClass.Knight;
+                    break;
+                }
+
+                case CharacterClasses.Arrow:
+                {
+                    uiCharacterClass = UICharacterClass.Arrow;
+                    break;
+                }
+
+                case CharacterClasses.Wizard:
+                {
+                    uiCharacterClass = UICharacterClass.Wizard;
+                    break;
+                }
+            }
+
+            return uiCharacterClass;
+        }
+
+        public static UIMapIndex ConvertToUiMapIndex(this Maps map)
+        {
+            var uiMapIndex = UIMapIndex.Map_1;
+
+            switch (map)
+            {
+                case Maps.Map_1:
+                {
+                    uiMapIndex = UIMapIndex.Map_1;
+                    break;
+                }
+
+                case Maps.Map_2:
+                {
+                    uiMapIndex = UIMapIndex.Map_2;
+                    break;
+                }
+            }
+
+            return uiMapIndex;
+        }
+    }
+}
