efbdd4f167c0afcd29637791d8466364de571a7d
Ben Ukhanov <ben1zuk321@gmail.com>
2019-03-13 23:21:15 +0200

    :alien: Code refactoring.

--- src/ui-manager/Assets/UI Manager/Sample/Scripts/SampleWindowController.cs
+++ src/ui-manager/Assets/UI Manager/Sample/Scripts/SampleWindowController.cs
@@ -11,14 +11,6 @@ namespace Sample.Scripts
         private void Awake()
         {
             CreateAndSubscribeToSampleWindow();
-            SubscribeToFadeAnimationEvents();
-        }
-
-        private void CreateAndSubscribeToSampleWindow()
-        {
-            sampleWindow =
-                UIElementsCreator.GetInstance().Create<SampleWindow>();
-            sampleWindow.PointerClicked += OnPointerClicked;
         }
 
         private void Start()
@@ -29,7 +21,21 @@ namespace Sample.Scripts
         private void OnDestroy()
         {
             UnsubscribeFromSampleWindow();
-            UnsubscribeFromFadeAnimationEvents();
+        }
+
+        private void CreateAndSubscribeToSampleWindow()
+        {
+            sampleWindow =
+                UIElementsCreator.GetInstance().Create<SampleWindow>();
+            sampleWindow.PointerClicked += OnPointerClicked;
+        }
+
+        private void UnsubscribeFromSampleWindow()
+        {
+            if (sampleWindow != null)
+            {
+                sampleWindow.PointerClicked -= OnPointerClicked;
+            }
         }
 
         private void OnPointerClicked(PointerEventData eventData)
@@ -50,49 +56,6 @@ namespace Sample.Scripts
             if (sampleWindow != null)
             {
                 sampleWindow.Hide();
-            }
-        }
-
-        private void SubscribeToFadeAnimationEvents()
-        {
-            if (sampleWindow != null)
-            {
-                var uiFadeAnimation =
-                    sampleWindow.GetComponent<UIFadeAnimation>();
-                uiFadeAnimation.FadeInCompleted += OnFadeInCompleted;
-                uiFadeAnimation.FadeOutCompleted += OnFadeOutCompleted;
-            }
-        }
-
-        private void UnsubscribeFromFadeAnimationEvents()
-        {
-            if (sampleWindow != null)
-            {
-                var uiFadeAnimation =
-                    sampleWindow.GetComponent<UIFadeAnimation>();
-                uiFadeAnimation.FadeInCompleted -= OnFadeInCompleted;
-                uiFadeAnimation.FadeOutCompleted -= OnFadeOutCompleted;
-            }
-        }
-
-        private void OnFadeInCompleted()
-        {
-            var sampleMessage =
-                UIElementsCreator.GetInstance().Create<SampleMessage>();
-            sampleMessage.Show();
-        }
-
-        private void OnFadeOutCompleted()
-        {
-            UnsubscribeFromFadeAnimationEvents();
-            UnsubscribeFromSampleWindow();
-        }
-
-        private void UnsubscribeFromSampleWindow()
-        {
-            if (sampleWindow != null)
-            {
-                sampleWindow.PointerClicked -= OnPointerClicked;
             }
         }
     }
