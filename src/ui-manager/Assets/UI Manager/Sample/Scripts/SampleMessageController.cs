285e77c4eec1156eefe8a826e805bfc6392b3804
Ben Ukhanov <ben1zuk321@gmail.com>
2019-03-13 23:17:34 +0200

    :alien: A new script.

---
+++ src/ui-manager/Assets/UI Manager/Sample/Scripts/SampleMessageController.cs
@@ -0,0 +1,15 @@
+﻿using UI.Manager;
+using UnityEngine;
+
+namespace Sample.Scripts
+{
+    public class SampleMessageController : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var sampleMessage =
+                UIElementsCreator.GetInstance().Create<SampleMessage>();
+            sampleMessage.Show();
+        }
+    }
+}
