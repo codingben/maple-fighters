218ab65cb988ea149f53b83901d89d8d777bc9c1
Ben Ukhanov <ben1zuk321@gmail.com>
2019-03-13 23:31:54 +0200

    :alien: A bug fix.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/EventSystemSendNavigationEventsSetter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Scripts.UI
+{
+    public class EventSystemSendNavigationEventsSetter : MonoBehaviour
+    {
+        [SerializeField]
+        private bool sendNavigationEvents;
+
+        private void Start()
+        {
+            EventSystem.current.sendNavigationEvents = sendNavigationEvents;
+        }
+    }
+}
