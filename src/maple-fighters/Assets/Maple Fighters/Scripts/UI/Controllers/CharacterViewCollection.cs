2b68545cf017e5a6d87644f0e826c77e698aba39
Ben Ukhanov <ben1zuk321@gmail.com>
2019-03-16 20:21:07 +0200

    :alien: Code refactoring.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Controllers/CharacterViewCollection.cs
@@ -0,0 +1,22 @@
+﻿namespace Scripts.UI.Controllers
+{
+    public struct CharacterViewCollection
+    {
+        private IClickableCharacterView[] collection;
+
+        public CharacterViewCollection(IClickableCharacterView[] collection)
+        {
+            this.collection = collection;
+        }
+
+        public void Set(int index, IClickableCharacterView characterView)
+        {
+            collection[index] = characterView;
+        }
+        
+        public IClickableCharacterView[] GetAll()
+        {
+            return collection;
+        }
+    }
+}
