c6cf622d47e3236d8c973afd30ef15555c6dac62
Ben Ukhanov <ben1zuk321@gmail.com>
2019-05-19 08:48:05 +0300

    :alien: A falling down with direction.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/PlayerFallingState.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/PlayerFallingState.cs
@@ -1,19 +1,35 @@
 using Game.Common;
+using UnityEngine;
 
 namespace Scripts.Gameplay.Actors
 {
     public class PlayerFallingState : IPlayerStateBehaviour
     {
         private readonly PlayerController playerController;
+        private readonly Rigidbody2D rigidbody2D;
 
         public PlayerFallingState(PlayerController playerController)
         {
             this.playerController = playerController;
+
+            var collider = playerController.GetComponent<Collider2D>();
+            rigidbody2D = collider.attachedRigidbody;
         }
 
         public void OnStateEnter()
         {
-            // Left blank intentionally
+            FallDownWithDirection();
+        }
+
+        private void FallDownWithDirection()
+        {
+            // TODO: Move to the configuration
+            var horizontal = Input.GetAxis("Horizontal");
+            var speed = playerController.Configuration.Speed;
+            var direction = 
+                new Vector2(horizontal * speed, rigidbody2D.velocity.y);
+
+            rigidbody2D.velocity = direction;
         }
 
         public void OnStateUpdate()
