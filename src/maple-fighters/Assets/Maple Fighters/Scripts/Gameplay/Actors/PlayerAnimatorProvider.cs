00c356cabc68c848974a052e2b70ff0c753ba29c
Ben Ukhanov <ben1zuk321@gmail.com>
2019-05-24 12:22:05 +0300

    :alien: Code refactoring.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Actors/PlayerAnimatorProvider.cs
@@ -0,0 +1,21 @@
+﻿using Game.Common;
+using Scripts.Utils.Shared;
+using UnityEngine;
+
+namespace Scripts.Gameplay.Actors
+{
+    public class PlayerAnimatorProvider : MonoBehaviour
+    {
+        private Animator animator;
+
+        public void Set(Animator animator)
+        {
+            this.animator = animator;
+        }
+
+        public Animator Provide()
+        {
+            return animator;
+        }
+    }
+}
