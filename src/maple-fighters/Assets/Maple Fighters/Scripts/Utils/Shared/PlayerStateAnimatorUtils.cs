5d60a010f1ecdd817a12601d1ca81523aa20fee8
Ben Ukhanov <ben1zuk321@gmail.com>
2019-05-24 11:51:39 +0300

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Utils/Shared/PlayerStateAnimatorUtils.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Utils/Shared/PlayerStateAnimatorUtils.cs
@@ -6,7 +6,8 @@ namespace Scripts.Utils.Shared
     public static class PlayerStateAnimatorUtils
     {
         public static void ChangePlayerAnimationState(
-            this Animator animator, PlayerState playerState)
+            this Animator animator, 
+            PlayerState playerState)
         {
             // TODO: Hack
             if (playerState == PlayerState.Attacked)
@@ -14,16 +15,10 @@ namespace Scripts.Utils.Shared
                 playerState = PlayerState.Falling;
             }
 
-            // TODO: Do a better way to get name of the animations
-            const string WalkName = "Walking";
-            const string JumpName = "Jump";
-            const string RopeName = "Rope";
-            const string LadderName = "Ladder";
-
-            animator.SetBool(WalkName, playerState == PlayerState.Moving);
-            animator.SetBool(JumpName, playerState == PlayerState.Jumping || playerState == PlayerState.Falling);
-            animator.SetBool(RopeName, playerState == PlayerState.Rope);
-            animator.SetBool(LadderName, playerState == PlayerState.Ladder);
+            animator.SetBool("WalkName", playerState == PlayerState.Moving);
+            animator.SetBool("JumpName", playerState == PlayerState.Jumping || playerState == PlayerState.Falling);
+            animator.SetBool("RopeName", playerState == PlayerState.Rope);
+            animator.SetBool("LadderName", playerState == PlayerState.Ladder);
         }
     }
 }
