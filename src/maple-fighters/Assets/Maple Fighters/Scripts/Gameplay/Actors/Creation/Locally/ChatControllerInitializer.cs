eba123a83df790df5db56e9b6a3434af3020ce82
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-04 14:09:18 +0200

    :alien: Extracted scripts.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Actors/Creation/Locally/ChatControllerInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Scripts.UI.Controllers;
+using UnityEngine;
+
+namespace Scripts.Gameplay.Actors
+{
+    [RequireComponent(typeof(SpawnCharacter), typeof(SpawnedCharacterDetails))]
+    public class ChatControllerInitializer : MonoBehaviour
+    {
+        private ISpawnedCharacter spawnedCharacter;
+
+        private void Awake()
+        {
+            spawnedCharacter = GetComponent<ISpawnedCharacter>();
+        }
+
+        private void Start()
+        {
+            spawnedCharacter.CharacterSpawned += OnCharacterSpawned;
+        }
+
+        private void OnDestroy()
+        {
+            spawnedCharacter.CharacterSpawned -= OnCharacterSpawned;
+        }
+
+        private void OnCharacterSpawned()
+        {
+            var chatController = FindObjectOfType<ChatController>();
+            if (chatController != null)
+            {
+                var characterDetailsProvider = GetComponent<ISpawnedCharacterDetails>();
+                var characterDetails = characterDetailsProvider.GetCharacterDetails();
+                var characterName = characterDetails.Character.Name;
+
+                chatController.SetCharacterName(characterName);
+            }
+        }
+    }
+}
---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Actors/Creation/Locally/PlayerControllerInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Scripts.Utils.Shared;
+using UnityEngine;
+
+namespace Scripts.Gameplay.Actors
+{
+    [RequireComponent(typeof(SpawnCharacter))]
+    public class PlayerControllerInitializer : MonoBehaviour
+    {
+        private ISpawnedCharacter spawnedCharacter;
+
+        private void Awake()
+        {
+            spawnedCharacter = GetComponent<ISpawnedCharacter>();
+        }
+
+        private void Start()
+        {
+            spawnedCharacter.CharacterSpawned += OnCharacterSpawned;
+        }
+
+        private void OnDestroy()
+        {
+            spawnedCharacter.CharacterSpawned -= OnCharacterSpawned;
+        }
+
+        private void OnCharacterSpawned()
+        {
+            var playerStateAnimator = spawnedCharacter
+                .GetCharacterSpriteGameObject()
+                .AddComponent<PlayerStateAnimator>();
+            if (playerStateAnimator != null)
+            {
+                var playerController = spawnedCharacter
+                    .GetCharacterGameObject().GetComponent<PlayerController>();
+                if (playerController != null)
+                {
+                    // TODO: Wtf? Who will unsubscribe from there?
+                    playerController.PlayerStateChanged +=
+                        playerStateAnimator.OnPlayerStateChanged;
+                }
+            }
+        }
+    }
+}
