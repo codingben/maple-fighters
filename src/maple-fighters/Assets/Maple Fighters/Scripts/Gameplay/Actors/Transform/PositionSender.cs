3992d42c7b545f21373f2dfbb6fb9187e2b4387a
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-08 09:02:39 +0200

    :alien: Calculating the direction with position.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Actors/Transform/PositionSender.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Actors/Transform/PositionSender.cs
@@ -7,33 +7,39 @@ namespace Scripts.Gameplay.Actors
     public class PositionSender : MonoBehaviour
     {
         private const float GreaterDistance = 0.1f;
-        private Vector2 position;
+        private Vector2 lastPosition;
 
         private void Awake()
         {
-            position = transform.position;
+            lastPosition = transform.position;
         }
 
         private void Update()
         {
-            var distance = Vector2.Distance(transform.position, position);
+            var distance = Vector2.Distance(transform.position, lastPosition);
             if (distance > GreaterDistance)
             {
-                position = transform.position;
+                lastPosition = transform.position;
 
                 var gameSceneApi = ServiceContainer.GameService.GetGameSceneApi();
                 if (gameSceneApi != null)
                 {
-                    // TODO: A way to change the direction
-                    var direction = Directions.None;
+                    var z = GetDirection();
 
                     gameSceneApi.UpdatePosition(
                         new UpdatePositionRequestParameters(
-                            transform.position.x,
-                            transform.position.y,
-                            direction));
+                            transform.position.x, transform.position.y, z));
                 }
             }
+        }
+
+        private Directions GetDirection()
+        {
+            var position = new Vector3(lastPosition.x, lastPosition.y);
+            var orientation = (transform.position - position).normalized;
+            var direction = orientation.x < 0 ? Directions.Left : Directions.Right;
+
+            return direction;
         }
     }
 }
