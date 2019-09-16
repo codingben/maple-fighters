ee9bbcc0cf5fe3006d89861e48e1ea8422bbfab8
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-16 09:19:57 +0200

    :alien: A new behaviour when the player attacked.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/IAttackPlayer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Scripts.Gameplay.Player
+{
+    public interface IAttackPlayer
+    {
+        void OnPlayerAttacked(Vector3 direction);
+    }
+}
---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/PlayerAttackedBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using Game.Common;
+using UnityEngine;
+
+namespace Scripts.Gameplay.Player
+{
+    [RequireComponent(typeof(PlayerController))]
+    public class PlayerAttackedBehaviour : MonoBehaviour, IAttackPlayer
+    {
+        private PlayerController playerController;
+
+        private void Awake()
+        {
+            playerController = GetComponent<PlayerController>();
+        }
+
+        public void OnPlayerAttacked(Vector3 direction)
+        {
+            if (playerController.PlayerState != PlayerState.Attacked)
+            {
+                playerController.ChangePlayerState(PlayerState.Attacked);
+
+                StartCoroutine(WaitFrameAndBounce(direction));
+            }
+        }
+
+        private IEnumerator WaitFrameAndBounce(Vector3 direction)
+        {
+            yield return null;
+
+            playerController.Bounce(direction);
+        }
+    }
+}
