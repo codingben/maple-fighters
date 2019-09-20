f1a411c2214ec8f7b348ce1defc175bd7313d97e
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-20 06:58:04 +0200

    :alien: Removed unused method.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/ScriptableObjects/NetworkConfiguration.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/ScriptableObjects/NetworkConfiguration.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using CommonCommunicationInterfaces;
-using ExitGames.Client.Photon;
-using Scripts.Utils;
+﻿using ExitGames.Client.Photon;
 using UnityEngine;
 
 namespace Scripts.ScriptableObjects
@@ -17,49 +14,5 @@ namespace Scripts.ScriptableObjects
         public bool LogOperationsRequest;
         public bool LogOperationsResponse;
         public bool LogEvents;
-
-        public PeerConnectionInformation GetPeerConnectionInformation(
-            ConnectionInformation connectionInformation)
-        {
-            PeerConnectionInformation peerConnectionInformation;
-
-            if (ConnectionProtocol == ConnectionProtocol.Tcp)
-            {
-                Debug.LogWarning(
-                    "TCP is not supported. TCP is only for communication between servers.");
-            }
-
-            switch (ConnectionProtocol)
-            {
-                case ConnectionProtocol.Udp:
-                case ConnectionProtocol.Tcp:
-                {
-                    peerConnectionInformation =
-                        connectionInformation.UdpConnectionDetails;
-                    break;
-                }
-
-                case ConnectionProtocol.WebSocket:
-                {
-                    peerConnectionInformation = connectionInformation
-                        .WebSocketConnectionDetails;
-                    break;
-                }
-
-                case ConnectionProtocol.WebSocketSecure:
-                {
-                    peerConnectionInformation = connectionInformation
-                        .WebSocketSecureConnectionDetails;
-                    break;
-                }
-
-                default:
-                {
-                    throw new ArgumentOutOfRangeException();
-                }
-            }
-
-            return peerConnectionInformation;
-        }
     }
 }
