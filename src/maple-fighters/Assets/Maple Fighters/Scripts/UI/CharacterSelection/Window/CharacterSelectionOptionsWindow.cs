319c74171c46926e1f03f8394c20df375ba97ae3
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-20 05:44:23 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Window/CharacterSelectionOptionsWindow.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Window/CharacterSelectionOptionsWindow.cs
@@ -3,7 +3,7 @@ using UI.Manager;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Scripts.UI.Windows
+namespace Scripts.UI.CharacterSelection
 {
     [RequireComponent(typeof(UIFadeAnimation))]
     public class CharacterSelectionOptionsWindow : UIElement,
