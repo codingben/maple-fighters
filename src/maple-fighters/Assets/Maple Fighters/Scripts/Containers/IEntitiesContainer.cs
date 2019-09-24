5ca9d8306fe05040a9073fe07ffa46e00c777123
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-24 10:01:53 +0200

    :alien: Code refactoring.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Containers/IEntitiesContainer.cs
@@ -0,0 +1,14 @@
+﻿using Game.Common;
+using Scripts.Gameplay;
+
+namespace Scripts.Containers
+{
+    public interface IEntitiesContainer
+    {
+        void SetLocalEntity(IEntity entity);
+
+        IEntity AddEntity(SceneObjectParameters parameters);
+
+        void RemoveEntity(int id);
+    }
+}
