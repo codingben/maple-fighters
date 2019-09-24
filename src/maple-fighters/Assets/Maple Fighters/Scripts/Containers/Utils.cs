ecd1f85a9b83042b003ce6c5f2ef7fb1e13ad90e
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-24 10:53:58 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Containers/Utils.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Scripts.Containers
+{
+    public class Utils
+    {
+        public static GameObject CreateGameObject(string name, Vector3 position)
+        {
+            GameObject gameObject = null;
+
+            var entity = Resources.Load($"Game/{name}");
+            if (entity != null)
+            {
+                gameObject =
+                    Object.Instantiate(entity, position, Quaternion.identity)
+                        as GameObject;
+
+                if (gameObject != null)
+                {
+                    gameObject.name = name;
+                }
+            }
+            else
+            {
+                Debug.LogError(
+                    $"Could not find an object with name {name}");
+            }
+
+            return gameObject;
+        }
+    }
+}
