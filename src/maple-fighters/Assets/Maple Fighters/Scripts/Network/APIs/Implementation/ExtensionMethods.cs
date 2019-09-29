8a2b8ccf943be85d4a1da13e99bff66100dfa832
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-29 11:07:31 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Network/APIs/Implementation/ExtensionMethods.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Network/APIs/Implementation/ExtensionMethods.cs
@@ -7,7 +7,7 @@ namespace Scripts.Network.APIs
         public static EventHandler<TParameters> ToHandler<TParameters>(this UnityEvent<TParameters> action)
             where TParameters : struct, IParameters
         {
-            return new EventHandler<TParameters>((messageData) => action?.Invoke(messageData.Parameters));
+            return new EventHandler<TParameters>((x) => action?.Invoke(x.Parameters));
         }
     }
 }
