9b354317bad1e747660e32ab5952f7ad352e3a00
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-29 10:57:35 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/World/Dummy/DummyLocalEntityCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Scripts.Gameplay.Actors;
+using Scripts.Network.Services;
+using UnityEngine;
+
+namespace Scripts.World.Dummy
+{
+    [RequireComponent(typeof(DummyCharacterDetailsProvider))]
+    public class DummyLocalEntityCreator : MonoBehaviour
+    {
+        private void Start()
+        {
+            StartCoroutine(WaitFrameAndStart());
+        }
+
+        private IEnumerator WaitFrameAndStart()
+        {
+            yield return null;
+
+            CreateLocalDummyEntity();
+        }
+
+        private void CreateLocalDummyEntity()
+        {
+            var dummyCharacterDetailsProvider =
+                GetComponent<DummyCharacterDetailsProvider>();
+            var parameters =
+                dummyCharacterDetailsProvider.GetDummyCharacterParameters();
+            var gameSceneApi = ServiceProvider.GameService.GetGameSceneApi();
+            gameSceneApi?.SceneEntered.Invoke(parameters);
+        }
+    }
+}
