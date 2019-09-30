e69879fad4f8880e7551b3aec869969f30344565
Ben Ukhanov <ben1zuk321@gmail.com>
2019-09-30 02:11:36 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Actors/DummyCharacterDetailsProvider.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Actors/DummyCharacterDetailsProvider.cs
@@ -1,21 +1,44 @@
 ﻿using Game.Common;
+using Scripts.Editor;
 using UnityEngine;
 
 namespace Scripts.Gameplay.Actors
 {
     public class DummyCharacterDetailsProvider : MonoBehaviour
     {
-        [SerializeField]
-        private DummyCharacter dummyCharacter;
+        [ViewOnly, SerializeField]
+        private int id;
 
-        public EnterSceneResponseParameters GetDummyCharacterParameters()
+        [SerializeField]
+        private string name;
+
+        [SerializeField]
+        private CharacterClasses characterClass;
+
+        [SerializeField]
+        private Vector2 position;
+
+        [SerializeField]
+        private Directions direction;
+
+        public EnterSceneResponseParameters GetDummyCharacter()
         {
-            return DummyCharacter.CreateDummyCharacter(
-                dummyCharacter.Id,
-                dummyCharacter.Name,
-                dummyCharacter.CharacterClass,
-                dummyCharacter.Position,
-                dummyCharacter.Direction);
+            var sceneObject = new SceneObjectParameters(
+                id,
+                "Local Player",
+                position.x,
+                position.y,
+                direction);
+
+            var character = new CharacterSpawnDetailsParameters(
+                id,
+                new CharacterParameters(
+                    name,
+                    characterClass,
+                    CharacterIndex.Zero),
+                direction);
+
+            return new EnterSceneResponseParameters(sceneObject, character);
         }
     }
 }
