d48eefe88df9fdc29a68c0221ef47659041b9f27
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-02 07:34:31 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/Paths.cs
@@ -0,0 +1,12 @@
+﻿namespace Scripts.Constants
+{
+    public static class Paths
+    {
+        public static class Resources
+        {
+            public const string GameObjectsPath = "Game/{0}";
+            public const string ConfigurationsPath = "Configurations/{0}";
+            public const string BubbleMessagePath = "Game/Graphics/BubbleMessage";
+        }
+    }
+}
