1941dd4bcee744356e4af5a4afdaaa9832f432b0
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-02 07:59:04 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Authenticator/Controller/AuthenticationValidator.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Authenticator/Controller/AuthenticationValidator.cs
@@ -10,77 +10,77 @@ namespace Scripts.UI.Authenticator
 
         public bool IsEmptyEmailAddress(string email, out string message)
         {
-            message = WindowMessages.EmptyEmailAddress;
+            message = NoticeMessages.AuthView.EmptyEmailAddress;
 
             return string.IsNullOrWhiteSpace(email);
         }
 
         public bool IsInvalidEmailAddress(string email, out string message)
         {
-            message = WindowMessages.InvalidEmailAddress;
+            message = NoticeMessages.AuthView.InvalidEmailAddress;
 
             return WindowUtils.IsEmailAddressValid(email) == false;
         }
 
         public bool IsEmptyPassword(string password, out string message)
         {
-            message = WindowMessages.EmptyPassword;
+            message = NoticeMessages.AuthView.EmptyPassword;
 
             return string.IsNullOrWhiteSpace(password);
         }
 
         public bool IsPasswordTooShort(string password, out string message)
         {
-            message = WindowMessages.ShortPassword;
+            message = NoticeMessages.AuthView.ShortPassword;
 
             return password.Length <= PasswordLength;
         }
 
         public bool IsEmptyConfirmPassword(string password, out string message)
         {
-            message = WindowMessages.EmptyConfirmPassword;
+            message = NoticeMessages.AuthView.EmptyConfirmPassword;
 
             return string.IsNullOrWhiteSpace(password);
         }
 
         public bool IsConfirmPasswordTooShort(string password, out string message)
         {
-            message = WindowMessages.ShortPassword;
+            message = NoticeMessages.AuthView.ShortPassword;
 
             return password.Length <= PasswordLength;
         }
 
         public bool ArePasswordsDoNotMatch(string password, string confirmPassword, out string message)
         {
-            message = WindowMessages.PasswordsDoNotMatch;
+            message = NoticeMessages.AuthView.PasswordsDoNotMatch;
 
             return password != confirmPassword;
         }
 
         public bool IsFirstNameEmpty(string firstName, out string message)
         {
-            message = WindowMessages.EmptyFirstName;
+            message = NoticeMessages.AuthView.EmptyFirstName;
 
             return string.IsNullOrWhiteSpace(firstName);
         }
 
         public bool IsLastNameEmpty(string lastName, out string message)
         {
-            message = WindowMessages.EmptyLastName;
+            message = NoticeMessages.AuthView.EmptyLastName;
 
             return string.IsNullOrWhiteSpace(lastName);
         }
 
         public bool IsFirstNameTooShort(string firstName, out string message)
         {
-            message = WindowMessages.ShortFirstName;
+            message = NoticeMessages.AuthView.ShortFirstName;
 
             return firstName.Length < FirstNameLength;
         }
 
         public bool IsLastNameTooShort(string lastName, out string message)
         {
-            message = WindowMessages.ShortLastName;
+            message = NoticeMessages.AuthView.ShortLastName;
 
             return lastName.Length < LastNameLength;
         }
