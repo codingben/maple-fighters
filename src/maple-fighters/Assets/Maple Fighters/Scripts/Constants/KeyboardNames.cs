3d9625152d11393844f283f6f87d781ee851cd66
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-02 08:13:56 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/KeyboardNames.cs
@@ -0,0 +1,11 @@
+﻿namespace Scripts.Constants
+{
+    public static class KeyboardNames
+    {
+        public static class Axes
+        {
+            public const string Vertical = "Vertical";
+            public const string Horizontal = "Horizontal";
+        }
+    }
+}
