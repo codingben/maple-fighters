e68c015ea90d0ff6fe5ad5b555449caee83364b1
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-02 07:17:13 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/AnimationNames.cs
@@ -0,0 +1,13 @@
+﻿namespace Scripts.Constants
+{
+    public static class AnimationNames
+    {
+        public static class Player
+        {
+            public const string Walk = "Walking";
+            public const string Jump = "Jump";
+            public const string Rope = "Rope";
+            public const string Ladder = "Ladder";
+        }
+    }
+}
