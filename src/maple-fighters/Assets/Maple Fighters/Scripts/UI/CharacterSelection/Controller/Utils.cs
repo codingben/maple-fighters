5422c875ec9c0241524f3b20b74e52f7dab556ce
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-06 11:09:24 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Controller/Utils.cs
@@ -0,0 +1,18 @@
+﻿namespace Scripts.UI.CharacterSelection
+{
+    public static class Utils
+    {
+        private static string GetCharacterPath(CharacterDetails characterDetails)
+        {
+            var characterIndex = characterDetails.GetCharacterIndex();
+            var characterClass = characterDetails.GetCharacterClass();
+            var hasCharacter = characterDetails.HasCharacter();
+            var name =
+                hasCharacter
+                    ? $"{characterClass} {(int)characterIndex}"
+                    : $"Sample {(int)characterIndex}";
+
+            return $"Characters/{name}";
+        }
+    }
+}
