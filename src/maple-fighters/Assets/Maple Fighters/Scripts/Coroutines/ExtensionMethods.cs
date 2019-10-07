65c5aa319a77f6d7c339884d5c60ef86a70a6a82
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-07 10:22:26 +0200

    :alien: Fixed the errors.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Coroutines/ExtensionMethods.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Coroutines/ExtensionMethods.cs
@@ -4,19 +4,19 @@ namespace Scripts.Coroutines
 {
     public static class ExtensionMethods
     {
-        public static CoroutinesWrappersUpdater CoroutinesWrappersUpdater =>
-            CoroutinesWrappersUpdater.GetOrCreateInstance();
+        public static CoroutineWrapperUpdater CoroutineWrapperUpdater =>
+            CoroutineWrapperUpdater.GetOrCreateInstance();
 
         public static void ExecuteExternally(
             this ExternalCoroutinesExecutor executer)
         {
-            CoroutinesWrappersUpdater?.AddCoroutineExecutor(executer);
+            CoroutineWrapperUpdater?.AddCoroutineExecutor(executer);
         }
 
         public static void RemoveFromExternalExecutor(
             this ExternalCoroutinesExecutor executer)
         {
-            CoroutinesWrappersUpdater?.RemoveCoroutineExecutor(executer);
+            CoroutineWrapperUpdater?.RemoveCoroutineExecutor(executer);
         }
     }
 }
