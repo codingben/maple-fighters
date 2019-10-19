ed333d61e93a34d34e1b7171b15d73c121eabc63
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 18:26:15 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Chat/IChatService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Chat/IChatService.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Scripts.Services.Authorizer;
 
-public class IChatService : MonoBehaviour
+namespace Scripts.Services.Chat
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IChatService
     {
-        
-    }
+        IAuthorizerApi AuthorizerApi { get; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        IChatApi ChatApi { get; }
     }
 }
