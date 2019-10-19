4a1c81e1f7247c8f91c4307736acfb5e63f738c4
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 16:27:56 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorOperations.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorOperations.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AuthenticatorOperations : MonoBehaviour
+﻿namespace Scripts.Services.Authenticator
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum AuthenticatorOperations
     {
-        
-    }
+        /// <summary>
+        /// The authenticate.
+        /// </summary>
+        Authenticate,
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// The register.
+        /// </summary>
+        Register
     }
 }
