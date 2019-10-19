f5b483889b9f55cd0c6e3e86127b1b6957962f5e
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 19:37:04 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameService.cs
@@ -1,18 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using ExitGames.Client.Photon;
+using Network.Scripts;
+using Network.Utils;
+using Scripts.Services.Authorizer;
 
-public class GameService : MonoBehaviour
+namespace Scripts.Services.Game
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameService : Singleton<GameService>, IGameService
     {
-        
-    }
+        public IAuthorizerApi AuthorizerApi
+        {
+            get
+            {
+                if (authorizerApi == null)
+                {
+                    authorizerApi = new DummyAuthorizerApi(serverPeer);
+                }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                return authorizerApi;
+            }
+        }
+
+        public ICharacterSelectorApi CharacterSelectorApi
+        {
+            get
+            {
+                if (characterSelectorApi == null)
+                {
+                    characterSelectorApi =
+                        new DummyCharacterSelectorApi(serverPeer);
+                }
+
+                return characterSelectorApi;
+            }
+        }
+
+        public IGameSceneApi GameSceneApi
+        {
+            get
+            {
+                if (gameSceneApi == null)
+                {
+                    gameSceneApi = new DummyGameSceneApi(serverPeer);
+                }
+
+                return gameSceneApi;
+            }
+        }
+
+        private IAuthorizerApi authorizerApi;
+        private ICharacterSelectorApi characterSelectorApi;
+        private IGameSceneApi gameSceneApi;
+        private IServerPeer serverPeer;
+
+        private ExternalCoroutinesExecutor coroutinesExecutor;
+
+        private void Awake()
+        {
+            coroutinesExecutor = new ExternalCoroutinesExecutor();
+            coroutinesExecutor.StartTask(ConnectAsync);
+        }
+
+        private void Update()
+        {
+            coroutinesExecutor?.Update();
+        }
+
+        private void OnDestroy()
+        {
+            ((IDisposable)authorizerApi)?.Dispose();
+            ((IDisposable)characterSelectorApi)?.Dispose();
+            ((IDisposable)gameSceneApi)?.Dispose();
+            coroutinesExecutor?.Dispose();
+        }
+
+        private async Task ConnectAsync(IYield yield)
+        {
+            var serverConnector = new DummyServerConnector();
+            var connectionInfo = new PeerConnectionInformation();
+            var connectionProtocol = ConnectionProtocol.Tcp;
+
+            serverPeer =
+                await serverConnector.Connect(
+                    yield,
+                    connectionInfo,
+                    connectionProtocol);
+        }
     }
 }
