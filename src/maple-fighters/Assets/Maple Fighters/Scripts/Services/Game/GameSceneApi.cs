ecc0483bfc92dff84e8dd235dd8ea205446033cd
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 18:59:15 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameSceneApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameSceneApi.cs
@@ -1,18 +1,131 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using CommunicationHelper;
+using Game.Common;
+using Network.Scripts;
 
-public class GameSceneApi : MonoBehaviour
+namespace Scripts.Services.Game
 {
-    // Start is called before the first frame update
-    void Start()
+    internal class GameSceneApi : NetworkApi<GameOperations, GameEvents>, IGameSceneApi
     {
-        
-    }
+        public UnityEvent<EnterSceneResponseParameters> SceneEntered { get; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public UnityEvent<SceneObjectAddedEventParameters> SceneObjectAdded { get; }
+
+        public UnityEvent<SceneObjectRemovedEventParameters> SceneObjectRemoved { get; }
+
+        public UnityEvent<SceneObjectsAddedEventParameters> SceneObjectsAdded { get; }
+
+        public UnityEvent<SceneObjectsRemovedEventParameters> SceneObjectsRemoved { get; }
+
+        public UnityEvent<SceneObjectPositionChangedEventParameters> PositionChanged { get; }
+
+        public UnityEvent<PlayerStateChangedEventParameters> PlayerStateChanged { get; }
+
+        public UnityEvent<PlayerAttackedEventParameters> PlayerAttacked { get; }
+
+        public UnityEvent<CharacterAddedEventParameters> CharacterAdded { get; }
+
+        public UnityEvent<CharactersAddedEventParameters> CharactersAdded { get; }
+
+        public UnityEvent<BubbleMessageEventParameters> BubbleMessageReceived { get; }
+
+        internal GameSceneApi(IServerPeer serverPeer, bool log = false)
+            : base(serverPeer, log)
+        {
+            SceneEntered = new UnityEvent<EnterSceneResponseParameters>();
+            SceneObjectAdded = new UnityEvent<SceneObjectAddedEventParameters>();
+            SceneObjectRemoved = new UnityEvent<SceneObjectRemovedEventParameters>();
+            SceneObjectsAdded = new UnityEvent<SceneObjectsAddedEventParameters>();
+            SceneObjectsRemoved = new UnityEvent<SceneObjectsRemovedEventParameters>();
+            PositionChanged = new UnityEvent<SceneObjectPositionChangedEventParameters>();
+            PlayerStateChanged = new UnityEvent<PlayerStateChangedEventParameters>();
+            PlayerAttacked = new UnityEvent<PlayerAttackedEventParameters>();
+            CharacterAdded = new UnityEvent<CharacterAddedEventParameters>();
+            CharactersAdded = new UnityEvent<CharactersAddedEventParameters>();
+            BubbleMessageReceived = new UnityEvent<BubbleMessageEventParameters>();
+
+            EventHandlerRegister.SetHandler(GameEvents.SceneObjectAdded, SceneObjectAdded.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.SceneObjectRemoved, SceneObjectRemoved.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.SceneObjectsAdded, SceneObjectsAdded.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.SceneObjectsRemoved, SceneObjectsRemoved.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.PositionChanged, PositionChanged.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.PlayerStateChanged, PlayerStateChanged.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.PlayerAttacked, PlayerAttacked.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.CharacterAdded, CharacterAdded.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.CharactersAdded, CharactersAdded.ToEventHandler());
+            EventHandlerRegister.SetHandler(GameEvents.BubbleMessage, BubbleMessageReceived.ToEventHandler());
+        }
+
+        public new void Dispose()
+        {
+            base.Dispose();
+
+            EventHandlerRegister.RemoveHandler(GameEvents.SceneObjectAdded);
+            EventHandlerRegister.RemoveHandler(GameEvents.SceneObjectRemoved);
+            EventHandlerRegister.RemoveHandler(GameEvents.SceneObjectsAdded);
+            EventHandlerRegister.RemoveHandler(GameEvents.SceneObjectsRemoved);
+            EventHandlerRegister.RemoveHandler(GameEvents.PositionChanged);
+            EventHandlerRegister.RemoveHandler(GameEvents.PlayerStateChanged);
+            EventHandlerRegister.RemoveHandler(GameEvents.PlayerAttacked);
+            EventHandlerRegister.RemoveHandler(GameEvents.CharacterAdded);
+            EventHandlerRegister.RemoveHandler(GameEvents.CharactersAdded);
+            EventHandlerRegister.RemoveHandler(GameEvents.BubbleMessage);
+        }
+
+        public async Task EnterSceneAsync(IYield yield)
+        {
+            var id =
+                OperationRequestSender.Send(
+                    GameOperations.EnterScene,
+                    new EmptyParameters(),
+                    MessageSendOptions.DefaultReliable());
+
+            var responseParameters =
+                await SubscriptionProvider
+                    .ProvideSubscription<EnterSceneResponseParameters>(
+                        yield,
+                        id);
+
+            SceneEntered?.Invoke(responseParameters);
+        }
+
+        public async Task<ChangeSceneResponseParameters> ChangeSceneAsync(
+            IYield yield,
+            ChangeSceneRequestParameters parameters)
+        {
+            var id =
+                OperationRequestSender.Send(
+                    GameOperations.ChangeScene,
+                    parameters,
+                    MessageSendOptions.DefaultReliable());
+
+            return
+                await SubscriptionProvider
+                    .ProvideSubscription<ChangeSceneResponseParameters>(
+                        yield,
+                        id);
+        }
+
+        public Task UpdatePosition(UpdatePositionRequestParameters parameters)
+        {
+            OperationRequestSender.Send(
+                GameOperations.PositionChanged,
+                parameters,
+                MessageSendOptions.DefaultUnreliable((byte)GameDataChannels.Position));
+
+            return Task.CompletedTask;
+        }
+
+        public Task UpdatePlayerState(UpdatePlayerStateRequestParameters parameters)
+        {
+            OperationRequestSender.Send(
+                GameOperations.PlayerStateChanged,
+                parameters,
+                MessageSendOptions.DefaultUnreliable((byte)GameDataChannels.Animations));
+
+            return Task.CompletedTask;
+        }
     }
 }
