3ae4c3136908b05b9ad7098dd0d1ae6c028beef2
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 19:55:26 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Utils/GameNetworkTrafficStateSetter.cs
@@ -0,0 +1,18 @@
+﻿using CommonCommunicationInterfaces;
+using UnityEngine;
+
+namespace Scripts.Services.Utils
+{
+    public class GameNetworkTrafficStateSetter : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // TODO: Implement
+        }
+
+        private void OnDestroy()
+        {
+            // TODO: Implement
+        }
+    }
+}
