3ab2dc6c99d17eca748d4671557bb97f26965941
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 19:48:25 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/GameServerProviderService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/GameServerProviderService.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using ExitGames.Client.Photon;
+using Network.Scripts;
+using Network.Utils;
+using Scripts.Services.Authorizer;
 
-public class GameServerProviderService : MonoBehaviour
+namespace Scripts.Services.GameServerProvider
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameServerProviderService : Singleton<GameServerProviderService>, IGameServerProviderService
     {
-        
-    }
+        public IAuthorizerApi AuthorizerApi
+        {
+            get
+            {
+                if (authorizerApi == null)
+                {
+                    authorizerApi = new DummyAuthorizerApi(serverPeer);
+                }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                return authorizerApi;
+            }
+        }
+
+        public IGameServerProviderApi GameServerProviderApi
+        {
+            get
+            {
+                if (gameServerProviderApi == null)
+                {
+                    gameServerProviderApi = new DummyGameServerProviderApi(serverPeer);
+                }
+
+                return gameServerProviderApi;
+            }
+        }
+
+        private IAuthorizerApi authorizerApi;
+        private IGameServerProviderApi gameServerProviderApi;
+        private IServerPeer serverPeer;
+
+        private ExternalCoroutinesExecutor coroutinesExecutor;
+
+        private void Awake()
+        {
+            coroutinesExecutor = new ExternalCoroutinesExecutor();
+            coroutinesExecutor.StartTask(ConnectAsync);
+        }
+
+        private void Update()
+        {
+            coroutinesExecutor?.Update();
+        }
+
+        private void OnDestroy()
+        {
+            ((IDisposable)authorizerApi)?.Dispose();
+            ((IDisposable)gameServerProviderApi)?.Dispose();
+            coroutinesExecutor?.Dispose();
+        }
+
+        private async Task ConnectAsync(IYield yield)
+        {
+            var serverConnector = new DummyServerConnector();
+            var connectionInfo = new PeerConnectionInformation();
+            var connectionProtocol = ConnectionProtocol.Tcp;
+
+            serverPeer =
+                await serverConnector.Connect(
+                    yield,
+                    connectionInfo,
+                    connectionProtocol);
+        }
     }
 }
