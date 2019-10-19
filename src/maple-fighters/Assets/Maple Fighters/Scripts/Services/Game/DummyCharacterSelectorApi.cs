af3652d1ff4ce67b5626b28d4c9b5d2b131191ef
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 19:31:12 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/DummyCharacterSelectorApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/DummyCharacterSelectorApi.cs
@@ -1,18 +1,111 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using CommunicationHelper;
+using Game.Common;
+using Network.Scripts;
 
-public class DummyCharacterSelectorApi : MonoBehaviour
+namespace Scripts.Services.Game
 {
-    // Start is called before the first frame update
-    void Start()
+    internal class DummyCharacterSelectorApi : NetworkApi<CharacterOperations, EmptyEventCode>, ICharacterSelectorApi
     {
-        
-    }
+        private readonly Dictionary<CharacterIndex, CharacterParameters> characters;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        internal DummyCharacterSelectorApi(IServerPeer serverPeer, bool log = false)
+            : base(serverPeer, log)
+        {
+            characters = new Dictionary<CharacterIndex, CharacterParameters>
+            {
+                {
+                    CharacterIndex.First,
+                    new CharacterParameters
+                    {
+                        Name = "Dummy",
+                        HasCharacter = false,
+                        Index = CharacterIndex.First
+                    }
+                },
+                {
+                    CharacterIndex.Second,
+                    new CharacterParameters
+                    {
+                        Name = "benzuk",
+                        HasCharacter = true,
+                        Index = CharacterIndex.Second,
+                        CharacterType = CharacterClasses.Arrow
+                    }
+                },
+                {
+                    CharacterIndex.Third,
+                    new CharacterParameters
+                    {
+                        Name = "Dummy",
+                        HasCharacter = false,
+                        Index = CharacterIndex.Third
+                    }
+                }
+            };
+        }
+
+        public async Task<CreateCharacterResponseParameters> CreateCharacterAsync(
+            IYield yield,
+            CreateCharacterRequestParameters parameters)
+        {
+            var index = parameters.Index;
+            var name = parameters.Name;
+            var characterClass = parameters.CharacterClass;
+
+            characters[index] =
+                new CharacterParameters(name, characterClass, index)
+                {
+                    HasCharacter = true
+                };
+
+            return 
+                await Task.FromResult(
+                    new CreateCharacterResponseParameters(
+                        CharacterCreationStatus.Succeed));
+        }
+
+        public async Task<RemoveCharacterResponseParameters> RemoveCharacterAsync(
+            IYield yield,
+            RemoveCharacterRequestParameters parameters)
+        {
+            var index = parameters.CharacterIndex;
+            var characterIndex = (CharacterIndex)index;
+
+            characters[characterIndex] = new CharacterParameters
+            {
+                Name = $"Dummy {index}",
+                HasCharacter = false,
+                Index = characterIndex
+            };
+
+            return 
+                await Task.FromResult(
+                    new RemoveCharacterResponseParameters(
+                        RemoveCharacterStatus.Succeed));
+        }
+
+        public async Task<ValidateCharacterResponseParameters> ValidateCharacterAsync(
+            IYield yield,
+            ValidateCharacterRequestParameters parameters)
+        {
+            return
+                await Task.FromResult(
+                    new ValidateCharacterResponseParameters(
+                        CharacterValidationStatus.Ok,
+                        Maps.Map_1));
+        }
+
+        public async Task<GetCharactersResponseParameters> GetCharactersAsync(IYield yield)
+        {
+            return 
+                await Task.FromResult(
+                    new GetCharactersResponseParameters(
+                        characters.Values.ToArray()));
+        }
     }
 }
