1fcddec716a5c005fcc2dc991a6d5ebaf3fd817f
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 18:08:44 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authorizer/AuthorizerApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authorizer/AuthorizerApi.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Threading.Tasks;
+using Authorization.Client.Common;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using CommunicationHelper;
+using Network.Scripts;
 
-public class AuthorizerApi : MonoBehaviour
+namespace Scripts.Services.Authorizer
 {
-    // Start is called before the first frame update
-    void Start()
+    internal class AuthorizerApi : NetworkApi<AuthorizationOperations, EmptyEventCode>, IAuthorizerApi
     {
-        
-    }
+        internal AuthorizerApi(IServerPeer serverPeer, bool log = false)
+            : base(serverPeer, log)
+        {
+            // Left blank intentionally
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public async Task<AuthorizeResponseParameters> AuthorizeAsync(
+            IYield yield,
+            AuthorizeRequestParameters parameters)
+        {
+            var id =
+                OperationRequestSender.Send(
+                    AuthorizationOperations.Authorize,
+                    parameters,
+                    MessageSendOptions.DefaultReliable());
+
+            return
+                await SubscriptionProvider
+                    .ProvideSubscription<AuthorizeResponseParameters>(
+                        yield,
+                        id);
+        }
     }
 }
