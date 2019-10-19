0b2c7d2f947d4f946d5826e3c2f4d99b4284830e
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 18:39:18 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Chat/ChatService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Chat/ChatService.cs
@@ -1,18 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using ExitGames.Client.Photon;
+using Network.Scripts;
+using Network.Utils;
+using Scripts.Services.Authorizer;
 
-public class ChatService : MonoBehaviour
+namespace Scripts.Services.Chat
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ChatService : Singleton<ChatService>, IChatService
     {
-        
-    }
+        public IAuthorizerApi AuthorizerApi
+        {
+            get
+            {
+                if (authorizerApi == null)
+                {
+                    authorizerApi = new DummyAuthorizerApi(serverPeer);
+                }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                return authorizerApi;
+            }
+        }
+
+        public IChatApi ChatApi
+        {
+            get
+            {
+                if (chatApi == null)
+                {
+                    chatApi = new DummyChatApi(serverPeer);
+                }
+
+                return chatApi;
+            }
+        }
+
+        private IAuthorizerApi authorizerApi;
+        private IChatApi chatApi;
+        private IServerPeer serverPeer;
+
+        private ExternalCoroutinesExecutor coroutinesExecutor;
+
+        private void Awake()
+        {
+            coroutinesExecutor = new ExternalCoroutinesExecutor();
+            coroutinesExecutor.StartTask(ConnectAsync);
+        }
+
+        private void Update()
+        {
+            coroutinesExecutor?.Update();
+        }
+
+        private void OnDestroy()
+        {
+            ((IDisposable)chatApi)?.Dispose();
+            coroutinesExecutor?.Dispose();
+        }
+
+        private async Task ConnectAsync(IYield yield)
+        {
+            var serverConnector = new DummyServerConnector();
+            var connectionInfo = new PeerConnectionInformation();
+            var connectionProtocol = ConnectionProtocol.Tcp;
+
+            serverPeer =
+                await serverConnector.Connect(
+                    yield,
+                    connectionInfo,
+                    connectionProtocol);
+        }
     }
 }
