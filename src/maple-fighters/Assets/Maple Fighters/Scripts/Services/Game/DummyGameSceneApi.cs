3cf8399a49a2110b19ed6fb2730ee99b37d3bb49
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 19:20:20 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/DummyGameSceneApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/DummyGameSceneApi.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using Game.Common;
+using Network.Scripts;
+using Scripts.World.Dummy;
 
-public class DummyGameSceneApi : MonoBehaviour
+namespace Scripts.Services.Game
 {
-    // Start is called before the first frame update
-    void Start()
+    internal class DummyGameSceneApi : NetworkApi<GameOperations, GameEvents>, IGameSceneApi
     {
-        
-    }
+        public UnityEvent<EnterSceneResponseParameters> SceneEntered { get; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public UnityEvent<SceneObjectAddedEventParameters> SceneObjectAdded { get; }
+
+        public UnityEvent<SceneObjectRemovedEventParameters> SceneObjectRemoved { get; }
+
+        public UnityEvent<SceneObjectsAddedEventParameters> SceneObjectsAdded { get; }
+
+        public UnityEvent<SceneObjectsRemovedEventParameters> SceneObjectsRemoved { get; }
+
+        public UnityEvent<SceneObjectPositionChangedEventParameters> PositionChanged { get; }
+
+        public UnityEvent<PlayerStateChangedEventParameters> PlayerStateChanged { get; }
+
+        public UnityEvent<PlayerAttackedEventParameters> PlayerAttacked { get; }
+
+        public UnityEvent<CharacterAddedEventParameters> CharacterAdded { get; }
+
+        public UnityEvent<CharactersAddedEventParameters> CharactersAdded { get; }
+
+        public UnityEvent<BubbleMessageEventParameters> BubbleMessageReceived { get; }
+
+        internal DummyGameSceneApi(IServerPeer serverPeer, bool log = false)
+            : base(serverPeer, log)
+        {
+            SceneEntered = new UnityEvent<EnterSceneResponseParameters>();
+            SceneObjectAdded = new UnityEvent<SceneObjectAddedEventParameters>();
+            SceneObjectRemoved = new UnityEvent<SceneObjectRemovedEventParameters>();
+            SceneObjectsAdded = new UnityEvent<SceneObjectsAddedEventParameters>();
+            SceneObjectsRemoved = new UnityEvent<SceneObjectsRemovedEventParameters>();
+            PositionChanged = new UnityEvent<SceneObjectPositionChangedEventParameters>();
+            PlayerStateChanged = new UnityEvent<PlayerStateChangedEventParameters>();
+            PlayerAttacked = new UnityEvent<PlayerAttackedEventParameters>();
+            CharacterAdded = new UnityEvent<CharacterAddedEventParameters>();
+            CharactersAdded = new UnityEvent<CharactersAddedEventParameters>();
+            BubbleMessageReceived = new UnityEvent<BubbleMessageEventParameters>();
+        }
+
+        public async Task EnterSceneAsync(IYield yield)
+        {
+            await Task.Delay(1);
+        }
+
+        public async Task<ChangeSceneResponseParameters> ChangeSceneAsync(
+            IYield yield,
+            ChangeSceneRequestParameters parameters)
+        {
+            var map =
+                DummyPortalContainer.GetInstance().GetMap(parameters.PortalId);
+
+            return 
+                await Task.FromResult(new ChangeSceneResponseParameters(map));
+        }
+
+        public void UpdatePosition(UpdatePositionRequestParameters parameters)
+        {
+            // Left blank intentionally
+        }
+
+        public void UpdatePlayerState(
+            UpdatePlayerStateRequestParameters parameters)
+        {
+            // Left blank intentionally
+        }
     }
 }
