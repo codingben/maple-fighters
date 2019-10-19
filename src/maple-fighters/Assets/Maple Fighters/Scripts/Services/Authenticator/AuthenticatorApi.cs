1b8b458479430c72fa2dc0b74ae22c256891f1be
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 16:37:05 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorApi.cs
@@ -1,15 +1,61 @@
-﻿using CommonCommunicationInterfaces;
+﻿using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
 using CommunicationHelper;
+using Login.Common;
 using Network.Scripts;
+using Registration.Common;
 
 namespace Scripts.Services.Authenticator
 {
-    public class AuthenticatorApi : NetworkApi<AuthenticatorOperations, EmptyEventCode>
+    internal class AuthenticatorApi : NetworkApi<AuthenticatorOperations, EmptyEventCode>, IAuthenticatorApi
     {
         protected AuthenticatorApi(IServerPeer serverPeer, bool log = false)
             : base(serverPeer, log)
         {
             // Left blank intentionally
+        }
+
+        public async Task<AuthenticateResponseParameters> AuthenticateAsync(
+            IYield yield,
+            AuthenticateRequestParameters parameters)
+        {
+            var id =
+                OperationRequestSender.Send(
+                    AuthenticatorOperations.Authenticate,
+                    parameters,
+                    MessageSendOptions.DefaultReliable());
+
+            var responseParameters =
+                await SubscriptionProvider
+                    .ProvideSubscription<AuthenticateResponseParameters>(
+                        yield,
+                        id);
+
+            if (responseParameters.HasAccessToken)
+            {
+                // TODO: Use the access token
+            }
+
+            return new AuthenticateResponseParameters(
+                responseParameters.Status);
+        }
+
+        public async Task<RegisterResponseParameters> RegisterAsync(
+            IYield yield,
+            RegisterRequestParameters parameters)
+        {
+            var id =
+                OperationRequestSender.Send(
+                    AuthenticatorOperations.Register,
+                    parameters,
+                    MessageSendOptions.DefaultReliable());
+
+            return
+                await SubscriptionProvider
+                    .ProvideSubscription<RegisterResponseParameters>(
+                        yield,
+                        id);
         }
     }
 }
