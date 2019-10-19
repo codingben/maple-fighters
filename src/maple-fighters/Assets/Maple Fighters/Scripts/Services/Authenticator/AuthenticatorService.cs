b9a99b5a1aff94d5cfb72f377e3834bb0908c035
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 16:52:53 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using ExitGames.Client.Photon;
+using Network.Scripts;
+using Network.Utils;
 
-public class AuthenticatorService : MonoBehaviour
+namespace Scripts.Services.Authenticator
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AuthenticatorService : Singleton<AuthenticatorService>
     {
-        
-    }
+        public IAuthenticatorApi AuthenticatorApi
+        {
+            get
+            {
+                if (authenticatorApi == null)
+                {
+                    authenticatorApi = new DummyAuthenticatorApi(serverPeer);
+                }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                return authenticatorApi;
+            }
+        }
+
+        private IAuthenticatorApi authenticatorApi;
+        private IServerPeer serverPeer;
+
+        private ExternalCoroutinesExecutor coroutinesExecutor;
+
+        private void Awake()
+        {
+            coroutinesExecutor = new ExternalCoroutinesExecutor();
+            coroutinesExecutor.StartTask(ConnectAsync);
+        }
+
+        private void Update()
+        {
+            coroutinesExecutor?.Update();
+        }
+
+        private void OnDestroy()
+        {
+            ((IDisposable)authenticatorApi)?.Dispose();
+            coroutinesExecutor?.Dispose();
+        }
+
+        private async Task ConnectAsync(IYield yield)
+        {
+            var serverConnector = new DummyServerConnector();
+            var connectionInfo = new PeerConnectionInformation();
+            var connectionProtocol = ConnectionProtocol.Tcp;
+
+            serverPeer = await serverConnector.Connect(yield, connectionInfo, connectionProtocol);
+        }
     }
 }
