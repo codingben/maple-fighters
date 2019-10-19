8701ef5c48f7e7a071eefdf0a5f70dab5ea46d83
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-19 19:43:54 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/DummyGameServerProviderApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/DummyGameServerProviderApi.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Threading.Tasks;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using CommunicationHelper;
+using GameServerProvider.Client.Common;
+using Network.Scripts;
 
-public class DummyGameServerProviderApi : MonoBehaviour
+namespace Scripts.Services.GameServerProvider
 {
-    // Start is called before the first frame update
-    void Start()
+    internal class DummyGameServerProviderApi : NetworkApi<GameServerProviderOperations, EmptyEventCode>, IGameServerProviderApi
     {
-        
-    }
+        internal DummyGameServerProviderApi(IServerPeer serverPeer, bool log = false)
+            : base(serverPeer, log)
+        {
+            // Left blank intentionally
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public async Task<GameServersProviderResponseParameters> ProvideGameServersAsync(
+            IYield yield)
+        {
+            var gameServers = new[]
+            {
+                new GameServerInformationParameters(
+                    name: "Game 1",
+                    ip: "127.0.0.1",
+                    port: 8000,
+                    connections: 10,
+                    maxConnections: 100)
+            };
+
+            return 
+                await Task.FromResult(
+                    new GameServersProviderResponseParameters(gameServers));
+        }
     }
 }
