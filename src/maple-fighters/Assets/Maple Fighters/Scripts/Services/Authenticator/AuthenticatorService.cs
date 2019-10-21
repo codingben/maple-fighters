4d880e9f8c7c8a595a4701357895c9f5a353ad9c
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-21 21:16:22 +0200

    :alien: Initialized the api.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using Network.Scripts;
 using Network.Utils;
+using Scripts.ScriptableObjects;
 
 namespace Scripts.Services.Authenticator
 {
     public class AuthenticatorService : Singleton<AuthenticatorService>, IAuthenticatorService
     {
         public IAuthenticatorApi AuthenticatorApi { get; set; }
+
+        private void Awake()
+        {
+            var gameConfiguration = GameConfiguration.GetInstance();
+            if (gameConfiguration != null)
+            {
+                if (gameConfiguration.Environment == HostingEnvironment.Development)
+                {
+                    AuthenticatorApi =
+                        new DummyAuthenticatorApi(new DummyPeer());
+                }
+            }
+        }
 
         private void OnDestroy()
         {
