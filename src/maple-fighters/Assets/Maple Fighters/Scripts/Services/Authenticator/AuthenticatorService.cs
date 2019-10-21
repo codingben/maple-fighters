b91fd7fd3c59d57136f118d105cddaa8fec27c58
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-21 21:01:33 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
@@ -1,61 +1,15 @@
 ﻿using System;
-using System.Threading.Tasks;
-using CommonCommunicationInterfaces;
-using CommonTools.Coroutines;
-using ExitGames.Client.Photon;
-using Network.Scripts;
 using Network.Utils;
 
 namespace Scripts.Services.Authenticator
 {
     public class AuthenticatorService : Singleton<AuthenticatorService>, IAuthenticatorService
     {
-        public IAuthenticatorApi AuthenticatorApi
-        {
-            get
-            {
-                if (authenticatorApi == null)
-                {
-                    authenticatorApi = new DummyAuthenticatorApi(serverPeer);
-                }
-
-                return authenticatorApi;
-            }
-        }
-
-        private IAuthenticatorApi authenticatorApi;
-        private IServerPeer serverPeer;
-
-        private ExternalCoroutinesExecutor coroutinesExecutor;
-
-        private void Awake()
-        {
-            coroutinesExecutor = new ExternalCoroutinesExecutor();
-            coroutinesExecutor.StartTask(ConnectAsync);
-        }
-
-        private void Update()
-        {
-            coroutinesExecutor?.Update();
-        }
+        public IAuthenticatorApi AuthenticatorApi { get; set; }
 
         private void OnDestroy()
         {
-            ((IDisposable)authenticatorApi)?.Dispose();
-            coroutinesExecutor?.Dispose();
-        }
-
-        private async Task ConnectAsync(IYield yield)
-        {
-            var serverConnector = new DummyServerConnector();
-            var connectionInfo = new PeerConnectionInformation();
-            var connectionProtocol = ConnectionProtocol.Tcp;
-
-            serverPeer = 
-                await serverConnector.Connect(
-                    yield,
-                    connectionInfo,
-                    connectionProtocol);
+            (AuthenticatorApi as IDisposable)?.Dispose();
         }
     }
 }
