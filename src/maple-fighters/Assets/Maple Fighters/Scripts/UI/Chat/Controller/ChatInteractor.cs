d202e89bbf49241b16f7a496e6018839edbfb705
Ben Ukhanov <ben1zuk321@gmail.com>
2019-10-30 20:23:50 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/Chat/Controller/ChatInteractor.cs
@@ -0,0 +1,42 @@
+﻿using Chat.Common;
+using CommonTools.Coroutines;
+using Scripts.Services.Chat;
+using UnityEngine;
+
+namespace Scripts.UI.Chat
+{
+    public class ChatInteractor : MonoBehaviour
+    {
+        private IChatService chatService;
+
+        private ExternalCoroutinesExecutor coroutinesExecutor;
+
+        private void Awake()
+        {
+            chatService = ChatService.GetInstance();
+
+            coroutinesExecutor = new ExternalCoroutinesExecutor();
+        }
+
+        private void Update()
+        {
+            coroutinesExecutor?.Update();
+        }
+
+        private void OnDestroy()
+        {
+            coroutinesExecutor?.Dispose();
+        }
+
+        public void SendChatMessage(string message)
+        {
+            var chatApi = chatService?.ChatApi;
+            if (chatApi != null)
+            {
+                var parameters = new ChatMessageRequestParameters(message);
+
+                chatApi.SendChatMessage(parameters);
+            }
+        }
+    }
+}
