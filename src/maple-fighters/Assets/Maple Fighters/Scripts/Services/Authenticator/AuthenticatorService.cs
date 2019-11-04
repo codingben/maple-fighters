73fde97559af4c899a11981a456d48e910dabffb
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-04 23:20:49 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorService.cs
@@ -11,10 +11,10 @@ namespace Scripts.Services.Authenticator
 
         private void Awake()
         {
-            var gameConfiguration = GameConfiguration.GetInstance();
-            if (gameConfiguration != null)
+            var networkConfiguration = NetworkConfiguration.GetInstance();
+            if (networkConfiguration != null)
             {
-                switch (gameConfiguration.Environment)
+                switch (networkConfiguration.Environment)
                 {
                     case HostingEnvironment.Production:
                     {
