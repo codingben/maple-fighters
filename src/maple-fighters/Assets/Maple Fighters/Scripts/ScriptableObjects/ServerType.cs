7302d413df0064530899094875929de8bea25e24
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-04 23:32:32 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/ScriptableObjects/ServerType.cs
@@ -0,0 +1,25 @@
+﻿namespace Scripts.ScriptableObjects
+{
+    public enum ServerType
+    {
+        /// <summary>
+        /// The authenticator.
+        /// </summary>
+        Authenticator,
+
+        /// <summary>
+        /// The game server provider.
+        /// </summary>
+        GameServerProvider,
+
+        /// <summary>
+        /// The game.
+        /// </summary>
+        Game,
+
+        /// <summary>
+        /// The chat.
+        /// </summary>
+        Chat
+    }
+}
