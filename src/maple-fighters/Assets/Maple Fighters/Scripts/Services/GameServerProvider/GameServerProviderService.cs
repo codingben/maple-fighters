6a442efa9ccf8928af25f3cab7ad0cb20c96ae82
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-10 15:59:56 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/GameServerProviderService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/GameServerProviderService.cs
@@ -1,45 +1,101 @@
 ﻿using System;
+using ClientCommunicationInterfaces;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using ExitGames.Client.Photon;
 using Network.Scripts;
-using Network.Utils;
+using PhotonClientImplementation;
 using ScriptableObjects.Configurations;
 using Scripts.Services.Authorizer;
 
 namespace Scripts.Services.GameServerProvider
 {
-    public class GameServerProviderService : Singleton<GameServerProviderService>, IGameServerProviderService
+    public class GameServerProviderService : NetworkService
     {
         public IAuthorizerApi AuthorizerApi { get; set; }
 
         public IGameServerProviderApi GameServerProviderApi { get; set; }
 
+        private IServerPeer gameServerProviderPeer;
+
+        private ExternalCoroutinesExecutor coroutinesExecutor;
+
         private void Awake()
         {
-            var networkConfiguration = NetworkConfiguration.GetInstance();
-            if (networkConfiguration != null)
-            {
-                switch (networkConfiguration.Environment)
-                {
-                    case HostingEnvironment.Production:
-                    {
-                        break;
-                    }
+            coroutinesExecutor = new ExternalCoroutinesExecutor();
 
-                    case HostingEnvironment.Development:
-                    {
-                        var dummyPeer = new DummyPeer();
+            // TODO: Remove
+            coroutinesExecutor.StartTask(ConnectAsync);
+        }
 
-                        AuthorizerApi = new DummyAuthorizerApi(dummyPeer);
-                        GameServerProviderApi = new DummyGameServerProviderApi(dummyPeer);
-                        break;
-                    }
-                }
-            }
+        private void Update()
+        {
+            coroutinesExecutor?.Update();
+        }
+
+        private void OnDisable()
+        {
+            gameServerProviderPeer?.Disconnect();
         }
 
         private void OnDestroy()
         {
-            (AuthorizerApi as IDisposable)?.Dispose();
-            (GameServerProviderApi as IDisposable)?.Dispose();
+            ((IDisposable)AuthorizerApi)?.Dispose();
+            ((IDisposable)GameServerProviderApi)?.Dispose();
+
+            coroutinesExecutor?.Dispose();
+        }
+
+        protected override void OnConnected(IServerPeer serverPeer)
+        {
+            gameServerProviderPeer = serverPeer;
+
+            var isDummy = NetworkConfiguration.GetInstance().IsDummy();
+            if (isDummy)
+            {
+                AuthorizerApi = new DummyAuthorizerApi(serverPeer);
+                GameServerProviderApi = new DummyGameServerProviderApi(serverPeer);
+            }
+            else
+            {
+                AuthorizerApi = new AuthorizerApi(serverPeer);
+                GameServerProviderApi = new GameServerProviderApi(serverPeer);
+            }
+        }
+
+        protected override IServerConnector GetServerConnector()
+        {
+            IServerConnector serverConnector;
+
+            var isDummy = NetworkConfiguration.GetInstance().IsDummy();
+            if (isDummy)
+            {
+                serverConnector = new DummyServerConnector();
+            }
+            else
+            {
+                serverConnector =
+                    new PhotonServerConnector(() => coroutinesExecutor);
+            }
+
+            return serverConnector;
+        }
+
+        protected override PeerConnectionInformation GetConnectionInfo()
+        {
+            var serverInfo = NetworkConfiguration.GetInstance().GetServerInfo(ServerType.GameServerProvider);
+            var ip = serverInfo.IpAddress;
+            var port = serverInfo.Port;
+
+            return new PeerConnectionInformation(ip, port);
+        }
+
+        protected override ConnectionProtocol GetConnectionProtocol()
+        {
+            var serverInfo = NetworkConfiguration.GetInstance().GetServerInfo(ServerType.GameServerProvider);
+            var protocol = serverInfo.Protocol;
+
+            return protocol;
         }
     }
 }
