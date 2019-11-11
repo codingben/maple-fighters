0fcc0ebb24636a98434bcdb152ec3a430da377d4
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-11 22:19:04 +0200

    :alien: A new script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/GameServerBrowser/Controller/GameServerBrowserController.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/GameServerBrowser/Controller/GameServerBrowserController.cs
@@ -6,26 +6,6 @@ using UnityEngine.SceneManagement;
 
 namespace Scripts.UI.GameServerBrowser
 {
-    public struct GameServerViewCollection
-    {
-        private readonly IGameServerView[] collection;
-
-        public GameServerViewCollection(int length)
-        {
-            collection = new IGameServerView[length];
-        }
-
-        public void Set(int index, IGameServerView gameServerView)
-        {
-            collection[index] = gameServerView;
-        }
-
-        public IEnumerable<IGameServerView> GetAll()
-        {
-            return collection;
-        }
-    }
-
     [RequireComponent(typeof(GameServerBrowserInteractor))]
     public class GameServerBrowserController : MonoBehaviour,
                                                IOnGameServerReceivedListener
---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/GameServerBrowser/Controller/GameServerViewCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Scripts.UI.GameServerBrowser
+{
+    public struct GameServerViewCollection
+    {
+        private readonly IGameServerView[] collection;
+
+        public GameServerViewCollection(int length)
+        {
+            collection = new IGameServerView[length];
+        }
+
+        public void Set(int index, IGameServerView gameServerView)
+        {
+            collection[index] = gameServerView;
+        }
+
+        public IEnumerable<IGameServerView> GetAll()
+        {
+            return collection;
+        }
+    }
+}
