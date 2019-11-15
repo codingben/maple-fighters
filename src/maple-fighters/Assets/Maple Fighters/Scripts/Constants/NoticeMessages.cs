2e328257db01ca230d5616e2a080de994b8cee11
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-15 22:26:35 +0200

    :alien: Added new message.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/NoticeMessages.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/NoticeMessages.cs
@@ -19,6 +19,7 @@
             public const string UnknownError = "An unknown error has occurred.";
             public const string EmailAddressExists = "The email address is already in use.";
             public const string NonAuthorized = "The user is not authorized to log in.";
+            public const string CouldNotConnect = "Failed to connect to authenticator server.";
         }
 
         public static class CharacterView
