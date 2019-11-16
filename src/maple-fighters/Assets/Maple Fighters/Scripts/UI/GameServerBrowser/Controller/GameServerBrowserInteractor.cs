bdc95d5e5cdb0785b88180c95481c2ae5518b19c
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-16 13:02:55 +0200

    :alien: Connecting to the game server provider.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/GameServerBrowser/Controller/GameServerBrowserInteractor.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/GameServerBrowser/Controller/GameServerBrowserInteractor.cs
@@ -1,16 +1,19 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using CommonTools.Coroutines;
+using Network.Scripts;
 using Scripts.Services.GameServerProvider;
 using UnityEngine;
 
 namespace Scripts.UI.GameServerBrowser
 {
+    [RequireComponent(typeof(IOnConnectionFinishedListener))]
     [RequireComponent(typeof(IOnGameServerReceivedListener))]
     public class GameServerBrowserInteractor : MonoBehaviour
     {
         private GameServerProviderService gameServerProviderService;
 
+        private IOnConnectionFinishedListener onConnectionFinishedListener;
         private IOnGameServerReceivedListener onGameServerReceivedListener;
 
         private ExternalCoroutinesExecutor coroutinesExecutor;
@@ -20,6 +23,8 @@ namespace Scripts.UI.GameServerBrowser
             gameServerProviderService =
                 FindObjectOfType<GameServerProviderService>();
 
+            onConnectionFinishedListener =
+                GetComponent<IOnConnectionFinishedListener>();
             onGameServerReceivedListener =
                 GetComponent<IOnGameServerReceivedListener>();
 
@@ -43,6 +48,8 @@ namespace Scripts.UI.GameServerBrowser
 
         private async Task ProvideGameServersAsync(IYield yield)
         {
+            await ConnectIfNotConnectedAsync(yield);
+
             var gameServerProviderApi =
                 gameServerProviderService?.GameServerProviderApi;
             if (gameServerProviderApi != null)
@@ -59,6 +66,19 @@ namespace Scripts.UI.GameServerBrowser
                             x.MaxConnections));
 
                 onGameServerReceivedListener.OnGameServerReceived(gameServers);
+            }
+        }
+
+        private async Task ConnectIfNotConnectedAsync(IYield yield)
+        {
+            if (gameServerProviderService != null)
+            {
+                var connectionStatus =
+                    await gameServerProviderService.ConnectAsync(yield);
+                if (connectionStatus == ConnectionStatus.Failed)
+                {
+                    onConnectionFinishedListener.OnConnectionFailed();
+                }
             }
         }
     }
