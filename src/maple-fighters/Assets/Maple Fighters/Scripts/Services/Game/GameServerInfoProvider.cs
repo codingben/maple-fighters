8205223b0b7a28127e26b55d9f4a84df0b63216c
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-16 22:28:53 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameServerInfoProvider.cs
@@ -0,0 +1,28 @@
+﻿using CommonCommunicationInterfaces;
+using Network.Utils;
+
+namespace Scripts.Services.Game
+{
+    public class GameServerInfoProvider : Singleton<GameServerInfoProvider>
+    {
+        private PeerConnectionInformation connectionInformation;
+
+        private void Awake()
+        {
+            connectionInformation = new PeerConnectionInformation();
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void SetConnectionInfo(string ip, int port)
+        {
+            connectionInformation.Ip = ip;
+            connectionInformation.Port = port;
+        }
+
+        public PeerConnectionInformation GetConnectionInfo()
+        {
+            return connectionInformation;
+        }
+    }
+}
