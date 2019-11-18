42d728bc5cc9a75516af60bf8d4428d76ae84504
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-18 17:13:12 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameServerDisconnectionHandler.cs
@@ -0,0 +1,48 @@
+﻿using CommonCommunicationInterfaces;
+using UnityEngine;
+
+namespace Scripts.Services.Game
+{
+    public class GameServerDisconnectionHandler : MonoBehaviour
+    {
+        private GameService gameService;
+
+        private void Awake()
+        {
+            gameService = FindObjectOfType<GameService>();
+        }
+
+        private void Start()
+        {
+            SubscribeToDisconnectionNotifier();
+        }
+
+        private void OnDestroy()
+        {
+            UnsubscribeFromDisconnectionNotifier();
+        }
+
+        private void SubscribeToDisconnectionNotifier()
+        {
+            if (gameService != null)
+            {
+                gameService.DisconnectionNotifier.Disconnected +=
+                    OnDisconnected;
+            }
+        }
+
+        private void UnsubscribeFromDisconnectionNotifier()
+        {
+            if (gameService != null)
+            {
+                gameService.DisconnectionNotifier.Disconnected -=
+                    OnDisconnected;
+            }
+        }
+
+        private void OnDisconnected(DisconnectReason reason, string details)
+        {
+            // TODO: Implement
+        }
+    }
+}
