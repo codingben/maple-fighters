a838643200d2f3e8c55c5b9afcdd4e5d6ba941e6
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-18 16:38:12 +0200

    :alien: Exposed new property.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameService.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameService.cs
@@ -18,6 +18,8 @@ namespace Scripts.Services.Game
 
         public IGameSceneApi GameSceneApi { get; private set; }
 
+        public bool IsConnected => gamePeer != null && gamePeer.IsConnected;
+
         private IServerPeer gamePeer;
 
         private ExternalCoroutinesExecutor coroutinesExecutor;
