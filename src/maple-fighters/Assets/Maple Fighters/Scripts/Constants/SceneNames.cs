54945802c27500dc66f577b7309854ab94d8f2f2
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-18 23:20:54 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/SceneNames.cs
@@ -0,0 +1,15 @@
+﻿namespace Scripts.Constants
+{
+    public static class SceneNames
+    {
+        public static class Maps
+        {
+            public const string Lobby = "Lobby";
+            public const string TheDarkForest = "TheDarkForest";
+        }
+
+        public const string Login = "Login";
+        public const string Main = "Main";
+        public const string Game = "Game";
+    }
+}
