4e6df0b41a3bec186502588fe417f6db1e42cbcd
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-19 21:44:03 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Window/LoadingText.cs
@@ -0,0 +1,11 @@
+﻿using UI.Manager;
+using UnityEngine;
+
+namespace Scripts.UI.CharacterSelection
+{
+    [RequireComponent(typeof(UIFadeAnimation))]
+    public class LoadingText : UIElement
+    {
+        // Left blank intentionally
+    }
+}
