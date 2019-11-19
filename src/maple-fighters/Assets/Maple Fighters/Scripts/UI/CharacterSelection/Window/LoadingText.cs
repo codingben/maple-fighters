a326fdfac273bacfca5bf098aac38c84364fc6e3
Ben Ukhanov <ben1zuk321@gmail.com>
2019-11-19 22:51:41 +0200

    :alien: Implemented the script.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Window/LoadingText.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Window/LoadingText.cs
@@ -1,11 +1,39 @@
-﻿using UI.Manager;
+﻿using System;
+using UI.Manager;
 using UnityEngine;
 
 namespace Scripts.UI.CharacterSelection
 {
     [RequireComponent(typeof(UIFadeAnimation))]
-    public class LoadingText : UIElement, ILoadingView
+    public class LoadingText : UIElement, ILoadingView, ILoadingAnimation
     {
-        // Left blank intentionally
+        public ILoadingAnimation LoadingAnimation => this;
+
+        public event Action Finished;
+
+        private UIFadeAnimation uiFadeAnimation;
+
+        private void Awake()
+        {
+            uiFadeAnimation = GetComponent<UIFadeAnimation>();
+            uiFadeAnimation.FadeInCompleted += OnFadeInCompleted;
+            uiFadeAnimation.FadeOutCompleted += OnFadeOutCompleted;
+        }
+
+        private void OnDestroy()
+        {
+            uiFadeAnimation.FadeInCompleted -= OnFadeInCompleted;
+            uiFadeAnimation.FadeOutCompleted -= OnFadeOutCompleted;
+        }
+
+        private void OnFadeInCompleted()
+        {
+            Finished?.Invoke();
+        }
+
+        private void OnFadeOutCompleted()
+        {
+            Finished?.Invoke();
+        }
     }
 }
