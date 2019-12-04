7ccfeb9c3ac857d3fd088ee6b1114746cc653211
Ben Ukhanov <ben1zuk321@gmail.com>
2019-12-04 23:54:56 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Map/Objects/Utils.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Scripts.Gameplay.Map.Objects
+{
+    public static class Utils
+    {
+        public static bool IsInLayerMask(int layer, LayerMask layermask)
+        {
+            return layermask == (layermask | (1 << layer));
+        }
+    }
+}
