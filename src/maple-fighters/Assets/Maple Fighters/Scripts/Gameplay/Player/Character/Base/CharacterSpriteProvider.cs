64ae535d04932ea8fec0c539cbc9de8b3b61e128
Ben Ukhanov <ben1zuk321@gmail.com>
2019-12-06 22:27:06 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Character/Base/CharacterSpriteProvider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Scripts.Gameplay.Player
+{
+    public class CharacterSpriteProvider : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject characterSprite;
+
+        public GameObject GetCharacterSprite()
+        {
+            return characterSprite;
+        }
+    }
+}
