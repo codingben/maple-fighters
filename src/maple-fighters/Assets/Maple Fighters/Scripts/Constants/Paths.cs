98c7240cef90399f0dd2a448d5d288927b964c76
Ben Ukhanov <ben1zuk321@gmail.com>
2019-12-09 21:40:28 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/Paths.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/Paths.cs
@@ -6,14 +6,14 @@
         {
             public static class Game
             {
-                public const string CharactersPath = "Game/Characters/{0}";
-                public const string GraphicsPath = "Game/Graphics/{0}";
-                public const string MobsPath = "Game/Mobs/{0}";
-                public const string NpcsPath = "Game/Npcs/{0}";
-                public const string PlayersPath = "Game/Players/{0}";
+                public const string Characters = "Game/Characters/{0}";
+                public const string Graphics = "Game/Graphics/{0}";
+                public const string Mobs = "Game/Mobs/{0}";
+                public const string Npcs = "Game/Npcs/{0}";
+                public const string Players = "Game/Players/{0}";
             }
 
-            public const string ConfigurationsPath = "Configurations/{0}";
+            public const string Configurations = "Configurations/{0}";
         }
     }
 }
