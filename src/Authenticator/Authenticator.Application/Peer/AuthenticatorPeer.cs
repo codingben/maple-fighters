d1802a355e84f3a844e35a02d1264af99f74f12d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-04 00:48:30 +0200

    :alien: Code refactoring.

--- src/Authenticator/Authenticator.Application/Peer/AuthenticatorPeer.cs
+++ src/Authenticator/Authenticator.Application/Peer/AuthenticatorPeer.cs
@@ -1,25 +1,10 @@
 ﻿using Authenticator.Application.Peer.Logic;
 using CommonCommunicationInterfaces;
-using ServerCommon.PeerBase;
 
 namespace Authenticator.Application.Peer
 {
-    public class AuthenticatorPeer : PeerBase
+    public class AuthenticatorPeer
     {
-        protected override void OnConnected()
-        {
-            base.OnConnected();
-
-            BindPeerLogic<MainPeerLogic>();
-        }
-
-        protected override void OnDisconnected(
-            DisconnectReason reason,
-            string details)
-        {
-            base.OnDisconnected(reason, details);
-
-            UnbindPeerLogic();
-        }
+        // TODO: Implement
     }
 }
