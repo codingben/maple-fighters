506a8fea344198511515f1f7482a922c664596c3
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-08 22:34:04 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Peer.Interfaces/IPeerBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Peer
+{
+    public interface IPeerBase<out TPeer> : IDisposable
+        where TPeer : class, IMinimalPeer
+    {
+        TPeer Peer { get; }
+
+        int PeerId { get; }
+    }
+}
