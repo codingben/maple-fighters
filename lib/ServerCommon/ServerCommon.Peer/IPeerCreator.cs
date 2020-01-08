41790315f4bcbb99b510fc21756d850ae36fd77e
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-08 17:23:07 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Peer/IPeerCreator.cs
@@ -0,0 +1,10 @@
+﻿using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Peer
+{
+    public interface IPeerCreator<in TPeer>
+        where TPeer : class, IMinimalPeer
+    {
+        void Create(TPeer peer);
+    }
+}
