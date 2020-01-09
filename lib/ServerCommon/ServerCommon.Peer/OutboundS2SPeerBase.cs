7ba3e3eb7a9d6ffc7ff288be98e546b3b18c0b5a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-10 00:00:10 +0200

    :alien: Implemented the outbound s2s peer base.

--- lib/ServerCommon/ServerCommon.Peer/OutboundS2SPeerBase.cs
+++ lib/ServerCommon/ServerCommon.Peer/OutboundS2SPeerBase.cs
@@ -1,7 +1,39 @@
-﻿namespace ServerCommon.Peer
+﻿using System;
+using CommunicationHelper;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Peer
 {
-    public class OutboundS2SPeerBase
+    public abstract class OutboundS2SPeerBase<TO, TE> : IDisposable
+        where TO : IComparable, IFormattable, IConvertible
+        where TE : IComparable, IFormattable, IConvertible
     {
-        // TODO: Implement
+        protected IOperationRequestSender<TO> OperationRequestSender
+        {
+            get;
+        }
+
+        protected IEventHandlerRegister<TE> EventHandlerRegister
+        {
+            get;
+        }
+
+        protected IOperationResponseSubscriptionProvider SubscriptionProvider
+        {
+            get;
+        }
+
+        protected OutboundS2SPeerBase(IOutboundServerPeer peer, bool log = false)
+        {
+            OperationRequestSender = new OperationRequestSender<TO>(peer.OperationRequestSender, log);
+            EventHandlerRegister = new EventHandlerRegister<TE>(peer.EventNotifier, log);
+            SubscriptionProvider = new OperationResponseSubscriptionProvider<TO>(peer.OperationResponseNotifier, null, log);
+        }
+
+        public void Dispose()
+        {
+            EventHandlerRegister?.Dispose();
+            SubscriptionProvider?.Dispose();
+        }
     }
 }
