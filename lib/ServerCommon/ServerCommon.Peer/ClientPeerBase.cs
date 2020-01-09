600ba100ba50df9659832aea1879e90419d1b883
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-09 23:40:50 +0200

    :alien: Implemented the client peer base.

--- lib/ServerCommon/ServerCommon.Peer/ClientPeerBase.cs
+++ lib/ServerCommon/ServerCommon.Peer/ClientPeerBase.cs
@@ -1,19 +1,43 @@
 ﻿using System;
+using CommonTools.Coroutines;
+using ServerCommunicationHelper;
 using ServerCommunicationInterfaces;
 
 namespace ServerCommon.Peer
 {
-    public abstract class ClientPeerBase<TO, TE> : IPeerBase<IClientPeer>
+    public abstract class ClientPeerBase<TO, TE>
         where TO : IComparable, IFormattable, IConvertible
         where TE : IComparable, IFormattable, IConvertible
     {
-        public IClientPeer Peer { get; }
+        protected IOperationRequestHandlerRegister<TO> OperationHandlerRegister
+        {
+            get;
+        }
 
-        public int PeerId { get; }
+        protected IEventSender<TE> EventSender
+        {
+            get;
+        }
+
+        protected IFiber Fiber
+        {
+            get;
+        }
+
+        protected ICoroutinesExecutor CoroutinesExecutor => GetCoroutinesExecutor();
+
+        protected ClientPeerBase(IClientPeer peer, bool log = false)
+        {
+            OperationHandlerRegister = new OperationRequestsHandler<TO>(peer.OperationRequestNotifier, peer.OperationResponseSender, log, log, CoroutinesExecutor);
+            EventSender = new EventSender<TE>(peer.EventSender, log);
+            Fiber = peer.Fiber;
+        }
 
         public void Dispose()
         {
-            // TODO: Implement
+            OperationHandlerRegister?.Dispose();
         }
+
+        protected abstract ICoroutinesExecutor GetCoroutinesExecutor();
     }
 }
