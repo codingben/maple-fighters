174b46a2875fd5b25e2b54d062b7b3fa934d16c1
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-10 00:16:13 +0200

    :alien: Implemented the event handler.

--- lib/ServerCommon/ServerCommon.Peer.Extensions/EventHandler.cs
+++ lib/ServerCommon/ServerCommon.Peer.Extensions/EventHandler.cs
@@ -1,7 +1,28 @@
-﻿namespace ServerCommon.Peer.Extensions
+﻿using System;
+using CommonCommunicationInterfaces;
+using CommunicationHelper;
+
+namespace ServerCommon.Peer.Extensions
 {
-    public class EventHandler
+    public class EventHandler<TP> : IEventHandler<TP>
+        where TP : IParameters, new()
     {
-        // TODO: Implement
+        private readonly Action<MessageData<TP>> action;
+
+        public EventHandler(Action<MessageData<TP>> action)
+        {
+            this.action = action;
+        }
+
+        public bool Handle(MessageData<TP> eventData)
+        {
+            if (action != null)
+            {
+                action.Invoke(eventData);
+                return true;
+            }
+
+            return false;
+        }
     }
 }
