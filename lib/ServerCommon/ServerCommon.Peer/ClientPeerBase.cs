db7cdba391a5e96a640bbb4d80a5bfb072d8e1de
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-09 22:31:13 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Peer/ClientPeerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Peer
+{
+    public abstract class ClientPeerBase<TO, TE> : IPeerBase<IClientPeer>
+        where TO : IComparable, IFormattable, IConvertible
+        where TE : IComparable, IFormattable, IConvertible
+    {
+        public IClientPeer Peer { get; }
+
+        public int PeerId { get; }
+
+        public void Dispose()
+        {
+            // TODO: Implement
+        }
+    }
+}
