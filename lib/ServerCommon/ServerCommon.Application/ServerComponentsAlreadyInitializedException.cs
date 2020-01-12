c43597e0893b70abdca6e15478f39e0723c2747e
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-12 23:17:28 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Application/ServerComponentsAlreadyInitializedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ServerCommon.Application
+{
+    public class ServerComponentsAlreadyInitializedException : Exception
+    {
+        public ServerComponentsAlreadyInitializedException()
+            : base("The server components provider has already been initialized.")
+        {
+            // Left blank intentionally
+        }
+    }
+}
