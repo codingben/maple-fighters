c4cc67ff3cfd3d7ec928d8b494a7ff356a3d7a8a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-12 23:18:30 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Application/ServerComponentsNotInitializedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ServerCommon.Application
+{
+    public class ServerComponentsNotInitializedException : Exception
+    {
+        public ServerComponentsNotInitializedException()
+            : base("The server components provider is not initialized.")
+        {
+            // Left blank intentionally
+        }
+    }
+}
