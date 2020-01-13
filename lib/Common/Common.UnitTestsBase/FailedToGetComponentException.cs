e91df0c37b1884e94dcbdb0d41be167c73776dcc
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-13 22:56:32 +0200

    :alien: A new script.

---
+++ lib/Common/Common.UnitTestsBase/FailedToGetComponentException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.UnitTestsBase
+{
+    public class FailedToGetComponentException : Exception
+    {
+        public FailedToGetComponentException(string componentName)
+            : base($"Failed to get {componentName} component.")
+        {
+            // Left blank intentionally
+        }
+    }
+}
