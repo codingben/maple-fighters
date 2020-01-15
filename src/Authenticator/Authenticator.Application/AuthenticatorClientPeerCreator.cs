97bd6fd7de32a545681799c8bef35ca9f68a69e5
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-15 23:02:39 +0200

    :alien: A new script.

---
+++ src/Authenticator/Authenticator.Application/AuthenticatorClientPeerCreator.cs
@@ -0,0 +1,13 @@
+﻿using ServerCommon.Application;
+using ServerCommunicationInterfaces;
+
+namespace Authenticator.Application
+{
+    public class AuthenticatorClientPeerCreator : IClientPeerCreator
+    {
+        public void Create(IClientPeer clientPeer)
+        {
+            // TODO: Implement
+        }
+    }
+}
