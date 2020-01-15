e965ab68a60188487fcf1f1eb6eae6a8541cfacd
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-15 23:01:41 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Application/IClientPeerCreator.cs
@@ -0,0 +1,9 @@
+﻿using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Application
+{
+    public interface IClientPeerCreator
+    {
+        void Create(IClientPeer clientPeer);
+    }
+}
