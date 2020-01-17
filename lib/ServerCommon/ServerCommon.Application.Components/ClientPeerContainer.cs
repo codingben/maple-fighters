efe6c571af08a58c6bdf230ec2f7641d96465775
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-17 23:56:09 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Application.Components/ClientPeerContainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Common.ComponentModel;
+
+namespace ServerCommon.Application.Components
+{
+    [ComponentSettings(ExposedState.Unexposable)]
+    public class ClientPeerContainer : ComponentBase, IClientPeerContainer
+    {
+        private readonly Dictionary<int, IDisposable> collection;
+
+        public ClientPeerContainer()
+        {
+            collection = new Dictionary<int, IDisposable>();
+        }
+
+        public void Add(int id, IDisposable peer)
+        {
+            collection.Add(id, peer);
+        }
+
+        public void Remove(int id)
+        {
+            collection.Remove(id);
+        }
+
+        public bool Get(int id, out IDisposable peer)
+        {
+            return collection.TryGetValue(id, out peer);
+        }
+    }
+}
