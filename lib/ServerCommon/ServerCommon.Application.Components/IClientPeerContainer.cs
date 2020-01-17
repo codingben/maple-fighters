375100d502b07826b7e5f98432a54f6a62c5025d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-17 23:52:48 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Application.Components/IClientPeerContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ServerCommon.Application.Components
+{
+    public interface IClientPeerContainer
+    {
+        void Add(IDisposable peer);
+
+        void Remove(int id);
+
+        bool Get(int id, out IDisposable peer);
+    }
+}
