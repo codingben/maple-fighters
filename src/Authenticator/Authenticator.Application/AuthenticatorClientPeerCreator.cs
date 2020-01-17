c5e310f5e7b23e18b42de48be78534d157865b05
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-18 00:03:20 +0200

    :alien: Adding a new peer.

--- src/Authenticator/Authenticator.Application/AuthenticatorClientPeerCreator.cs
+++ src/Authenticator/Authenticator.Application/AuthenticatorClientPeerCreator.cs
@@ -1,13 +1,30 @@
-﻿using ServerCommon.Application;
+﻿using Authenticator.Application.Peer.Logic;
+using Common.Components;
+using ServerCommon.Application;
+using ServerCommon.Application.Components;
 using ServerCommunicationInterfaces;
 
 namespace Authenticator.Application
 {
     public class AuthenticatorClientPeerCreator : IClientPeerCreator
     {
+        private readonly IClientPeerContainer clientPeerContainer;
+        private readonly IIdGenerator idGenerator;
+
+        public AuthenticatorClientPeerCreator()
+        {
+            clientPeerContainer = ServerExposedComponents
+                .Provide()
+                .Get<IClientPeerContainer>();
+            idGenerator = ServerExposedComponents
+                .Provide()
+                .Get<IIdGenerator>();
+        }
+
         public void Create(IClientPeer peer)
         {
-            // TODO: Implement
+            var id = idGenerator.GenerateId();
+            clientPeerContainer.Add(id, new AuthenticatorClientPeer(peer));
         }
     }
 }
