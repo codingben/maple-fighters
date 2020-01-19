48c6be12df9fb8465adb43691a79896230c5a430
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-19 23:16:56 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Application.Components/OnClientPeerContainerRemovedHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Common.ComponentModel;
+
+namespace ServerCommon.Application.Components
+{
+    public class OnClientPeerContainerRemovedHandler : ComponentBase, IOnClientPeerContainerRemoved
+    {
+        public void Handle(IEnumerable<IPeerWrapper> peerWrappers)
+        {
+            foreach (var peerWrapper in peerWrappers)
+            {
+                var peer = peerWrapper.Peer;
+                if (peer.IsConnected)
+                {
+                    peer.Disconnect();
+                }
+            }
+        }
+    }
+}
