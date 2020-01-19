8b68de3400d9d5f41cc01f417b93b4d66352cfc7
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-19 23:16:20 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Application.Components/IOnClientPeerContainerRemoved.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ServerCommon.Application.Components
+{
+    public interface IOnClientPeerContainerRemoved
+    {
+        void Handle(IEnumerable<IPeerWrapper> peerWrappers);
+    }
+}
