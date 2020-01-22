66f3e1ffd75d964a5bece1f204c4f83c1793d623
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-22 22:29:35 +0200

    :alien: Implemented script.

--- lib/ServerCommon/ServerCommon.Communication/ServerConnectorImplementation.cs
+++ lib/ServerCommon/ServerCommon.Communication/ServerConnectorImplementation.cs
@@ -1,7 +1,26 @@
-﻿namespace ServerCommon.Communication
+﻿using Common.ComponentModel;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Communication
 {
-    public class ServerConnectorImplementation
+    public interface IServerConnectorProvider
     {
-        // TODO: Implement
+        IServerConnector Provide();
+    }
+
+    [ComponentSettings(ExposedState.Unexposable)]
+    public class ServerConnectorProvider : ComponentBase, IServerConnectorProvider
+    {
+        private readonly IServerConnector serverConnector;
+
+        public ServerConnectorProvider(IServerConnector serverConnector)
+        {
+            this.serverConnector = serverConnector;
+        }
+
+        public IServerConnector Provide()
+        {
+            return serverConnector;
+        }
     }
 }
