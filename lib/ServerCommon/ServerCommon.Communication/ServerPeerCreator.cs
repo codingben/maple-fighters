52fe5181daf75bc054112cd2ac6b997fda9d6561
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-23 23:26:29 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Communication/ServerPeerCreator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.ComponentModel;
+using CommonCommunicationInterfaces;
+using CommonTools.Coroutines;
+using CommonTools.Log;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Communication
+{
+    public class ServerPeerCreator : ComponentBase
+    {
+        private readonly Action<IOutboundServerPeer> onServerPeerCreated;
+
+        private IServerConnectorProvider serverConnectorProvider;
+        private ICoroutinesExecuter coroutinesExecuter;
+
+        public ServerPeerCreator(Action<IOutboundServerPeer> onServerPeerCreated)
+        {
+            this.onServerPeerCreated = onServerPeerCreated;
+        }
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+
+            serverConnectorProvider = Components.Get<IServerConnectorProvider>();
+            coroutinesExecuter = Components.Get<ICoroutinesExecuter>();
+        }
+
+        public void Connect(string ip, int port)
+        {
+            coroutinesExecuter.StartTask((y) => ConnectAsync(y, ip, port));
+        }
+
+        public async Task ConnectAsync(IYield yield, string ip, int port)
+        {
+            var serverConnector = serverConnectorProvider.Provide();
+
+            try
+            {
+                var peer = new PeerConnectionInformation(ip, port);
+                var serverPeer = await serverConnector.Connect(yield, peer);
+                if (serverPeer != null)
+                {
+                    onServerPeerCreated?.Invoke(serverPeer);
+                }
+            }
+            catch (Exception exception)
+            {
+                LogUtils.Log(exception.Message);
+            }
+        }
+    }
+}
