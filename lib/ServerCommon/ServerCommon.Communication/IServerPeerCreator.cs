2db5ad23cdd7fb0ad0e771513ec7a170fff91952
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-23 23:32:43 +0200

    :alien: A new script.

---
+++ lib/ServerCommon/ServerCommon.Communication/IServerPeerCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CommonTools.Coroutines;
+
+namespace ServerCommon.Communication
+{
+    public interface IServerPeerCreator
+    {
+        Task ConnectAsync(IYield yield, string ip, int port);
+    }
+}
