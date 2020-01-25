cf823f7ead6b854d7cf83391a524b6ddaf6bc867
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-25 19:16:59 +0200

    :alien: Added documentation.

--- lib/ServerCommon/ServerCommon.Application/IClientPeerCreator.cs
+++ lib/ServerCommon/ServerCommon.Application/IClientPeerCreator.cs
@@ -2,8 +2,15 @@
 
 namespace ServerCommon.Application
 {
+    /// <summary>
+    /// Creator of a new connected peer.
+    /// </summary>
     public interface IClientPeerCreator
     {
+        /// <summary>
+        /// Uses a client peer and creates peer logic.
+        /// </summary>
+        /// <param name="peer">The new peer.</param>
         void Create(IClientPeer peer);
     }
 }
