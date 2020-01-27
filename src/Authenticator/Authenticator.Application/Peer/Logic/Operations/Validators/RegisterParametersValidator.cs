2ad2782a2f8a50b140a97c767716b6697da8b446
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-27 23:27:20 +0200

    :alien: Code refactoring.

--- src/Authenticator/Authenticator.Application/Peer/Logic/Operations/Validators/RegisterParametersValidator.cs
+++ src/Authenticator/Authenticator.Application/Peer/Logic/Operations/Validators/RegisterParametersValidator.cs
@@ -7,17 +7,10 @@ namespace Authenticator.Application.Peer.Logic.Operations.Validators
     {
         public RegisterParametersValidator()
         {
-            RuleFor(x => x.Email).NotEmpty().EmailAddress()
-                .WithMessage("Invalid email address.");
-
-            RuleFor(x => x.FirstName).NotEmpty()
-                .WithMessage("First name is required.");
-
-            RuleFor(x => x.LastName).NotEmpty()
-                .WithMessage("Last name is required.");
-
-            RuleFor(x => x.Password).NotEmpty()
-                .WithMessage("Password is required.");
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Invalid email address.");
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.");
         }
     }
 }
