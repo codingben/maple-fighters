e9dce747c5dc81ec296d87f873437fc50a7ea96b
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-30 23:10:42 +0200

    :alien: Code refactoring.

--- lib/Common/Common.ComponentModel/Core/ComponentsContainerUtils.cs
+++ lib/Common/Common.ComponentModel/Core/ComponentsContainerUtils.cs
@@ -4,41 +4,6 @@ namespace Common.ComponentModel.Core
 {
     internal static class ComponentsContainerUtils
     {
-        public static void GetComponentByLifetime<TComponent>(ref TComponent component)
-            where TComponent : class
-        {
-            var componentSettings =
-                ComponentSettingsUtils.GetComponentSettings<TComponent>();
-            if (componentSettings != null)
-            {
-                var lifeTime = componentSettings.Lifetime;
-                switch (lifeTime)
-                {
-                    case Lifetime.Singleton:
-                    {
-                        break;
-                    }
-
-                    case Lifetime.PerThread:
-                    {
-                        throw new NotImplementedException();
-                    }
-
-                    case Lifetime.PerCall:
-                    {
-                        component = 
-                            (TComponent)Activator.CreateInstance(typeof(TComponent));
-                        break;
-                    }
-
-                    default:
-                    {
-                        throw new ArgumentOutOfRangeException();
-                    }
-                }
-            }
-        }
-
         public static ExposedState GetExposedState<TComponent>()
             where TComponent : IComponent
         {
