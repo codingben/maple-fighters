9f194119c3e5ee2d708ba1617d0e0d8a1c8fdbd2
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-30 23:18:05 +0200

    :alien: Code refactoring.

--- lib/Common/Common.ComponentModel/Core/Utils.cs
+++ lib/Common/Common.ComponentModel/Core/Utils.cs
@@ -1,9 +1,40 @@
-﻿using Common.ComponentModel.Exceptions;
+﻿using System;
+using Common.ComponentModel.Exceptions;
 
 namespace Common.ComponentModel.Core
 {
     internal static class Utils
     {
+        public static ComponentSettingsAttribute GetComponentSettings<TComponent>()
+            where TComponent : IComponent
+        {
+            var componentSettings =
+                (ComponentSettingsAttribute)Attribute.GetCustomAttribute(
+                    typeof(TComponent),
+                    typeof(ComponentSettingsAttribute));
+            if (componentSettings == null)
+            {
+                if (typeof(IDisposable).IsAssignableFrom(typeof(TComponent)))
+                {
+                    throw new ComponentSettingsMissingException(nameof(TComponent));
+                }
+            }
+
+            return componentSettings;
+        }
+
+        public static ExposedState GetExposedState<TComponent>()
+            where TComponent : IComponent
+        {
+            var componentSettings = GetComponentSettings<TComponent>();
+            if (componentSettings != null)
+            {
+                return componentSettings.ExposedState;
+            }
+
+            throw new InvalidOperationException();
+        }
+
         public static void AssertNotInterface<TComponent>()
             where TComponent : class
         {
