727270415f28e86b7ee0c9b21144e4fe52abaabe
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-30 23:44:53 +0200

    :alien: Code refactoring.

--- lib/Common/Common.ComponentModel.Tests/ExtensionMethods.cs
+++ lib/Common/Common.ComponentModel.Tests/ExtensionMethods.cs
@@ -5,15 +5,15 @@ namespace Common.ComponentModel.Tests
     public static class ExtensionMethods
     {
         public static T AddAndMock<T>(this IComponents components) 
-            where T : class, IComponent
+            where T : class
         {
-            return components.Add(Substitute.For<T>());
+            return (T)components.Add((IComponent)Substitute.For<T>());
         }
 
         public static T AddAndMock<T>(this IExposedComponents components) 
-            where T : class, IComponent
+            where T : class
         {
-            return components.Add(Substitute.For<T>());
+            return (T)components.Add((IComponent)Substitute.For<T>());
         }
     }
 }
