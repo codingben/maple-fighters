669bc9265fbba801a21b7dec369ce1b147063572
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-30 23:51:20 +0200

    :alien: Code refactoring.

--- tests/unit/Common/Common.Components.UnitTests/IdGeneratorTests.cs
+++ tests/unit/Common/Common.Components.UnitTests/IdGeneratorTests.cs
@@ -11,7 +11,7 @@ namespace Common.Components.UnitTests
 
         public IdGeneratorTests()
         {
-            components = new ComponentsProvider();
+            components = new ComponentsContainer();
             components.Add(new IdGenerator());
         }
 
