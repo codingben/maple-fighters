db8203d3687ade143d604d79f0f33fb9b39673c2
Ben Ukhanov <ben1zuk321@gmail.com>
2020-01-30 23:02:53 +0200

    :alien: Code refactoring.

--- lib/Common/Common.ComponentModel/Core/ComponentsContainerUtils.cs
+++ lib/Common/Common.ComponentModel/Core/ComponentsContainerUtils.cs
@@ -5,7 +5,7 @@ namespace Common.ComponentModel.Core
     internal static class ComponentsContainerUtils
     {
         public static void GetComponentByLifetime<TComponent>(ref TComponent component)
-            where TComponent : IComponent
+            where TComponent : class
         {
             var componentSettings =
                 ComponentSettingsUtils.GetComponentSettings<TComponent>();
