69bf34b3475936248f366620151c198bf296932c
Ben Ukhanov <ben1zuk321@gmail.com>
2020-02-29 16:00:04 +0200

    :alien: Added more documentation.

--- lib/common/Common.Repository/IAsyncEditableRepository.cs
+++ lib/common/Common.Repository/IAsyncEditableRepository.cs
@@ -14,18 +14,21 @@ namespace Common.Repository
         /// Creates the new entity in the repository.
         /// </summary>
         /// <param name="entity">The entity.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
         Task CreateAsync(TEntity entity);
 
         /// <summary>
         /// Deletes an entity from the repository by its id.
         /// </summary>
         /// <param name="id">The entity's id.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
         Task DeleteAsync(TKey id);
 
         /// <summary>
         /// Upserts an entity.
         /// </summary>
         /// <param name="entity">The entity.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
         Task UpdateAsync(TEntity entity);
     }
 }
