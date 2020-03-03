6d5f15997100d4f0aae4918ff067259e6b0d2146
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-03 22:54:06 +0200

    :alien: Added scripts.

---
+++ tests/unit/common/Common.Components.UnitTests/RandomNumberGeneratorTests.cs
@@ -0,0 +1,47 @@
+﻿using Common.ComponentModel;
+using Shouldly;
+using Xunit;
+
+namespace Common.Components.UnitTests
+{
+    public class RandomNumberGeneratorTests
+    {
+        private readonly IComponents components;
+
+        public RandomNumberGeneratorTests()
+        {
+            components = new ComponentsContainer();
+            components.Add(new RandomNumberGenerator());
+        }
+
+        [Fact]
+        private void GenerateRandomNumber()
+        {
+            // Arrange
+            var randomNumberGenerator =
+                components.Get<IRandomNumberGenerator>();
+
+            // Act
+            var number = randomNumberGenerator.GenerateRandomNumber();
+
+            // Assert
+            number.Equals(0).ShouldBeFalse();
+        }
+
+        [Theory]
+        [InlineData(100, 200)]
+        [InlineData(1000, 2000)]
+        private void GenerateRandomNumber_WithParameters(int min, int max)
+        {
+            // Arrange
+            var randomNumberGenerator =
+                components.Get<IRandomNumberGenerator>();
+
+            // Act
+            var number = randomNumberGenerator.GenerateRandomNumber(min, max);
+
+            // Assert
+            Assert.InRange(number, min, max);
+        }
+    }
+}
