abf5d26ab74bfaea7a12aabea84fac6998822c2d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-03 22:37:24 +0200

    :alien: Added a script.

---
+++ lib/common/Common.ComponentModel.Tests/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using NSubstitute;
+
+namespace Common.ComponentModel.Tests
+{
+    public static class ExtensionMethods
+    {
+        public static T AddAndMock<T>(this IComponents components) 
+            where T : class, IComponent
+        {
+            return components.Add(Substitute.For<T>());
+        }
+
+        public static T AddAndMock<T>(this IExposedComponents components) 
+            where T : class, IComponent
+        {
+            return components.Add(Substitute.For<T>());
+        }
+    }
+}
