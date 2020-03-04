7495f190b085ad4dcde44b834af996daa2cd03c9
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-04 22:46:28 +0200

    :alien: Added a script.

---
+++ lib/common/Common.ParametersValidation/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+
+namespace Common.ParametersValidation
+{
+    public static class ExtensionMethods
+    {
+        public static string ToErrorMessage(this ValidationResult validationResult)
+        {
+            return !validationResult.IsValid ? validationResult.ToString() : null;
+        }
+    }
+}
