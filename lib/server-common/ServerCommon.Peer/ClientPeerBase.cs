02f9cefefde060627a92123969bfbf73be93a634
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-06 21:58:40 +0200

    :alien: Added scripts.

---
+++ lib/server-common/ServerCommon.Peer/ClientPeerBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CommonTools.Coroutines;
+using ServerCommunicationHelper;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Peer
+{
+    public abstract class ClientPeerBase<TO, TE> : IDisposable
+        where TO : IComparable, IFormattable, IConvertible
+        where TE : IComparable, IFormattable, IConvertible
+    {
+        protected IMinimalPeer Peer
+        {
+            get;
+        }
+
+        protected IOperationRequestHandlerRegister<TO> OperationHandlerRegister
+        {
+            get;
+        }
+
+        protected IEventSender<TE> EventSender
+        {
+            get;
+        }
+
+        protected ICoroutinesExecutor CoroutinesExecutor => GetCoroutinesExecutor();
+
+        protected ClientPeerBase(IClientPeer peer, bool log = false)
+        {
+            Peer = peer;
+            OperationHandlerRegister = new OperationRequestsHandler<TO>(
+                peer.OperationRequestNotifier,
+                peer.OperationResponseSender,
+                log,
+                log,
+                CoroutinesExecutor);
+            EventSender = new EventSender<TE>(peer.EventSender, log);
+        }
+
+        public void Dispose()
+        {
+            OperationHandlerRegister?.Dispose();
+        }
+
+        protected abstract ICoroutinesExecutor GetCoroutinesExecutor();
+    }
+}
---
+++ lib/server-common/ServerCommon.Peer/OutboundS2SPeerBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CommunicationHelper;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Peer
+{
+    public abstract class OutboundS2SPeerBase<TO, TE> : IDisposable
+        where TO : IComparable, IFormattable, IConvertible
+        where TE : IComparable, IFormattable, IConvertible
+    {
+        protected IOperationRequestSender<TO> OperationRequestSender
+        {
+            get;
+        }
+
+        protected IEventHandlerRegister<TE> EventHandlerRegister
+        {
+            get;
+        }
+
+        protected IOperationResponseSubscriptionProvider SubscriptionProvider
+        {
+            get;
+        }
+
+        protected OutboundS2SPeerBase(IOutboundServerPeer peer, bool log = false)
+        {
+            OperationRequestSender = new OperationRequestSender<TO>(
+                peer.OperationRequestSender,
+                log);
+            EventHandlerRegister =
+                new EventHandlerRegister<TE>(peer.EventNotifier, log);
+            SubscriptionProvider =
+                new OperationResponseSubscriptionProvider<TO>(
+                    peer.OperationResponseNotifier,
+                    null,
+                    log);
+        }
+
+        public void Dispose()
+        {
+            EventHandlerRegister?.Dispose();
+            SubscriptionProvider?.Dispose();
+        }
+    }
+}
