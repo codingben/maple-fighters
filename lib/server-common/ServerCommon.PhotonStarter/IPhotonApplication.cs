90d9072b1a88139a3dd678ffe33a9579a29fecda
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-06 22:54:53 +0200

    :alien: A new script.

---
+++ lib/server-common/ServerCommon.PhotonStarter/IPhotonApplication.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerCommon.PhotonStarter
+{
+    /// <summary>
+    /// Represents an access to the server application from the outside.
+    /// </summary>
+    public interface IPhotonApplication
+    {
+        /// <summary>
+        /// Awakes the server application.
+        /// </summary>
+        void Startup();
+
+        /// <summary>
+        /// Kills the server application.
+        /// </summary>
+        void Shutdown();
+    }
+}
