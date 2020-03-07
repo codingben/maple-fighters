ad97d8a9ed5262fb1e2cb3ea6dff4464407aa27f
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-07 20:55:45 +0200

    :alien: Added scripts.

---
+++ lib/server-common/ServerCommon.Application/TimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CommonTools.Coroutines;
+
+namespace ServerCommon.Application
+{
+    public class TimeProvider : ITimeProvider
+    {
+        public float GetCurrentTime()
+        {
+            return (float)(Environment.TickCount * 1E-03);
+        }
+    }
+}
