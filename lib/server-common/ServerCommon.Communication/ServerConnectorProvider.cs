5a9fc148bad44cbc2d5a584b5c297559ab73adaa
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-07 20:43:30 +0200

    :alien: Added scripts.

---
+++ lib/server-common/ServerCommon.Communication/ServerConnectorProvider.cs
@@ -0,0 +1,21 @@
+﻿using Common.ComponentModel;
+using ServerCommunicationInterfaces;
+
+namespace ServerCommon.Communication
+{
+    [ComponentSettings(ExposedState.Unexposable)]
+    public class ServerConnectorProvider : ComponentBase, IServerConnectorProvider
+    {
+        private readonly IServerConnector serverConnector;
+
+        public ServerConnectorProvider(IServerConnector serverConnector)
+        {
+            this.serverConnector = serverConnector;
+        }
+
+        public IServerConnector Provide()
+        {
+            return serverConnector;
+        }
+    }
+}
