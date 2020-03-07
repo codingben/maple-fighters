bed7c2dcfdda019e227ccb417c04516237a9584b
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-07 19:31:59 +0200

    :alien: Removed script.

--- lib/server-common/ServerCommon.Application.Components/IOnClientPeerContainerRemoved.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ServerCommon.Application.Components
-{
-    public interface IOnClientPeerContainerRemoved
-    {
-        void Handle(IEnumerable<IPeerWrapper> peerWrappers);
-    }
-}
