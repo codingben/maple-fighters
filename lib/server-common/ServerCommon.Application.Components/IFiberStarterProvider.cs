e099695d6e4d903396b1280051e55e11e8820ae6
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-07 20:45:22 +0200

    :alien: Removed unused files.

--- lib/server-common/ServerCommon.Application.Components/IFiberStarterProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using ServerCommunicationInterfaces;
-
-namespace ServerCommon.Application.Components
-{
-    public interface IFiberStarterProvider
-    {
-        IFiber ProvideFiberStarter();
-    }
-}
