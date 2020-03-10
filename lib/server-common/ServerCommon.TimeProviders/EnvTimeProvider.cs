42567f8ad64e21e9034554f16e923e19c5021267
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-10 18:32:01 +0200

    :alien: A new script.

---
+++ lib/server-common/ServerCommon.TimeProviders/EnvTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CommonTools.Coroutines;
+
+namespace ServerCommon.TimeProviders
+{
+    public class EnvTimeProvider : ITimeProvider
+    {
+        public float GetCurrentTime()
+        {
+            return (float)(Environment.TickCount * 1E-03);
+        }
+    }
+}
