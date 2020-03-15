a8b5a54be131cd7d0003822e86400b3715f2956a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-15 21:37:58 +0200

    :alien: Code refactoring.

--- src/auth-service/Authenticator.Infrastructure/Repository/AccountRepository.cs
+++ src/auth-service/Authenticator.Infrastructure/Repository/AccountRepository.cs
@@ -1,30 +1,21 @@
 ﻿using Authenticator.Domain.Aggregates.User;
-using Common.ComponentModel;
 using Common.MongoDB;
-using CommonTools.Log;
 using MongoDB.Driver;
 
 namespace Authenticator.Infrastructure.Repository
 {
-    [ComponentSettings(ExposedState.Unexposable)]
-    public class AccountRepository : MongoRepositoryBase<Account>, IAccountRepository
+    public class AccountRepository : MongoRepository<Account>, IAccountRepository
     {
-        private const string CollectionName = "accounts";
+        private readonly IMongoDatabase mongoDatabase;
 
-        private IMongoDatabase mongoDatabase;
-
-        protected override void OnAwake()
+        public AccountRepository(IDatabaseProvider databaseProvider)
         {
-            base.OnAwake();
-
-            var mongoDatabaseProvider = Components.Get<IMongoDatabaseProvider>()
-                .AssertNotNull();
-            mongoDatabase = mongoDatabaseProvider.Provide();
+            mongoDatabase = databaseProvider.Provide();
         }
 
         protected override IMongoCollection<Account> GetCollection()
         {
-            return mongoDatabase.GetCollection<Account>(CollectionName);
+            return mongoDatabase.GetCollection<Account>(name: "accounts");
         }
     }
 }
