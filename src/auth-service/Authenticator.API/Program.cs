9f11989f4dc3217c9434a7089647b65ea78b89e9
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-15 20:17:01 +0200

    :alien: Code refactoring.

--- src/auth-service/Authenticator.API/Program.cs
+++ src/auth-service/Authenticator.API/Program.cs
@@ -5,20 +5,14 @@ namespace Authenticator.API
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main() => CreateHostBuilder().Build().Run();
 
         /*
          * Additional configuration is required to successfully run gRPC on macOS.
          * For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         */
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(x => x.UseStartup<AuthenticatorStartup>());
     }
 }
