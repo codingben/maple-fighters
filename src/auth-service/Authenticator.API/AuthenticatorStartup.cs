93bbfc7557947d8f0a61e3f3eec7f0fd03c95d61
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-15 20:42:19 +0200

    :alien: Code refactoring.

--- src/auth-service/Authenticator.API/AuthenticatorStartup.cs
+++ src/auth-service/Authenticator.API/AuthenticatorStartup.cs
@@ -13,22 +13,21 @@ namespace Authenticator.API
             services.AddGrpc();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder applicationBuilder,
+            IWebHostEnvironment environment)
         {
-            if (env.IsDevelopment())
+            if (environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                applicationBuilder.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
+            applicationBuilder.UseRouting();
+            applicationBuilder.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet(
                     "/", 
-                    async context =>
-                    {
-                        await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                    });
+                    async context => await context.Response.WriteAsync("Please use a gRPC client."));
             });
         }
     }
