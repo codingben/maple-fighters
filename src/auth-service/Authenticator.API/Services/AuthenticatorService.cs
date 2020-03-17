557a18547588eace4e737791b86d667aad5dcd38
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-17 23:21:34 +0200

    :alien: A new authenticator service.

---
+++ src/auth-service/Authenticator.API/Services/AuthenticatorService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Authenticator.API.Services
+{
+    public class AuthenticatorService : Authenticator.AuthenticatorBase
+    {
+        public override Task<LoginResponse> Login(LoginRequest request, ServerCallContext context)
+        {
+            return base.Login(request, context);
+        }
+
+        public override Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
+        {
+            return base.Register(request, context);
+        }
+    }
+}
