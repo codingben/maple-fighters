d6c319d838e3e63cd37180d6d80e58a971f76303
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-18 23:25:57 +0200

    :alien: A new script.

---
+++ src/auth-service/Authenticator.API/Controllers/RegistrationController.cs
@@ -0,0 +1,42 @@
+﻿using Authenticator.API.Datas;
+using Authenticator.API.Validators;
+using Authenticator.Domain.Aggregates.User;
+using Authenticator.Domain.Aggregates.User.Services;
+
+namespace Authenticator.API.Controllers
+{
+    public class RegistrationController : IRegistrationController
+    {
+        private readonly IRegistrationService registrationService;
+        private readonly RegistrationDataValidator registrationDataValidator;
+
+        public RegistrationController(IRegistrationService registrationService)
+        {
+            this.registrationService = registrationService;
+
+            registrationDataValidator = new RegistrationDataValidator();
+        }
+
+        public AccountCreationStatus Register(RegistrationData registrationData)
+        {
+            var accountCreationStatus = AccountCreationStatus.Failed;
+
+            // TODO: Get errors if invalid
+            var validationResult =
+                registrationDataValidator.Validate(registrationData);
+            if (validationResult.IsValid)
+            {
+                var account = AccountFactory.CreateAccount(
+                    registrationData.Email,
+                    registrationData.Password,
+                    registrationData.FirstName,
+                    registrationData.LastName);
+
+                accountCreationStatus =
+                    registrationService.CreateAccount(account);
+            }
+
+            return accountCreationStatus;
+        }
+    }
+}
