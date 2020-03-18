b81ee7971baeb52223ef0eb2661d070297b5aa50
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-18 22:50:09 +0200

    :alien: Added parameters.

--- src/auth-service/Authenticator.API/Datas/RegistrationData.cs
+++ src/auth-service/Authenticator.API/Datas/RegistrationData.cs
@@ -2,6 +2,24 @@
 {
     public struct RegistrationData
     {
-        // TODO: Implement
+        public string Email { get; }
+
+        public string Password { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public RegistrationData(
+            string email,
+            string password,
+            string firstName,
+            string lastName)
+        {
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
