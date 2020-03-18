36dcb4ca22414e14875c106112fea9a20c8afb13
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-18 21:39:47 +0200

    :alien: A new script.

---
+++ src/auth-service/Authenticator.API/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Authenticator.Domain.Aggregates.User.Services;
+
+namespace Authenticator.API.Controllers
+{
+    public class LoginController : ILoginController
+    {
+        private ILoginService loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            this.loginService = loginService;
+        }
+
+        public AuthenticationStatus Login(string userName, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
