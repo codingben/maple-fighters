7039f51ca0698b7a77b2b691ac948bc484cf6915
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-18 23:08:10 +0200

    :alien: A new script.

---
+++ src/auth-service/Authenticator.API/Controllers/IRegistrationController.cs
@@ -0,0 +1,10 @@
+﻿using Authenticator.API.Datas;
+using Authenticator.Domain.Aggregates.User.Services;
+
+namespace Authenticator.API.Controllers
+{
+    public interface IRegistrationController
+    {
+        AccountCreationStatus Register(RegistrationData registrationData);
+    }
+}
