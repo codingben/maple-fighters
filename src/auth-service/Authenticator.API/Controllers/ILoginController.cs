0151443f9b7eb348945cbc6dd60faef3974375b5
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-18 21:39:50 +0200

    :alien: A new script.

---
+++ src/auth-service/Authenticator.API/Controllers/ILoginController.cs
@@ -0,0 +1,9 @@
+﻿using Authenticator.Domain.Aggregates.User.Services;
+
+namespace Authenticator.API.Controllers
+{
+    public interface ILoginController
+    {
+        AuthenticationStatus Login(string userName, string password);
+    }
+}
