e61db78bdb39061996bb222aff4fe1ad72d5fe48
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-18 22:51:43 +0200

    :alien: A new validator.

---
+++ src/auth-service/Authenticator.API/Validators/RegistrationDataValidator.cs
@@ -0,0 +1,16 @@
+﻿using Authenticator.API.Datas;
+using FluentValidation;
+
+namespace Authenticator.API.Validators
+{
+    public class RegistrationDataValidator : AbstractValidator<RegistrationData>
+    {
+        public RegistrationDataValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Invalid email address.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.");
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required.");
+        }
+    }
+}
