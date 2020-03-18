88f01bb7aa36a1062104e5b2f3cb6cedec42a12c
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-18 23:06:42 +0200

    :alien: Code refactoring.

--- src/auth-service/Authenticator.API/Controllers/LoginController.cs
+++ src/auth-service/Authenticator.API/Controllers/LoginController.cs
@@ -1,19 +1,38 @@
-﻿using Authenticator.Domain.Aggregates.User.Services;
+﻿using Authenticator.API.Datas;
+using Authenticator.API.Validators;
+using Authenticator.Domain.Aggregates.User.Services;
 
 namespace Authenticator.API.Controllers
 {
     public class LoginController : ILoginController
     {
-        private ILoginService loginService;
+        private readonly ILoginService loginService;
+        private readonly AuthenticationDataValidator authenticationDataValidator;
 
         public LoginController(ILoginService loginService)
         {
             this.loginService = loginService;
+
+            authenticationDataValidator = new AuthenticationDataValidator();
         }
 
-        public AuthenticationStatus Login(string userName, string password)
+        public AuthenticationStatus Login(AuthenticationData authenticationData)
         {
-            throw new System.NotImplementedException();
+            var authenticationStatus = AuthenticationStatus.NotFound;
+
+            // TODO: Get errors if invalid
+            var validationResult =
+                authenticationDataValidator.Validate(authenticationData);
+            if (validationResult.IsValid)
+            {
+                var email = authenticationData.Email;
+                var password = authenticationData.Password;
+
+                authenticationStatus =
+                    loginService.Authenticate(email, password);
+            }
+
+            return authenticationStatus;
         }
     }
 }
