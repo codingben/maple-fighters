f44b835491d147eeb21b38c56cb03e3f43c4e655
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-23 21:36:11 +0200

    :alien: A new script.

---
+++ src/auth-service/Authenticator.Infrastructure/Repository/InMemoryAccountRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Authenticator.Domain.Aggregates.User;
+
+namespace Authenticator.Infrastructure.Repository
+{
+    public class InMemoryAccountRepository : IAccountRepository
+    {
+        private readonly List<Account> collection;
+
+        public InMemoryAccountRepository()
+        {
+            collection = new List<Account>();
+        }
+
+        public void Create(Account entity)
+        {
+            collection.Add(entity);
+        }
+
+        public void Delete(string id)
+        {
+            var account = collection.Find(x => x.Id == id);
+            if (account != null)
+            {
+                collection.Remove(account);
+            }
+        }
+
+        public void Update(Account entity)
+        {
+            var index = collection.FindIndex(x => x.Id == entity.Id);
+            collection[index] = entity;
+        }
+
+        public Account Read(Expression<Func<Account, bool>> predicate)
+        {
+            return collection.AsQueryable().SingleOrDefault(predicate);
+        }
+
+        public IEnumerable<Account> ReadMany(
+            Expression<Func<Account, bool>> predicate)
+        {
+            return collection.AsQueryable().Where(predicate);
+        }
+    }
+}
