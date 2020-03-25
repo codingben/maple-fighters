980fc105136409320776e82da4641255870ca553
Ben Ukhanov <ben1zuk321@gmail.com>
2020-03-25 22:59:23 +0200

    :alien: Added tests.

--- tests/unit/auth-service/Authenticator.UnitTests/Domain/User/Services/RegistrationServiceTests.cs
+++ tests/unit/auth-service/Authenticator.UnitTests/Domain/User/Services/RegistrationServiceTests.cs
@@ -1,7 +1,60 @@
-﻿namespace Authenticator.UnitTests.Domain.User.Services
+﻿using System;
+using System.Linq.Expressions;
+using Authenticator.Domain.Aggregates.User;
+using Authenticator.Domain.Aggregates.User.Services;
+using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace Authenticator.UnitTests.Domain.User.Services
 {
     public class RegistrationServiceTests
     {
-        // TODO: Implement
+        [Fact]
+        public void CreateAccount_Returns_EmailExists()
+        {
+            // Arrange
+            var account = AccountFactory.CreateAccount(
+                "benzuk@gmail.com",
+                "benzuk",
+                "Ben",
+                "Ben Ukhanov");
+            var accountRepository = Substitute.For<IAccountRepository>();
+            accountRepository
+                .Read(Arg.Any<Expression<Func<Account, bool>>>())
+                .Returns(account);
+            var registrationService = new RegistrationService(accountRepository);
+
+            // Act
+            var accountCreationStatus =
+                registrationService.CreateAccount(account);
+
+            // Assert
+            accountCreationStatus
+                .Equals(AccountCreationStatus.EmailExists)
+                .ShouldBeTrue();
+        }
+
+        [Fact]
+        public void CreateAccount_Returns_Succeed()
+        {
+            // Arrange
+            var account = AccountFactory.CreateAccount(
+                "benzuk@gmail.com",
+                "benzuk",
+                "Ben",
+                "Ben Ukhanov");
+            var accountRepository = Substitute.For<IAccountRepository>();
+            var registrationService = new RegistrationService(accountRepository);
+
+            // Act
+            var accountCreationStatus =
+                registrationService.CreateAccount(account);
+
+            // Assert
+            accountCreationStatus
+                .Equals(AccountCreationStatus.Succeed)
+                .ShouldBeTrue();
+        }
     }
 }
