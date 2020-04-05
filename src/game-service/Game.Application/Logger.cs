07c2ab28daa7ea9972e7953c8c719279addadb78
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-06 00:21:31 +0200

    :alien: A new script.

---
+++ src/game-service/Game.Application/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CommonTools.Log;
+
+namespace Game.Application
+{
+    public class Logger : ILogger
+    {
+        public void Log(string message, LogMessageType type = LogMessageType.Log, object context = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Break()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
