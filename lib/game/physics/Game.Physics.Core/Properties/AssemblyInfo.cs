094f7f721d4fecb64d019f821a4a5c234022168e
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-07 23:46:10 +0200

    :alien: New name for the project.

--- lib/game/physics/Game.Physics.Core/Properties/AssemblyInfo.cs
+++ lib/game/physics/Game.Physics.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Physics.Box2D.Core")]
+[assembly: AssemblyTitle("Game.Physics.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Physics.Box2D.Core")]
+[assembly: AssemblyProduct("Game.Physics.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
