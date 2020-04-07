90136ddc58797668171136815df3b4d1facbf6bc
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-07 23:49:51 +0200

    :alien: New name for the project.

--- lib/game/physics/Game.Physics.SimulationWindow/Properties/AssemblyInfo.cs
+++ lib/game/physics/Game.Physics.SimulationWindow/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Physics.Box2D.SimulationWindow")]
+[assembly: AssemblyTitle("Game.Physics.SimulationWindow")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Physics.Box2D.SimulationWindow")]
+[assembly: AssemblyProduct("Game.Physics.SimulationWindow")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
