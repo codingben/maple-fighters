18d2aec999abc8cbf49bb6d87f392bab8a31076b
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-13 23:13:32 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Graphics/BubbleMessage/BubbleMessageCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Scripts.Gameplay.Graphics
+{
+    [ExecuteInEditMode]
+    public class BubbleMessageCreator : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform owner;
+
+        [SerializeField]
+        private string message;
+
+        [SerializeField]
+        private int time;
+
+        private void Start()
+        {
+            BubbleMessage.Create(owner, message, time);
+        }
+
+        private void Update()
+        {
+            if (owner == null)
+            {
+                owner = transform;
+            }
+        }
+    }
+}
