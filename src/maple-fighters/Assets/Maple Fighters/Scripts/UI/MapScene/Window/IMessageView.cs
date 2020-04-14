34fd940a23cbfc1099bad964b0b601837cc4c4c5
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-14 03:17:46 +0200

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/MapScene/Window/IMessageView.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/MapScene/Window/IMessageView.cs
@@ -1,7 +1,11 @@
-﻿namespace Scripts.UI.MapScene
+﻿using System;
+
+namespace Scripts.UI.MapScene
 {
     public interface IMessageView
     {
+        Action TimeUp { get; set; }
+
         string Text { set; }
 
         float Seconds { set; }
