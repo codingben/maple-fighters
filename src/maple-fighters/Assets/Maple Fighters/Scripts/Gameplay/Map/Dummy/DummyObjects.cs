cf5381c0e7398c10211077549c248972b8790a38
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-14 04:02:57 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Map/Dummy/DummyObjects.cs
@@ -0,0 +1,25 @@
+﻿namespace Scripts.Gameplay.Map.Dummy
+{
+    public enum DummyObjects
+    {
+        /// <summary>
+        /// The local player.
+        /// </summary>
+        LocalPlayer,
+
+        /// <summary>
+        /// The portal.
+        /// </summary>
+        Portal,
+
+        /// <summary>
+        /// The guardian.
+        /// </summary>
+        Guardian,
+
+        /// <summary>
+        /// The blue snail.
+        /// </summary>
+        BlueSnail
+    }
+}
