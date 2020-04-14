8ca4e28ff960e0219290f2aeb5a2439e4b30185b
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-14 03:18:00 +0200

    :alien: Code refactoring.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/MapScene/Controller/MapSceneMessageCreator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UI.Manager;
+using UnityEngine;
+
+namespace Scripts.UI.MapScene
+{
+    public class MapSceneMessageCreator : MonoBehaviour
+    {
+        [SerializeField]
+        private string messageText;
+
+        [SerializeField]
+        private string tipText;
+
+        [SerializeField]
+        private float seconds = 2;
+
+        private void Awake()
+        {
+            CreateMessage(messageText, seconds, OnMessageTimeUp);
+        }
+
+        private void OnMessageTimeUp()
+        {
+            if (tipText != string.Empty)
+            {
+                CreateMessage(tipText, seconds);
+            }
+
+            Destroy(gameObject);
+        }
+
+        private void CreateMessage(
+            string message,
+            float seconds,
+            Action onTimeUp = null)
+        {
+            IMessageView messageView =
+                UIElementsCreator.GetInstance().Create<MessageText>();
+            messageView.Text = message;
+            messageView.Seconds = seconds;
+
+            if (onTimeUp != null)
+            {
+                messageView.TimeUp += onTimeUp;
+            }
+        }
+    }
+}
