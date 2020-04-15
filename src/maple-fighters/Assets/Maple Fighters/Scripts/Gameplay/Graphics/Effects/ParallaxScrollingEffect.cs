03a910c194b983956d7588c1bf4d352b1b29d821
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-15 02:40:09 +0200

    :alien: A new effect.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Graphics/Effects/ParallaxScrollingEffect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Scripts.Gameplay.Graphics
+{
+    public class ParallaxScrollingEffect : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] backgrounds;
+
+        [SerializeField]
+        private float speed;
+
+        private Transform mainCamera;
+        private Vector3 cameraPosition;
+
+        private void Awake()
+        {
+            mainCamera = UnityEngine.Camera.main.transform;
+            cameraPosition = mainCamera.position;
+        }
+
+        private void Update()
+        {
+            foreach (var background in backgrounds)
+            {
+                var x = background.position.x;
+                var y = background.position.y;
+                var a = background.position;
+                var b = new Vector3(
+                    x + (mainCamera.position.x - cameraPosition.x),
+                    y);
+                var t = speed * Time.deltaTime;
+
+                background.position = Vector3.Lerp(a, b, t);
+            }
+
+            cameraPosition = mainCamera.position;
+        }
+    }
+}
