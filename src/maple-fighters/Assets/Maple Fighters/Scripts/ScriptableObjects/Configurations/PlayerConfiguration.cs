7a7bf1d6c14fdc4a1fd5d343f69bfdfd415277cd
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-16 23:21:39 +0200

    :alien: A new configuration.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/ScriptableObjects/Configurations/PlayerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using ScriptableObjects.Utils;
+using Scripts.Gameplay.Player;
+using UnityEngine;
+
+namespace ScriptableObjects.Configurations
+{
+    [CreateAssetMenu(
+        fileName = "PlayerConfiguration",
+        menuName = "Configurations/PlayerConfiguration",
+        order = 2)]
+    public class PlayerConfiguration : ScriptableSingleton<PlayerConfiguration>
+    {
+        public PlayerProperties PlayerProperties;
+        public PlayerKeyboard PlayerKeyboard;
+    }
+}
