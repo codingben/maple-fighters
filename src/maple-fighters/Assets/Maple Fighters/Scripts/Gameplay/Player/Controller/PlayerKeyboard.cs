93a88b5d1436e803d1a056e55d5f1ef6236894ec
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-16 23:21:33 +0200

    :alien: A new script.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Controller/PlayerKeyboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts.Gameplay.Player
+{
+    [Serializable]
+    public class PlayerKeyboard
+    {
+        [Header("Keyboard")]
+        public KeyCode JumpKey = KeyCode.Space;
+        public KeyCode TeleportKey = KeyCode.T;
+        public KeyCode ClimbKey = KeyCode.C;
+    }
+}
