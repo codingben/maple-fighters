0329e55ab25417c744f7c5eb14ffbbc43f9912db
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-20 21:27:54 +0200

    :alien: Code refactoring.

--- lib/game/physics/Game.Physics/Core/LayerMask.cs
+++ lib/game/physics/Game.Physics/Core/LayerMask.cs
@@ -2,8 +2,19 @@
 {
     public enum LayerMask : short
     {
+        /// <summary>
+        /// The ground.
+        /// </summary>
         Ground,
+
+        /// <summary>
+        /// The player.
+        /// </summary>
         Player,
+
+        /// <summary>
+        /// The mob.
+        /// </summary>
         Mob
     }
 }
