2f3a70ac070fd5bcad09e65a6e017ebb4e1dd76a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-20 22:11:22 +0200

    :alien: Code refactoring.

---
+++ lib/game/physics/Game.Physics/Core/BodyUtils.cs
@@ -0,0 +1,35 @@
+﻿using Box2DX.Dynamics;
+using Common.MathematicsHelper;
+
+namespace Physics.Box2D.Core
+{
+    public static class BodyUtils
+    {
+        public static void MoveBody(Body body, Vector2 position, float speed)
+        {
+            var direction = position - body.GetPosition().ToVector2();
+            var distanceToTravel = direction.FromVector2().Normalize();
+            if (distanceToTravel > PhysicsSettings.MaxTravelDistance)
+            {
+                body.SetXForm(position.FromVector2(), body.GetAngle());
+            }
+            else
+            {
+                var distancePerTimestep =
+                    speed / PhysicsSettings.FramesPerSecond;
+                if (distancePerTimestep > distanceToTravel)
+                {
+                    speed *= distanceToTravel / distancePerTimestep;
+                }
+
+                var desiredVelocity = speed * direction;
+                var changeInVelocity =
+                    desiredVelocity - body.GetLinearVelocity().ToVector2();
+                var force =
+                    body.GetMass() * PhysicsSettings.FramesPerSecond * changeInVelocity;
+
+                body.ApplyForce(force.FromVector2(), body.GetWorldCenter());
+            }
+        }
+    }
+}
