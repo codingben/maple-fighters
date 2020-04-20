02924e41be9e4a29aa112af57d5d0fa889dcf02e
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-20 22:03:35 +0200

    :alien: A new script.

---
+++ lib/game/physics/Game.Physics/Core/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using Box2DX.Common;
+using Common.MathematicsHelper;
+
+namespace Physics.Box2D.Core
+{
+    public static class ExtensionMethods
+    {
+        public static Vector2 ToVector2(this Vec2 vec2)
+        {
+            return new Vector2(vec2.X, vec2.Y);
+        }
+
+        public static Vec2 FromVector2(this Vector2 vector2)
+        {
+            return new Vec2(vector2.X, vector2.Y);
+        }
+    }
+}
