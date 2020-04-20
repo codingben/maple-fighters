3f7ddb614c07609419235fed0120927660090d89
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-20 21:45:42 +0200

    :alien: Code refactoring.

--- lib/game/physics/Game.Physics/Core/PhysicsWorldInfo.cs
+++ lib/game/physics/Game.Physics/Core/PhysicsWorldInfo.cs
@@ -1,15 +1,22 @@
-﻿using MathematicsHelper;
+﻿using Common.MathematicsHelper;
 
 namespace Physics.Box2D.Core
 {
     public struct PhysicsWorldInfo
     {
-        public readonly Vector2 LowerBound;
-        public readonly Vector2 UpperBound;
-        public readonly Vector2 Gravity;
-        public readonly bool DoSleep;
+        public Vector2 LowerBound { get; }
 
-        public PhysicsWorldInfo(Vector2 lowerBound, Vector2 upperBound, Vector2 gravity, bool doSleep = true)
+        public Vector2 UpperBound { get; }
+
+        public Vector2 Gravity { get; }
+
+        public bool DoSleep { get; }
+
+        public PhysicsWorldInfo(
+            Vector2 lowerBound,
+            Vector2 upperBound,
+            Vector2 gravity,
+            bool doSleep = true)
         {
             LowerBound = lowerBound;
             UpperBound = upperBound;
