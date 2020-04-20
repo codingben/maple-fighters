c472d388f60ca536848d4b1cc80db7804e762aea
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-20 22:01:05 +0200

    :alien: Code refactoring.

--- lib/game/physics/Game.Physics/Core/PhysicsUtils.cs
+++ lib/game/physics/Game.Physics/Core/PhysicsUtils.cs
@@ -62,7 +62,7 @@ namespace Physics.Box2D.Core
         }
 
         public static Body CreateCharacter(
-            this World world,
+            World world,
             BodyDef bodyDefinition,
             PolygonDef polygonDefinition)
         {
@@ -74,30 +74,28 @@ namespace Physics.Box2D.Core
             return body;
         }
 
-        public static void MoveBody(
-            Body body,
-            Vector2 position,
-            float speed,
-            bool teleport = true)
+        public static void MoveBody(Body body, Vector2 position, float speed)
         {
             var direction = position - body.GetPosition().ToVector2();
             var distanceToTravel = direction.FromVector2().Normalize();
-
-            if (teleport && (distanceToTravel > TeleportDistance))
+            if (distanceToTravel > PhysicsSettings.MaxTravelDistance)
             {
                 body.SetXForm(position.FromVector2(), body.GetAngle());
             }
             else
             {
-                var distancePerTimestep = speed / FramesPerSecond;
+                var distancePerTimestep =
+                    speed / PhysicsSettings.FramesPerSecond;
                 if (distancePerTimestep > distanceToTravel)
                 {
                     speed *= distanceToTravel / distancePerTimestep;
                 }
 
                 var desiredVelocity = speed * direction;
-                var changeInVelocity = desiredVelocity - body.GetLinearVelocity().ToVector2();
-                var force = body.GetMass() * FramesPerSecond * changeInVelocity;
+                var changeInVelocity =
+                    desiredVelocity - body.GetLinearVelocity().ToVector2();
+                var force = 
+                    body.GetMass() * PhysicsSettings.FramesPerSecond * changeInVelocity;
 
                 body.ApplyForce(force.FromVector2(), body.GetWorldCenter());
             }
