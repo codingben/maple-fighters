5a884a1f71e150839b99daf77e8619c94841668d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-20 22:31:19 +0200

    :alien: Code refactoring.

---
+++ lib/game/physics/Game.Physics/Components/PhysicsWorldSimulation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Box2DX.Collision;
+using Box2DX.Dynamics;
+using Physics.Box2D.Core;
+
+namespace Physics.Box2D.Components
+{
+    public class PhysicsWorldSimulation : IDisposable
+    {
+        private readonly World world;
+
+        public PhysicsWorldSimulation(PhysicsWorldInfo worldInfo)
+        {
+            var worldAabb = new AABB
+            {
+                LowerBound = worldInfo.LowerBound.FromVector2(),
+                UpperBound = worldInfo.UpperBound.FromVector2()
+            };
+            var gravity = worldInfo.Gravity.FromVector2();
+            var doSleep = worldInfo.DoSleep;
+
+            world = new World(worldAabb, gravity, doSleep);
+            world.SetContactFilter(new GroupContactFilter());
+            world.SetContactListener(new BodyContactListener());
+            world.SetContinuousPhysics(false);
+        }
+
+        private void SimulateWorld()
+        {
+            var timeStep = PhysicsSettings.TimeStep;
+            var velocityIterations = PhysicsSettings.VelocityIterations;
+            var positionIterations = PhysicsSettings.PositionIterations;
+
+            world.Step(timeStep, velocityIterations, positionIterations);
+        }
+
+        public void Dispose()
+        {
+            world.SetDebugDraw(null);
+            world.Dispose();
+        }
+    }
+}
