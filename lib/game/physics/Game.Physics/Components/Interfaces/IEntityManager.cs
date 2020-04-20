37ef42d4b0e0ea04b7e64172b6bbe01dff4be8d3
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-20 22:28:59 +0200

    :alien: Code refactoring.

--- lib/game/physics/Game.Physics/Components/Interfaces/IEntityManager.cs
+++ lib/game/physics/Game.Physics/Components/Interfaces/IEntityManager.cs
@@ -5,7 +5,10 @@ namespace Physics.Box2D.Components.Interfaces
 {
     public interface IEntityManager
     {
+        void Update();
+
         void AddBody(BodyInfo bodyInfo);
+
         void RemoveBody(int id);
 
         Body GetBody(int id);
