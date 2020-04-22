daab68e56dd65b9af609292e42fb082a78e98aef
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-22 20:22:02 +0200

    :alien: Code refactoring.

--- lib/game/physics/Game.Physics/PhysicsSettings.cs
+++ lib/game/physics/Game.Physics/PhysicsSettings.cs
@@ -2,11 +2,16 @@
 {
     public static class PhysicsSettings
     {
-        public static float TimeStep = 1.0f / 25.0f; // (25Hz Simulation)
-        public static int VelocityIterations = 8;
-        public static int PositionIterations = 3;
-        public static float UpdatesPerSecond = 30.0f;
-        public static float FramesPerSecond = 30.0f;
-        public static float MaxTravelDistance = 1.0f;
+        public static float TimeStep { get; set; } = 1.0f / 25.0f; // (25Hz)
+
+        public static int VelocityIterations { get; set; } = 8;
+
+        public static int PositionIterations { get; set; } = 3;
+
+        public static float UpdatesPerSecond { get; set; } = 30.0f;
+
+        public static float FramesPerSecond { get; set; } = 30.0f;
+
+        public static float MaxTravelDistance { get; set; } = 1.0f;
     }
 }
