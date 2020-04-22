878a6aeaf928e50aaa3546b90a91601466b3f617
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-22 20:18:09 +0200

    :alien: Code refactoring.

---
+++ lib/game/physics/Game.Physics/MathExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using Box2DX.Common;
+using Common.MathematicsHelper;
+
+namespace Game.Physics
+{
+    public static class MathExtensionMethods
+    {
+        public static Vector2 ToVector2(this Vec2 vec2)
+        {
+            return new Vector2(vec2.X, vec2.Y);
+        }
+
+        public static Vec2 FromVector2(this Vector2 vector2)
+        {
+            return new Vec2(vector2.X, vector2.Y);
+        }
+    }
+}
