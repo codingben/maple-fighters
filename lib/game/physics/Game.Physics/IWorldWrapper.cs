9797b3f4f9921c6d3009ad9df635ef1df55457d9
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-22 20:18:01 +0200

    :alien: Code refactoring.

---
+++ lib/game/physics/Game.Physics/IWorldWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Box2DX.Dynamics;
+
+namespace Game.Physics
+{
+    public interface IWorldWrapper : IDisposable
+    {
+        Body CreateBody(BodyDef bodyDefinition, PolygonDef polygonDefinition);
+
+        void DestroyBody(Body body);
+
+        World GetWorld();
+    }
+}
