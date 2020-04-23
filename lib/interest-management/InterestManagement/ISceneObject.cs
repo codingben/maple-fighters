c090133932af79007d7b61a24ca0a240d9a66ed5
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-23 19:57:43 +0200

    :alien: Code refactoring.

---
+++ lib/interest-management/InterestManagement/ISceneObject.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.InterestManagement
+{
+    public interface ISceneObject
+    {
+        /// <summary>
+        /// Gets scene object id.
+        /// </summary>
+        int Id { get; }
+
+        /// <summary>
+        /// Gets the transform of the scene object.
+        /// </summary>
+        ITransform Transform { get; }
+    }
+}
---
+++ lib/interest-management/InterestManagement/NearbySceneObjectsCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game.InterestManagement
+{
+    public class NearbySceneObjectsCollection<TObject> : INearbySceneObjectsEvents<TObject>
+        where TObject : ISceneObject
+    {
+        public event Action<TObject> SceneObjectAdded;
+
+        public event Action<TObject> SceneObjectRemoved;
+
+        public event Action<IEnumerable<TObject>> SceneObjectsAdded;
+
+        public event Action<IEnumerable<TObject>> SceneObjectsRemoved;
+
+        private readonly HashSet<TObject> collection;
+
+        public NearbySceneObjectsCollection()
+        {
+            collection = new HashSet<TObject>();
+        }
+
+        public void Dispose()
+        {
+            collection?.Clear();
+        }
+
+        public void Add(IEnumerable<TObject> sceneObjects)
+        {
+            var visibleSceneObjects =
+                sceneObjects
+                    .Where(
+                        sceneObject =>
+                            collection.Add(sceneObject))
+                    .ToArray();
+
+            if (visibleSceneObjects.Length != 0)
+            {
+                SceneObjectsAdded?.Invoke(visibleSceneObjects);
+            }
+        }
+
+        public void Add(TObject sceneObject)
+        {
+            if (collection.Add(sceneObject))
+            {
+                SceneObjectAdded?.Invoke(sceneObject);
+            }
+        }
+
+        public void Remove(IEnumerable<TObject> sceneObjects)
+        {
+            var invisibleSceneObjects =
+                sceneObjects
+                    .Where(
+                        sceneObject =>
+                            collection.Remove(sceneObject))
+                    .ToArray();
+
+            if (invisibleSceneObjects.Length != 0)
+            {
+                SceneObjectsRemoved?.Invoke(invisibleSceneObjects);
+            }
+        }
+
+        public void Remove(TObject sceneObject)
+        {
+            if (collection.Remove(sceneObject))
+            {
+                SceneObjectRemoved?.Invoke(sceneObject);
+            }
+        }
+    }
+}
