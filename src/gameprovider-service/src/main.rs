5098008994ef77a8eea117ee28f36185cacb64dc
Ben Ukhanov <ben1zuk321@gmail.com>
2020-04-30 22:49:37 +0200

    :alien: Code refactoring.

--- src/gameprovider-service/src/main.rs
+++ src/gameprovider-service/src/main.rs
@@ -3,11 +3,9 @@ use models::*;
 
 fn main() {
     let mut game_provider = GameProvider { games: Vec::new() };
-    game_provider
-        .games
-        .push(Game::new("My Game", "127.0.0.1", 1001));
+    game_provider.add(Game::new("My Game", "127.0.0.1", 1001));
 
-    for game in game_provider.games.iter() {
+    for game in game_provider.get_all() {
         println!("Server: {} IP: {}:{}", game.name, game.ip, game.port);
     }
 }
