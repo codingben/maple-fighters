724d54ee4b7298ec38b33092728c51dd0110edab
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-01 23:55:05 +0200

    :alien: Added game data from JSON.

--- src/gameprovider-service/src/main.rs
+++ src/gameprovider-service/src/main.rs
@@ -1,11 +1,22 @@
 mod models;
 use models::*;
+use serde_json::Result;
 
-fn main() {
+fn main() -> Result<()> {
+    let data = r#"
+    {
+        "name": "Game 1",
+        "ip": "127.0.0.1",
+        "port": 1025
+    }"#;
+
+    let game: models::Game = serde_json::from_str(&data)?;
     let mut gameprovider = GameProvider::new();
-    gameprovider.add(Game::new("My Game", "127.0.0.1", 1001));
+    gameprovider.add(Game::new(game.name, game.ip, game.port));
 
-    for game in gameprovider.get_all() {
-        println!("Server: {} IP: {}:{}", game.name, game.ip, game.port);
+    for x in gameprovider.get_all() {
+        println!("Server: {} IP: {}:{}", x.name, x.ip, x.port);
     }
+
+    Ok(())
 }
