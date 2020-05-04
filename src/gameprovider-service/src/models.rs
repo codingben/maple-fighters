5802273e66d740ba45c3bacd687e28bab011e933
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-04 21:21:22 +0200

    :alien: Code refactoring.

--- src/gameprovider-service/src/models.rs
+++ src/gameprovider-service/src/models.rs
@@ -1,16 +1,19 @@
 use serde::Deserialize;
+use serde_json;
+use std::fs::File;
+use std::path::Path;
 
 #[derive(Deserialize)]
-pub struct Game {
+pub struct GameServer {
     pub name: String,
     pub ip: String,
     pub port: i32,
 }
 
 #[allow(dead_code)]
-impl Game {
-    pub fn new(name: String, ip: String, port: i32) -> Game {
-        Game {
+impl GameServer {
+    pub fn new(name: String, ip: String, port: i32) -> GameServer {
+        GameServer {
             name: name,
             ip: ip,
             port: port,
@@ -19,21 +22,25 @@ impl Game {
 }
 
 #[derive(Deserialize)]
-pub struct GameProvider {
-    pub games: Vec<Game>,
+pub struct GameServerCollection {
+    pub game_servers: Vec<GameServer>,
 }
 
 #[allow(dead_code)]
-impl GameProvider {
-    pub fn new() -> GameProvider {
-        GameProvider { games: Vec::new() }
+impl GameServerCollection {
+    pub fn new(path: &str) -> GameServerCollection {
+        let json_file_path = Path::new(path);
+        let json_file = File::open(json_file_path).expect("File not found");
+        let gameprovider: GameServerCollection =
+            serde_json::from_reader(json_file).expect("Could not read json");
+        return gameprovider;
     }
 
-    pub fn add(&mut self, game: Game) {
-        self.games.push(game);
+    pub fn add(&mut self, game_server: GameServer) {
+        self.game_servers.push(game_server);
     }
 
-    pub fn get_all(&self) -> impl Iterator<Item = &Game> {
-        self.games.iter()
+    pub fn get_all(&self) -> impl Iterator<Item = &GameServer> {
+        return self.game_servers.iter();
     }
 }
