ea5cf5b8cc26e8753b548a6717593de104e72941
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-10 21:30:50 +0200

    :alien: Code refactoring.

--- src/gameprovider-service/build.rs
+++ src/gameprovider-service/build.rs
@@ -1,4 +1,6 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
