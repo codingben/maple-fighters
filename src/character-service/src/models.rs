4f803e43f37df5cb0a9f76f667616fe505f279ae
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-11 23:30:14 +0200

    :alien: Added insert function.

--- src/character-service/src/models.rs
+++ src/character-service/src/models.rs
@@ -1,3 +1,11 @@
+use diesel;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+
+use schema::characters;
+use schema::characters::dsl::*;
+
+#[derive(Queryable)]
 pub struct Character {
     pub id: i32,
     pub userid: i32,
@@ -5,4 +13,23 @@ pub struct Character {
     pub index: i32,
     pub classindex: i32,
     pub mapindex: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "characters"]
+pub struct NewCharacter {
+    pub userid: i32,
+    pub charactername: String,
+    pub index: i32,
+    pub classindex: i32,
+    pub mapindex: i32,
+}
+
+impl Character {
+    pub fn insert(character: NewCharacter, conn: &PgConnection) -> bool {
+        diesel::insert_into(characters::table)
+            .values(&character)
+            .execute(connection)
+            .is_ok()
+    }
 }
