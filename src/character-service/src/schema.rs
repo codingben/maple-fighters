98ec23590fed29e383fb304e089b35a387176eb1
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-11 22:49:51 +0200

    :alien: Added schema.

---
+++ src/character-service/src/schema.rs
@@ -0,0 +1,10 @@
+table! {
+    characters (id) {
+        id -> Int4,
+        userid -> Int4,
+        charactername -> Varchar,
+        index -> Int4,
+        classindex -> Int4,
+        mapindex -> Int4,
+    }
+}
