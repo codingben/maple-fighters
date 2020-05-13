b3e6f397abe634876ac1a48252f01518eec761ce
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-13 23:03:35 +0200

    :alien: Removed migration.

--- src/character-service/migrations/2020-05-11-201943_create_characters/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE characters (
-    id SERIAL PRIMARY KEY,
-    userid INTEGER NOT NULL,
-    charactername VARCHAR NOT NULL,
-    index INTEGER NOT NULL,
-    classindex INTEGER NOT NULL,
-    mapindex INTEGER NOT NULL
-)
