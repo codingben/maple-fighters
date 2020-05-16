b9478e63e82dad7de2016d800ea63f3d3b522ebb
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-16 23:39:15 +0200

    :alien: A new script.

---
+++ src/gameprovider-service/src/gameprovider.rs
@@ -0,0 +1,19 @@
+use super::game_provider::{game_provider_server::GameProvider, *};
+use tonic::{Request, Response, Status};
+
+#[derive(Debug, Default)]
+pub struct GameProviderImpl {
+    pub game_servers: Vec<Game>,
+}
+
+#[tonic::async_trait]
+impl GameProvider for GameProviderImpl {
+    async fn get_game_servers(
+        &self,
+        _request: Request<()>,
+    ) -> Result<Response<GetGameServersResponse>, Status> {
+        Ok(Response::new(GetGameServersResponse {
+            game_collection: self.game_servers.to_vec(),
+        }))
+    }
+}
