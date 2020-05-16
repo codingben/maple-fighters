f5a5dd0f872a295a22ecc8de5a9f11f2f5b25669
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-16 23:05:43 +0200

    :alien: Code refactoring.

--- src/gameprovider-service/src/models.rs
+++ src/gameprovider-service/src/models.rs
@@ -1,6 +1,4 @@
 use serde::Deserialize;
-use serde_json;
-use std::{fs::File, path::Path};
 
 #[derive(Deserialize)]
 pub struct GameServer {
@@ -17,29 +15,5 @@ impl GameServer {
             ip: ip,
             port: port,
         }
-    }
-}
-
-#[derive(Deserialize)]
-pub struct GameServerCollection {
-    pub game_servers: Vec<GameServer>,
-}
-
-#[allow(dead_code)]
-impl GameServerCollection {
-    pub fn new(path: &str) -> GameServerCollection {
-        let json_file_path = Path::new(path);
-        let json_file = File::open(json_file_path).expect("File not found");
-        let gameprovider: GameServerCollection =
-            serde_json::from_reader(json_file).expect("Could not read json");
-        return gameprovider;
-    }
-
-    pub fn add(&mut self, game_server: GameServer) {
-        self.game_servers.push(game_server);
-    }
-
-    pub fn get_all(&self) -> impl Iterator<Item = &GameServer> {
-        return self.game_servers.iter();
     }
 }
