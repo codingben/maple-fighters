2752f0c2cb61a733cc9a3d196989097428cce555
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-16 23:39:25 +0200

    :alien: Code refactoring.

--- src/gameprovider-service/src/main.rs
+++ src/gameprovider-service/src/main.rs
@@ -1,33 +1,14 @@
+mod database;
+mod gameprovider;
+mod models;
 mod game_provider {
     tonic::include_proto!("game_provider");
 }
-mod database;
-mod models;
 
 use dotenv::dotenv;
-use game_provider::{
-    game_provider_server::{GameProvider, GameProviderServer},
-    *,
-};
+use game_provider::{game_provider_server::GameProviderServer, *};
 use std::{env, error::Error};
-use tonic::{transport::Server, Request, Response, Status};
-
-#[derive(Debug, Default)]
-pub struct GameProviderImpl {
-    game_servers: Vec<Game>,
-}
-
-#[tonic::async_trait]
-impl GameProvider for GameProviderImpl {
-    async fn get_game_servers(
-        &self,
-        _request: Request<()>,
-    ) -> Result<Response<GetGameServersResponse>, Status> {
-        Ok(Response::new(GetGameServersResponse {
-            game_collection: self.game_servers.to_vec(),
-        }))
-    }
-}
+use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -36,7 +17,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let address = env::var("IP_ADDRESS").expect("IP_ADDRESS not found");
     let data_path = env::var("DATABASE_PATH").expect("DATABASE_PATH not found");
     let address_parsed = address.parse()?;
-    let mut game_provider = GameProviderImpl::default();
+
+    let mut game_provider = gameprovider::GameProviderImpl::default();
     let game_server_collection = database::GameServerCollection::new(&data_path);
     for game_server in game_server_collection.get_all() {
         game_provider.game_servers.push(Game {
