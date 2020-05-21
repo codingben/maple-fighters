a1ec32b04ff0fa19ad289228d84d1686d9ab0b6d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-21 19:25:21 +0200

    :alien: Code refactoring.

--- src/gameprovider-service/src/main.rs
+++ src/gameprovider-service/src/main.rs
@@ -5,10 +5,9 @@ mod game_provider {
     tonic::include_proto!("game_provider");
 }
 
-use database::GameServerCollection;
 use dotenv::dotenv;
 use endpoints::GameProviderService;
-use game_provider::{game_provider_server::GameProviderServer, *};
+use game_provider::game_provider_server::GameProviderServer;
 use std::{env, error::Error};
 use tonic::transport::Server;
 
@@ -19,16 +18,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let address = env::var("IP_ADDRESS").expect("IP_ADDRESS not found");
     let data_path = env::var("DATABASE_PATH").expect("DATABASE_PATH not found");
     let address_parsed = address.parse()?;
-
-    let mut game_provider_service = GameProviderService::default();
-    let game_server_collection = GameServerCollection::new(&data_path);
-    for game_server in game_server_collection.get_all() {
-        game_provider_service.game_servers.push(Game {
-            name: game_server.name.clone(),
-            ip: game_server.ip.clone(),
-            port: game_server.port.clone(),
-        });
-    }
+    let game_provider_service = GameProviderService {
+        game_servers: database::load(&data_path),
+    };
 
     Server::builder()
         .add_service(GameProviderServer::new(game_provider_service))
