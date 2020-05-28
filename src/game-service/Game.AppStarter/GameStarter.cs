7481669d84a95aed5ec3755d5cabcceb2796df91
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-28 23:57:42 +0200

    :alien: Added script.

---
+++ src/game-service/Game.AppStarter/GameStarter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Game.Application;
+
+namespace Game.AppStarter
+{
+    public static class GameStarter
+    {
+        public static void Main()
+        {
+            var gameApplication = new GameApplication("ws://localhost:50060");
+            gameApplication.Startup();
+
+            Console.ReadLine();
+
+            gameApplication.Shutdown();
+        }
+    }
+}
