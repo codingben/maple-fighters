a40e6424bc9db6b1507c63f44ad518033ced0378
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-28 23:56:13 +0200

    :alien: Added script.

---
+++ src/game-service/Game.Application/MessageCodes.cs
@@ -0,0 +1,20 @@
+namespace Game.Application
+{
+    public enum MessageCodes
+    {
+        EnterScene,
+        ChangeScene,
+        ChangePosition,
+        ChangePlayerState,
+        SceneObjectAdded,
+        SceneObjectRemoved,
+        SceneObjectsAdded,
+        SceneObjectsRemoved,
+        CharacterAdded,
+        CharactersAdded,
+        PositionChanged,
+        PlayerStateChanged,
+        PlayerAttacked,
+        BubbleNotification
+    }
+}
