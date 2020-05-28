4e3cad78b11867ead58846cc09eca6e79bd66146
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-28 23:03:22 +0200

    :alien: New Game.AppStarter project.

---
+++ src/game-service/Game.AppStarter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game.AppStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
