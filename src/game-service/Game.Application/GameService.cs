aad2dd297a0697114613ae48cc77976cf53faa81
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-28 23:57:08 +0200

    :alien: Added script.

---
+++ src/game-service/Game.Application/GameService.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Game.Application
+{
+    public class GameService : WebSocketBehavior
+    {
+        private readonly Dictionary<byte, IMessageHandler> handlers;
+
+        public GameService()
+        {
+            handlers = new Dictionary<byte, IMessageHandler>();
+        }
+
+        protected override void OnOpen()
+        {
+            handlers.Add((byte)MessageCodes.ChangePosition, new ChangePositionMessageHandler());
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            handlers.Remove((byte)MessageCodes.ChangePosition);
+        }
+
+        protected override void OnError(WebSocketSharp.ErrorEventArgs e)
+        {
+            // TODO: Log $"e.Message"
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (e.IsBinary)
+            {
+                var message = MessageUtils.GetMessage<MessageData>(e.RawData);
+                var code = message.Code;
+                var rawData = message.RawData;
+
+                if (handlers.TryGetValue(code, out var handler))
+                {
+                    handler?.Handle(rawData);
+                }
+            }
+            else
+            {
+                // TODO: Log "Only binary data is allowed."
+            }
+        }
+    }
+}
