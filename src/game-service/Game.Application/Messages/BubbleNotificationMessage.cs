bfaf1d470991a3de19ffd973c0a80f24811822f5
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-29 16:29:17 +0200

    :alien: Added message script.

---
+++ src/game-service/Game.Application/Messages/BubbleNotificationMessage.cs
@@ -0,0 +1,11 @@
+namespace Game.Application
+{
+    public class BubbleNotificationMessage
+    {
+        public int RequesterId { get; set; }
+
+        public string Message { get; set; }
+
+        public int Time { get; set; }
+    }
+}
