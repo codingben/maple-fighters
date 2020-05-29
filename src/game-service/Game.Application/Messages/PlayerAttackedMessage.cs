58e24a7b8b882ee3c2c6887ab35d794ddc218c7a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-29 18:35:22 +0200

    :alien: Added message script.

---
+++ src/game-service/Game.Application/Messages/PlayerAttackedMessage.cs
@@ -0,0 +1,9 @@
+namespace Game.Application
+{
+    public class PlayerAttackedMessage
+    {
+        public float ContactPointX { get; set; }
+
+        public float ContactPointY { get; set; }
+    }
+}
