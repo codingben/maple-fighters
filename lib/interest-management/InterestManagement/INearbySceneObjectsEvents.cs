1c00ba60305d48e6dfb312beeafe5a3f5797a0ab
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-30 18:39:17 +0200

    :alien: Code refactoring.

--- lib/interest-management/InterestManagement/INearbySceneObjectsEvents.cs
+++ lib/interest-management/InterestManagement/INearbySceneObjectsEvents.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 
 namespace InterestManagement
 {
-    public interface INearbySceneObjectsEvents<out TObject> : IDisposable
-        where TObject : ISceneObject
+    public interface INearbySceneObjectsEvents<out TSceneObject> : IDisposable
+        where TSceneObject : ISceneObject
     {
         /// <summary>
         /// Occurs when a new scene object has been entered to the region.
         /// </summary>
-        event Action<TObject> SceneObjectAdded;
+        event Action<TSceneObject> SceneObjectAdded;
 
         /// <summary>
         /// Occurs when a scene object has been left the region.
         /// </summary>
-        event Action<TObject> SceneObjectRemoved;
+        event Action<TSceneObject> SceneObjectRemoved;
 
         /// <summary>
         /// Sends the new visible scene objects when entered the region.
         /// </summary>
-        event Action<IEnumerable<TObject>> SceneObjectsAdded;
+        event Action<IEnumerable<TSceneObject>> SceneObjectsAdded;
 
         /// <summary>
         /// Sends the invisible scene objects when left the region.
         /// </summary>
-        event Action<IEnumerable<TObject>> SceneObjectsRemoved;
+        event Action<IEnumerable<TSceneObject>> SceneObjectsRemoved;
     }
 }
