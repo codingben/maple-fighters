ae72429eb2e6b3621abcaddb51b5a9bb198467ec
Ben Ukhanov <ben1zuk321@gmail.com>
2020-05-30 18:39:13 +0200

    :alien: Code refactoring.

--- lib/interest-management/InterestManagement/IMatrixRegion.cs
+++ lib/interest-management/InterestManagement/IMatrixRegion.cs
@@ -4,20 +4,20 @@ using Common.MathematicsHelper;
 
 namespace InterestManagement
 {
-    public interface IMatrixRegion<TObject> : IDisposable
-        where TObject : ISceneObject
+    public interface IMatrixRegion<TSceneObject> : IDisposable
+        where TSceneObject : ISceneObject
     {
         /// <summary>
         /// Gets the regions by the scene object position.
         /// </summary>
         /// <param name="points">The points which overlaps with the regions.</param>
         /// <returns>The relevant regions.</returns>
-        IEnumerable<IRegion<TObject>> GetRegions(IEnumerable<Vector2> points);
+        IEnumerable<IRegion<TSceneObject>> GetRegions(IEnumerable<Vector2> points);
 
         /// <summary>
         /// Gets all the regions in the scene.
         /// </summary>
         /// <returns>The regions.</returns>
-        IRegion<TObject>[,] GetAllRegions();
+        IRegion<TSceneObject>[,] GetAllRegions();
     }
 }
