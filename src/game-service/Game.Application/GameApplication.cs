f9f60613803f861071c76f3b1fcd05d44264f8d1
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-05 21:41:52 +0200

    :alien: Added components container.

--- src/game-service/Game.Application/GameApplication.cs
+++ src/game-service/Game.Application/GameApplication.cs
@@ -1,3 +1,4 @@
+using Common.ComponentModel;
 using WebSocketSharp.Server;
 
 namespace Game.Application
@@ -5,15 +6,17 @@ namespace Game.Application
     public class GameApplication
     {
         private readonly WebSocketServer webSocketServer;
+        private readonly IExposedComponents components;
 
         public GameApplication(string url)
         {
             webSocketServer = new WebSocketServer(url);
+            components = new ComponentsContainer();
         }
 
         public void Startup()
         {
-            webSocketServer.AddWebSocketService<GameService>("/game");
+            webSocketServer.AddWebSocketService("/game", () => new GameService(components));
             webSocketServer.Start();
         }
 
