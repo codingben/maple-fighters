04d27d0aba88bf8635da6d710358b5506523ecaa
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-05 21:52:50 +0200

    :alien: Created player game object.

--- src/game-service/Game.Application/GameService.cs
+++ src/game-service/Game.Application/GameService.cs
@@ -1,21 +1,30 @@
+using Common.ComponentModel;
 using System.Collections.Generic;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using Game.Application.Objects;
+using Common.MathematicsHelper;
+using Common.Components;
 
 namespace Game.Application
 {
     public class GameService : WebSocketBehavior
     {
+        private readonly IExposedComponents components;
         private readonly Dictionary<byte, IMessageHandler> handlers;
+        private readonly GameObject player;
 
-        public GameService()
+        public GameService(IExposedComponents components)
         {
+            this.components = components;
+
             handlers = new Dictionary<byte, IMessageHandler>();
+            player = CreatePlayerGameObject();
         }
 
         protected override void OnOpen()
         {
-            handlers.Add((byte)MessageCodes.ChangePlayerPosition, new ChangePositionMessageHandler());
+            handlers.Add((byte)MessageCodes.ChangePlayerPosition, new ChangePositionMessageHandler(player));
         }
 
         protected override void OnClose(CloseEventArgs eventArgs)
@@ -45,6 +54,20 @@ namespace Game.Application
             {
                 // TODO: Log "Only binary data is allowed."
             }
+        }
+
+        private GameObject CreatePlayerGameObject()
+        {
+            var idGenerator = components.Get<IIdGenerator>();
+            var id = idGenerator.GenerateId();
+            var name = "Player"; // (Map from Object Type to Name)
+            var player = new GameObject(id, name);
+            player.Transform.SetPosition(Vector2.Zero);
+            player.Transform.SetSize(Vector2.One);
+
+            // Add components
+
+            return player;
         }
     }
 }
