c7b5c70e7851fa2b4216a7fa15f336f48912cf54
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-05 21:51:56 +0200

    :alien: Implemented handler.

--- src/game-service/Game.Application/Handlers/ChangePositionMessageHandler.cs
+++ src/game-service/Game.Application/Handlers/ChangePositionMessageHandler.cs
@@ -1,12 +1,24 @@
+using Game.Application.Objects;
+using Common.MathematicsHelper;
+
 namespace Game.Application
 {
     public class ChangePositionMessageHandler : IMessageHandler
     {
+        private readonly GameObject player;
+
+        public ChangePositionMessageHandler(GameObject player)
+        {
+            this.player = player;
+        }
+
         public void Handle(byte[] rawData)
         {
             var message = MessageUtils.GetMessage<ChangePlayerPositionMessage>(rawData);
             var x = message.X;
             var y = message.Y;
+
+            player.Transform.SetPosition(new Vector2(x, y));
         }
     }
 }
