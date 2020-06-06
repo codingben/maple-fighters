07dc4bd5863970427b394462c1f164bc7052233f
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-06 18:15:41 +0200

    :alien: Code refactoring.

--- src/game-service/Game.Application/Components/IGameSceneContainer.cs
+++ src/game-service/Game.Application/Components/IGameSceneContainer.cs
@@ -1,9 +1,10 @@
+using Game.Application.Objects;
 using InterestManagement;
 
 namespace Game.Application.Components
 {
     public interface IGameSceneContainer
     {
-        bool TryGetScene(Map map, out IScene<ISceneObject> scene);
+        bool TryGetScene(Map map, out IScene<IGameObject> scene);
     }
 }
