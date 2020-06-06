cb7e31e99a02358168d376e4b95005c56e31cf23
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-06 20:14:07 +0200

    :alien: Code refactoring.

--- src/game-service/Game.Application/GameApplication.cs
+++ src/game-service/Game.Application/GameApplication.cs
@@ -1,36 +1,45 @@
 using Common.ComponentModel;
 using Common.Components;
+using Game.Application.Components;
 using WebSocketSharp.Server;
 
 namespace Game.Application
 {
-    public class GameApplication
+    public class GameApplication : WebSocketServer, IServerApplication
     {
-        private readonly WebSocketServer webSocketServer;
-        private readonly IExposedComponents components;
+        private readonly IComponents components;
 
         public GameApplication(string url)
+            : base(url)
         {
-            webSocketServer = new WebSocketServer(url);
             components = new ComponentsContainer();
         }
 
         public void Startup()
         {
             AddCommonComponents();
+            AddWebSocketService("/game", () => new GameService((IExposedComponents)components));
 
-            webSocketServer.AddWebSocketService("/game", () => new GameService(components));
-            webSocketServer.Start();
+            Start();
         }
 
         public void Shutdown()
         {
-            webSocketServer.Stop();
+            RemoveCommonComponents();
+            RemoveWebSocketService("/game");
+
+            Stop();
         }
 
         private void AddCommonComponents()
         {
             components.Add(new IdGenerator());
+            components.Add(new GameSceneContainer());
+        }
+
+        private void RemoveCommonComponents()
+        {
+            components.Dispose();
         }
     }
 }
