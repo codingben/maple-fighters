c4cbe8457e1c62cdd7a9571bc0d66d33b23e3d89
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-10 21:18:56 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Objects/GameObjectTransform.cs
+++ src/game-service/Game.Application/Objects/GameObjectTransform.cs
@@ -1,14 +1,15 @@
-using System;
+﻿using System;
 using Common.MathematicsHelper;
-using InterestManagement;
 
 namespace Game.Application.Objects
 {
-    public class Transform : ITransform
+    public class GameObjectTransform : IGameObjectTransform
     {
         public Vector2 Position { get; private set; }
 
         public Vector2 Size { get; private set; }
+
+        public byte Direction { get; private set; }
 
         public event Action PositionChanged;
 
@@ -22,6 +23,11 @@ namespace Game.Application.Objects
         public void SetSize(Vector2 size)
         {
             Size = size;
+        }
+
+        public void SetDirection(byte direction)
+        {
+            Direction = direction;
         }
     }
 }
