8ee62a114d84f80adda86a662f16d6152702d961
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-10 23:10:14 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Messages/AnimationStateChangedMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Game.Application
+{
+    public class AnimationStateChangedMessage
+    {
+        public int GameObjectId { get; set; }
+
+        public byte AnimationState { get; set; }
+    }
+}
