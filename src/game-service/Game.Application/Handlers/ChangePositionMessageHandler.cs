65a263f4438217da8b5364b64ae766e56f789064
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-11 23:58:01 +0300

    :alien: Changing position for other.

--- src/game-service/Game.Application/Handlers/ChangePositionMessageHandler.cs
+++ src/game-service/Game.Application/Handlers/ChangePositionMessageHandler.cs
@@ -6,10 +6,12 @@ namespace Game.Application
     public class ChangePositionMessageHandler : IMessageHandler
     {
         private readonly IGameObject player;
+        private readonly IGameService gameService;
 
-        public ChangePositionMessageHandler(IGameObject player)
+        public ChangePositionMessageHandler(IGameObject player, IGameService gameService)
         {
             this.player = player;
+            this.gameService = gameService;
         }
 
         public void Handle(byte[] rawData)
@@ -17,8 +19,30 @@ namespace Game.Application
             var message = MessageUtils.GetMessage<ChangePlayerPositionMessage>(rawData);
             var x = message.X;
             var y = message.Y;
+            var position = new Vector2(x, y);
 
-            player.Transform.SetPosition(new Vector2(x, y));
+            player.Transform.SetPosition(position);
+
+            ChangePositionForNearbyGameObjects(position);
+        }
+
+        private void ChangePositionForNearbyGameObjects(Vector2 position)
+        {
+            // TODO: Refactor this: collection.GetAllNearbyGameObjects()
+            var nearbyGameObjects = new int[0];
+
+            foreach (var id in nearbyGameObjects)
+            {
+                var message = new PositionChangedMessage()
+                {
+                    GameObjectId = id,
+                    X = position.X,
+                    Y = position.Y
+                };
+                var data = MessageUtils.ToMessage(message);
+
+                gameService.SendMessage(data, id);
+            }
         }
     }
 }
