56bc9c3ff9fc3973fcc4a206099b6d39c55c7b2d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-12 13:00:53 +0300

    :alien: Code refactoring.

---
+++ src/game-service/Game.Application/Components/ISessionDataContainer.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.Application.Components
+{
+    public interface ISessionDataContainer
+    {
+        bool AddSessionData(int id, SessionData sessionData);
+
+        bool RemoveSessionData(int id);
+
+        bool GetSessionData(int id, out SessionData sessionData);
+    }
+}
