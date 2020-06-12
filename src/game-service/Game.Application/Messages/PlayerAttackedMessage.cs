4428338508c3db2edb0ca7d65afd88c9ed63cca4
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-12 22:00:12 +0300

    :alien: Removed script.

--- src/game-service/Game.Application/Messages/PlayerAttackedMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace Game.Application.Messages
-{
-    public class PlayerAttackedMessage
-    {
-        public float ContactPointX { get; set; }
-
-        public float ContactPointY { get; set; }
-    }
-}
