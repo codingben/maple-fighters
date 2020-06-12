c8aac2a99e23dc1f675a8345cd221981f31143f5
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-12 13:00:49 +0300

    :alien: Code refactoring.

---
+++ src/game-service/Game.Application/Components/SessionDataContainer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using Common.ComponentModel;
+
+namespace Game.Application.Components
+{
+    [ComponentSettings(ExposedState.Exposable)]
+    public class SessionDataContainer : ComponentBase, ISessionDataContainer
+    {
+        private readonly ConcurrentDictionary<int, SessionData> container;
+
+        public SessionDataContainer()
+        {
+            container = new ConcurrentDictionary<int, SessionData>();
+        }
+
+        public bool AddSessionData(int id, SessionData sessionData)
+        {
+            return container.TryAdd(id, sessionData);
+        }
+
+        public bool RemoveSessionData(int id)
+        {
+            return container.TryRemove(id, out _);
+        }
+
+        public bool GetSessionData(int id, out SessionData sessionData)
+        {
+            return container.TryGetValue(id, out sessionData);
+        }
+    }
+}
