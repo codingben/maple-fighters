14bbd50ccfbd56d691bd5015e3b186bc5528cc0b
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-12 22:20:22 +0300

    :alien: Implemented handler.

--- src/game-service/Game.Application/Handlers/ChangeAnimationStateHandler.cs
+++ src/game-service/Game.Application/Handlers/ChangeAnimationStateHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using Game.Application.Messages;
 using Game.Application.Network;
 using Game.Application.Objects.Components;
 
@@ -6,16 +7,49 @@ namespace Game.Application.Handlers
 {
     public class ChangeAnimationStateHandler : IMessageHandler
     {
-        private IAnimationData animationData;
+        private readonly IAnimationData animationData;
+        private readonly IProximityChecker prxomitiyChecker;
+        private readonly Action<byte[], int> sendMessageCallback;
 
-        public ChangeAnimationStateHandler(IAnimationData animationData)
+        public ChangeAnimationStateHandler(
+            IAnimationData animationData,
+            IProximityChecker prxomitiyChecker,
+            Action<byte[], int> sendMessageCallback)
         {
             this.animationData = animationData;
+            this.prxomitiyChecker = prxomitiyChecker;
+            this.sendMessageCallback = sendMessageCallback;
         }
 
         public void Handle(byte[] rawData)
         {
-            throw new NotImplementedException();
+            var message = MessageUtils.FromMessage<ChangeAnimationStateMessage>(rawData);
+            var animationState = message.AnimationState;
+
+            animationData.SetAnimationState(animationState);
+
+            SendMessageToNearbyGameObjects(animationState);
+        }
+
+        private void SendMessageToNearbyGameObjects(byte animationState)
+        {
+            var nearbyGameObjects = prxomitiyChecker.GetGameObjects();
+
+            foreach (var gameObject in nearbyGameObjects)
+            {
+                var messageData = new MessageData()
+                {
+                    Code = (byte)MessageCodes.PositionChanged,
+                    RawData = MessageUtils.ToMessage(new AnimationStateChangedMessage()
+                    {
+                        GameObjectId = gameObject.Id,
+                        AnimationState = animationState
+                    })
+                };
+                var rawData = MessageUtils.ToMessage(messageData);
+
+                sendMessageCallback(rawData, gameObject.Id);
+            }
         }
     }
 }
