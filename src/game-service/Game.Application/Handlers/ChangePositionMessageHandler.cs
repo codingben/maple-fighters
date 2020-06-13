274d4f04f591959b03e94eeb9cfe6fbd6d9fd8d2
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-13 20:26:14 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Handlers/ChangePositionMessageHandler.cs
+++ src/game-service/Game.Application/Handlers/ChangePositionMessageHandler.cs
@@ -1,7 +1,5 @@
-using System;
 using Common.MathematicsHelper;
 using InterestManagement;
-using Game.Application.Objects.Components;
 using Game.Application.Network;
 using Game.Application.Messages;
 
@@ -10,17 +8,10 @@ namespace Game.Application.Handlers
     public class ChangePositionMessageHandler : IMessageHandler
     {
         private readonly ITransform transform;
-        private readonly IProximityChecker proximityChecker;
-        private readonly Action<byte[], int> sendMessageCallback;
 
-        public ChangePositionMessageHandler(
-            ITransform transform,
-            IProximityChecker proximityChecker,
-            Action<byte[], int> sendMessageCallback)
+        public ChangePositionMessageHandler(ITransform transform)
         {
             this.transform = transform;
-            this.proximityChecker = proximityChecker;
-            this.sendMessageCallback = sendMessageCallback;
         }
 
         public void Handle(byte[] rawData)
@@ -32,27 +23,6 @@ namespace Game.Application.Handlers
             var position = new Vector2(x, y);
 
             transform.SetPosition(position);
-
-            SendMessageToNearbyGameObjects(position);
-        }
-
-        private void SendMessageToNearbyGameObjects(Vector2 position)
-        {
-            var nearbyGameObjects = proximityChecker.GetGameObjects();
-
-            foreach (var gameObject in nearbyGameObjects)
-            {
-                var message = new PositionChangedMessage()
-                {
-                    GameObjectId = gameObject.Id,
-                    X = position.X,
-                    Y = position.Y
-                };
-                var rawData =
-                    MessageUtils.WrapMessage((byte)MessageCodes.PositionChanged, message);
-
-                sendMessageCallback(rawData, gameObject.Id);
-            }
         }
     }
 }
