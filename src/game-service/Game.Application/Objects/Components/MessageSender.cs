611110902b9ec86806e2a01cb99cdf5f885f173f
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-13 19:38:51 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Objects/Components/MessageSender.cs
@@ -0,0 +1,12 @@
+﻿using Common.ComponentModel;
+
+namespace Game.Application.Objects.Components
+{
+    public class MessageSender : ComponentBase, IMessageSender
+    {
+        public void SendMessage(byte[] rawData, int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
