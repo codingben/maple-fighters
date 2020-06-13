b3e2fd89e16b940e6dc38d3370c6cfb637a3f894
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-13 20:24:23 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Objects/Components/AnimationStateChangedMessageSender.cs
@@ -0,0 +1,50 @@
+using Common.ComponentModel;
+using Game.Application.Messages;
+
+namespace Game.Application.Objects.Components
+{
+    [ComponentSettings(ExposedState.Unexposable)]
+    public class AnimationStateChangedMessageSender : ComponentBase
+    {
+        private IMessageSender messageSender;
+        private IAnimationData animationData;
+        private IGameObject gameObject;
+
+        protected override void OnAwake()
+        {
+            messageSender = Components.Get<IMessageSender>();
+            animationData = Components.Get<IAnimationData>();
+
+            var gameObjectGetter = Components.Get<IGameObjectGetter>();
+            gameObject = gameObjectGetter.Get();
+
+            SubscribeToAnimationStateChanged();
+        }
+
+        protected override void OnRemoved()
+        {
+            UnsubscribeFromAnimationStateChanged();
+        }
+
+        private void SubscribeToAnimationStateChanged()
+        {
+            animationData.AnimationStateChanged += OnAnimationStateChanged;
+        }
+
+        private void UnsubscribeFromAnimationStateChanged()
+        {
+            animationData.AnimationStateChanged -= OnAnimationStateChanged;
+        }
+
+        private void OnAnimationStateChanged()
+        {
+            var message = new AnimationStateChangedMessage()
+            {
+                GameObjectId = gameObject.Id,
+                AnimationState = animationData.GetAnimationState()
+            };
+
+            messageSender.SendMessage((byte)MessageCodes.AnimationStateChanged, message);
+        }
+    }
+}
