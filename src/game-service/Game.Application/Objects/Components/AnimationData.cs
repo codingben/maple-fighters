f143eddccf9e4a38d74b4c3c334696d1c0b147e2
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-13 20:16:34 +0300

    :alien: Implemented action.

--- src/game-service/Game.Application/Objects/Components/AnimationData.cs
+++ src/game-service/Game.Application/Objects/Components/AnimationData.cs
@@ -1,15 +1,20 @@
-﻿using Common.ComponentModel;
+﻿using System;
+using Common.ComponentModel;
 
 namespace Game.Application.Objects.Components
 {
     [ComponentSettings(ExposedState.Exposable)]
     public class AnimationData : ComponentBase, IAnimationData
     {
+        public event Action AnimationStateChanged;
+
         private byte animationState;
 
         public void SetAnimationState(byte animationState)
         {
             this.animationState = animationState;
+
+            AnimationStateChanged?.Invoke();
         }
 
         public byte GetAnimationState()
