929673ebc3831c256de7cff44a4b8d33d457254d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-13 20:09:57 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Objects/Components/IMessageSender.cs
+++ src/game-service/Game.Application/Objects/Components/IMessageSender.cs
@@ -2,7 +2,7 @@ namespace Game.Application.Objects.Components
 {
     public interface IMessageSender
     {
-        void SendMessageToNearbyGameObjects<TMessage>(byte code, TMessage message)
+        void SendMessage<TMessage>(byte code, TMessage message)
             where TMessage : class;
     }
 }
