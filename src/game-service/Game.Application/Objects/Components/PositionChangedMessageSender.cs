8f77b66c53816ec62cb2690bfaa040b44ef58557
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-13 20:10:11 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Objects/Components/PositionChangedMessageSender.cs
@@ -0,0 +1,49 @@
+using Common.ComponentModel;
+using Game.Application.Messages;
+
+namespace Game.Application.Objects.Components
+{
+    [ComponentSettings(ExposedState.Exposable)]
+    public class PositionChangedMessageSender : ComponentBase
+    {
+        private IMessageSender messageSender;
+        private IGameObject gameObject;
+
+        protected override void OnAwake()
+        {
+            messageSender = Components.Get<IMessageSender>();
+
+            var gameObjectGetter = Components.Get<IGameObjectGetter>();
+            gameObject = gameObjectGetter.Get();
+
+            SubscribeToPositionChanged();
+        }
+
+        protected override void OnRemoved()
+        {
+            UnsubscribeFromPositionChanged();
+        }
+
+        private void SubscribeToPositionChanged()
+        {
+            gameObject.Transform.PositionChanged += OnPositionChanged;
+        }
+
+        private void UnsubscribeFromPositionChanged()
+        {
+            gameObject.Transform.PositionChanged -= OnPositionChanged;
+        }
+
+        private void OnPositionChanged()
+        {
+            var message = new PositionChangedMessage()
+            {
+                GameObjectId = gameObject.Id,
+                X = gameObject.Transform.Position.X,
+                Y = gameObject.Transform.Position.Y
+            };
+
+            messageSender.SendMessage((byte)MessageCodes.PositionChanged, message);
+        }
+    }
+}
