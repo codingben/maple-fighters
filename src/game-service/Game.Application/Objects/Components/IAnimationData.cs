32be6b638711eb1f199717927aedaf9f76438478
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-13 20:15:16 +0300

    :alien: Added action.

--- src/game-service/Game.Application/Objects/Components/IAnimationData.cs
+++ src/game-service/Game.Application/Objects/Components/IAnimationData.cs
@@ -1,7 +1,11 @@
-﻿namespace Game.Application.Objects.Components
+﻿using System;
+
+namespace Game.Application.Objects.Components
 {
     public interface IAnimationData
     {
+        event Action AnimationStateChanged;
+
         void SetAnimationState(byte animationState);
 
         byte GetAnimationState();
