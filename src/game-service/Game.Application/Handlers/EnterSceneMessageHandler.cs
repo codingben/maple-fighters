a3cf4e1cdb8d299c2b4408ded5876e1332bc9a2f
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-14 23:09:51 +0300

    :alien: Implemented handler.

--- src/game-service/Game.Application/Handlers/EnterSceneMessageHandler.cs
+++ src/game-service/Game.Application/Handlers/EnterSceneMessageHandler.cs
@@ -1,12 +1,29 @@
+using Game.Application.Messages;
 using Game.Application.Network;
+using Game.Application.Objects.Components;
 
 namespace Game.Application.Handlers
 {
     public class EnterSceneMessageHandler : IMessageHandler
     {
+        private ICharacterData characterData;
+
+        public EnterSceneMessageHandler(ICharacterData characterData)
+        {
+            this.characterData = characterData;
+        }
+
         public void Handle(byte[] rawData)
         {
-            throw new System.NotImplementedException();
+            var message =
+                MessageUtils.DeserializeMessage<EnterSceneMessage>(rawData);
+            var name = message.Character.Name;
+            var characterType = message.Character.CharacterType;
+
+            characterData.SetName(name);
+            characterData.SetCharacterType(characterType);
+
+            // TODO: Send the entered scene message from here or from component (and get player spawn data)
         }
     }
 }
