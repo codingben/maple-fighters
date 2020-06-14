11c29bf946b6206a9c63b3aac7fca5209bdd1c6b
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-14 23:01:41 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Handlers/EnterSceneMessageHandler.cs
@@ -0,0 +1,12 @@
+using Game.Application.Network;
+
+namespace Game.Application.Handlers
+{
+    public class EnterSceneMessageHandler : IMessageHandler
+    {
+        public void Handle(byte[] rawData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
