704d441e6d12fc735f3613ed7fe15f06f6d88e19
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-15 23:41:40 +0300

    :alien: Implemented new method.

--- src/game-service/Game.Application/Objects/Components/MessageSender.cs
+++ src/game-service/Game.Application/Objects/Components/MessageSender.cs
@@ -7,12 +7,15 @@ namespace Game.Application.Objects.Components
     [ComponentSettings(ExposedState.Exposable)]
     public class MessageSender : ComponentBase, IMessageSender
     {
-        private readonly Action<byte[], int> sendMessageCallback;
+        private readonly Action<byte[]> sendMessageCallback;
+        private readonly Action<byte[], int> sendMessageToCallback;
+
         private IProximityChecker proximityChecker;
 
-        public MessageSender(Action<byte[], int> sendMessageCallback)
+        public MessageSender(Action<byte[]> sendMessageCallback, Action<byte[], int> sendMessageToCallback)
         {
             this.sendMessageCallback = sendMessageCallback;
+            this.sendMessageToCallback = sendMessageToCallback;
         }
 
         protected override void OnAwake()
@@ -23,13 +26,21 @@ namespace Game.Application.Objects.Components
         public void SendMessage<TMessage>(byte code, TMessage message)
             where TMessage : class
         {
+            var rawData = MessageUtils.WrapMessage(code, message);
+
+            sendMessageCallback(rawData);
+        }
+
+        public void SendMessageToNearbyGameObjects<TMessage>(byte code, TMessage message)
+            where TMessage : class
+        {
             var nearbyGameObjects = proximityChecker.GetGameObjects();
 
             foreach (var gameObject in nearbyGameObjects)
             {
                 var rawData = MessageUtils.WrapMessage(code, message);
 
-                sendMessageCallback(rawData, gameObject.Id);
+                sendMessageToCallback(rawData, gameObject.Id);
             }
         }
     }
