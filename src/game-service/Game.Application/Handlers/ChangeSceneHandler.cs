2e03baf2019f4f8abbadbdec9c8c54ac96d578de
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-16 20:47:25 +0300

    :alien: New handler.

---
+++ src/game-service/Game.Application/Handlers/ChangeSceneHandler.cs
@@ -0,0 +1,12 @@
+using Game.Application.Network;
+
+namespace Game.Application.Handlers
+{
+    public class ChangeSceneHandler : IMessageHandler
+    {
+        public void Handle(byte[] rawData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
