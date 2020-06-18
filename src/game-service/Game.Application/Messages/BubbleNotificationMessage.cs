9a167134edcca4924ed7e39e003d66e5b1ad6180
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-18 22:48:56 +0300

    :alien: Added parameter.

--- src/game-service/Game.Application/Messages/BubbleNotificationMessage.cs
+++ src/game-service/Game.Application/Messages/BubbleNotificationMessage.cs
@@ -2,6 +2,8 @@ namespace Game.Application.Messages
 {
     public class BubbleNotificationMessage
     {
+        public int RequesterId { get; set; }
+
         public string Message { get; set; }
 
         public int Time { get; set; }
