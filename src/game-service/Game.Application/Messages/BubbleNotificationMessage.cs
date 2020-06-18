33e843729094afe67b1974b72262916ba101da30
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-18 22:42:24 +0300

    :alien: Removed data from message.

--- src/game-service/Game.Application/Messages/BubbleNotificationMessage.cs
+++ src/game-service/Game.Application/Messages/BubbleNotificationMessage.cs
@@ -2,8 +2,6 @@ namespace Game.Application.Messages
 {
     public class BubbleNotificationMessage
     {
-        public int RequesterId { get; set; }
-
         public string Message { get; set; }
 
         public int Time { get; set; }
