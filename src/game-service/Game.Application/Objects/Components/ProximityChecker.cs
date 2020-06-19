d913c837ad461802cc4dc2654c7a72a8b756f452
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-19 19:55:43 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Objects/Components/ProximityChecker.cs
+++ src/game-service/Game.Application/Objects/Components/ProximityChecker.cs
@@ -7,13 +7,7 @@ namespace Game.Application.Objects.Components
     [ComponentSettings(ExposedState.Exposable)]
     public class ProximityChecker : ComponentBase, IProximityChecker
     {
-        private IInterestArea<IGameObject> interestArea;
-        private List<IGameObject> nearbyGameObjects;
-
-        public ProximityChecker()
-        {
-            nearbyGameObjects = new List<IGameObject>();
-        }
+        private InterestArea<IGameObject> interestArea;
 
         protected override void OnAwake()
         {
@@ -22,65 +16,23 @@ namespace Game.Application.Objects.Components
             var gameObjectGetter = Components.Get<IGameObjectGetter>();
             var gameObject = gameObjectGetter.Get();
 
-            interestArea = new InterestArea<IGameObject>(scene, gameObject);
-
-            SubscribeToNearbySceneObjectsEvents();
+            interestArea = new InterestArea<IGameObject>(gameObject);
+            interestArea.SetScene(scene);
         }
 
         protected override void OnRemoved()
         {
-            UnsubscribeFromNearbySceneObjectsEvents();
-
             interestArea?.Dispose();
-        }
-
-        private void SubscribeToNearbySceneObjectsEvents()
-        {
-            interestArea.NearbySceneObjectsEvents.SceneObjectAdded += OnGameObjectAdded;
-            interestArea.NearbySceneObjectsEvents.SceneObjectRemoved += GameObjectRemoved;
-            interestArea.NearbySceneObjectsEvents.SceneObjectsAdded += OnGameObjectsAdded;
-            interestArea.NearbySceneObjectsEvents.SceneObjectsRemoved += OnGameObjectsRemoved;
-        }
-
-        private void UnsubscribeFromNearbySceneObjectsEvents()
-        {
-            interestArea.NearbySceneObjectsEvents.SceneObjectAdded -= OnGameObjectAdded;
-            interestArea.NearbySceneObjectsEvents.SceneObjectRemoved -= GameObjectRemoved;
-            interestArea.NearbySceneObjectsEvents.SceneObjectsAdded -= OnGameObjectsAdded;
-            interestArea.NearbySceneObjectsEvents.SceneObjectsRemoved -= OnGameObjectsRemoved;
-        }
-
-        private void OnGameObjectAdded(IGameObject gameObject)
-        {
-            nearbyGameObjects.Add(gameObject);
-        }
-
-        private void GameObjectRemoved(IGameObject gameObject)
-        {
-            nearbyGameObjects.Remove(gameObject);
-        }
-
-        private void OnGameObjectsAdded(IEnumerable<IGameObject> gameObjects)
-        {
-            nearbyGameObjects.AddRange(gameObjects);
-        }
-
-        private void OnGameObjectsRemoved(IEnumerable<IGameObject> gameObjects)
-        {
-            foreach (var gameObject in gameObjects)
-            {
-                nearbyGameObjects.Remove(gameObject);
-            }
         }
 
         public IEnumerable<IGameObject> GetNearbyGameObjects()
         {
-            return nearbyGameObjects;
+            return interestArea.GetNearbySceneObjects();
         }
 
         public INearbySceneObjectsEvents<IGameObject> GetNearbyGameObjectsEvents()
         {
-            return interestArea.NearbySceneObjectsEvents;
+            return interestArea.GetNearbySceneObjectsEvents();
         }
     }
 }
