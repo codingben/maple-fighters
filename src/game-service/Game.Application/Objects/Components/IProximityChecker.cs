ade3190c09905763dacd7068e88dae581d3cc54c
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-19 22:16:34 +0300

    :alien: Added new method.

--- src/game-service/Game.Application/Objects/Components/IProximityChecker.cs
+++ src/game-service/Game.Application/Objects/Components/IProximityChecker.cs
@@ -5,6 +5,8 @@ namespace Game.Application.Objects.Components
 {
     public interface IProximityChecker
     {
+        void ChangeScene();
+
         IEnumerable<IGameObject> GetNearbyGameObjects();
 
         INearbySceneObjectsEvents<IGameObject> GetNearbyGameObjectsEvents();
