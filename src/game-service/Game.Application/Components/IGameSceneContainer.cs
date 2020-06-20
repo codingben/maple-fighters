c530ce3e94125aba88ef392d52fc84538cf7337a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 18:47:41 +0300

    :alien: Added new methods.

--- src/game-service/Game.Application/Components/IGameSceneContainer.cs
+++ src/game-service/Game.Application/Components/IGameSceneContainer.cs
@@ -2,6 +2,10 @@ namespace Game.Application.Components
 {
     public interface IGameSceneContainer
     {
+        void AddScene(Map map, IGameScene scene);
+
+        void RemoveScene(Map map);
+
         bool TryGetScene(Map map, out IGameScene scene);
     }
 }
