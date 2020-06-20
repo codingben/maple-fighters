5029f227bed8f09c5344b47ce8c510a40a581168
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 18:48:24 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Components/LobbyGameScene.cs
@@ -0,0 +1,13 @@
+using Common.MathematicsHelper;
+
+namespace Game.Application.Components
+{
+    public class LobbyGameScene : GameScene
+    {
+        public LobbyGameScene()
+            : base(new Vector2(40, 5), new Vector2(10, 5))
+        {
+            // Left blank intentionally
+        }
+    }
+}
