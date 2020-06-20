1f2d8b25350ca95a1c49af6a9fc75c89078347be
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 20:26:20 +0300

    :alien: TODO completed.

--- src/game-service/Game.Application/Objects/BlueSnailGameObject.cs
+++ src/game-service/Game.Application/Objects/BlueSnailGameObject.cs
@@ -1,11 +1,19 @@
+using Common.MathematicsHelper;
+using Game.Application.Components;
+using Game.Application.Objects.Components;
+
 namespace Game.Application.Objects
 {
     public class BlueSnailGameObject : GameObject
     {
-        public BlueSnailGameObject(int id)
+        public BlueSnailGameObject(int id, Vector2 position, IGameScene scene)
             : base(id, "BlueSnail")
         {
-            // TODO: Implement
+            Transform.SetPosition(position);
+            Transform.SetSize(Vector2.One);
+
+            Components.Add(new PresenceSceneProvider(scene));
+            Components.Add(new ProximityChecker());
         }
     }
 }
