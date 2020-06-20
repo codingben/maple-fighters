abe930ac11742b09e441cacf7d15f559d7c9eb5f
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 18:48:27 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Components/TheDarkForestGameScene.cs
@@ -0,0 +1,13 @@
+using Common.MathematicsHelper;
+
+namespace Game.Application.Components
+{
+    public class TheDarkForestGameScene : GameScene
+    {
+        public TheDarkForestGameScene()
+            : base(new Vector2(30, 30), new Vector2(10, 5))
+        {
+            // Left blank intentionally
+        }
+    }
+}
