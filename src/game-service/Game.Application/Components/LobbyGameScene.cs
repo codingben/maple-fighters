1f08603d9874225561ce2e713e73113f066c1efb
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 20:26:47 +0300

    :alien: Creating game objects.

--- src/game-service/Game.Application/Components/LobbyGameScene.cs
+++ src/game-service/Game.Application/Components/LobbyGameScene.cs
@@ -1,13 +1,51 @@
+using Common.Components;
 using Common.MathematicsHelper;
+using Game.Application.Objects;
 
 namespace Game.Application.Components
 {
     public class LobbyGameScene : GameScene
     {
-        public LobbyGameScene()
+        private readonly IIdGenerator idGenerator;
+
+        public LobbyGameScene(IIdGenerator idGenerator)
             : base(new Vector2(40, 5), new Vector2(10, 5))
         {
-            // Left blank intentionally
+            this.idGenerator = idGenerator;
+
+            // TODO: Who will "own" the game objects?
+            // TODO: Who will "destroy" game objects?
+
+            CreateGuardian();
+            CreatePortal();
+        }
+
+        void CreateGuardian()
+        {
+            var id = idGenerator.GenerateId();
+            var position = new Vector2(-14.24f, -2.025f);
+            var scene = this;
+            var text = "Hello!";
+            var time = 5;
+            var guardianGameObject = new GuardianGameObject(
+                id,
+                position,
+                scene,
+                text,
+                time);
+        }
+
+        void CreatePortal()
+        {
+            var id = idGenerator.GenerateId();
+            var position = new Vector2(-17.125f, -1.5f);
+            var scene = this;
+            var map = (byte)Map.TheDarkForest;
+            var portalGameObject = new PortalGameObject(
+                id,
+                position,
+                scene,
+                map);
         }
     }
 }
