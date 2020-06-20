4f509d6c66f0888bf735bc75373d0f7ef6029856
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 19:29:27 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Objects/GuardianGameObject.cs
@@ -0,0 +1,11 @@
+namespace Game.Application.Objects
+{
+    public class GuardianGameObject : GameObject
+    {
+        public GuardianGameObject(int id)
+            : base(id, "Guardian")
+        {
+            // TODO: Implement
+        }
+    }
+}
