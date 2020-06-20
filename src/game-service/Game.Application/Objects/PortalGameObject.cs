efa7d0b6b476eb90da73f7c0add612f0e2c6ab33
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 20:26:30 +0300

    :alien: TODO completed.

--- src/game-service/Game.Application/Objects/PortalGameObject.cs
+++ src/game-service/Game.Application/Objects/PortalGameObject.cs
@@ -1,11 +1,24 @@
+using Common.MathematicsHelper;
+using Game.Application.Components;
+using Game.Application.Objects.Components;
+
 namespace Game.Application.Objects
 {
     public class PortalGameObject : GameObject
     {
-        public PortalGameObject(int id)
+        public PortalGameObject(
+            int id,
+            Vector2 position,
+            IGameScene scene,
+            byte map)
             : base(id, "Portal")
         {
-            // TODO: Implement
+            Transform.SetPosition(position);
+            Transform.SetSize(Vector2.One);
+
+            Components.Add(new PresenceSceneProvider(scene));
+            Components.Add(new ProximityChecker());
+            Components.Add(new PortalData(map));
         }
     }
 }
