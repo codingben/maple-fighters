78aec904dc932fa47aaf9ee6caa2bbdc14d38910
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-20 20:26:25 +0300

    :alien: TODO completed.

--- src/game-service/Game.Application/Objects/GuardianGameObject.cs
+++ src/game-service/Game.Application/Objects/GuardianGameObject.cs
@@ -1,11 +1,25 @@
+using Common.MathematicsHelper;
+using Game.Application.Components;
+using Game.Application.Objects.Components;
+
 namespace Game.Application.Objects
 {
     public class GuardianGameObject : GameObject
     {
-        public GuardianGameObject(int id)
+        public GuardianGameObject(
+            int id,
+            Vector2 position,
+            IGameScene scene,
+            string text,
+            int time)
             : base(id, "Guardian")
         {
-            // TODO: Implement
+            Transform.SetPosition(position);
+            Transform.SetSize(Vector2.One);
+
+            Components.Add(new PresenceSceneProvider(scene));
+            Components.Add(new ProximityChecker());
+            Components.Add(new BubbleNotificationSender(text, time));
         }
     }
 }
