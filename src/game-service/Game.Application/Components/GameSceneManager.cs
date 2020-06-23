c8d5651a5a6d3612cefdabe904034391c74602df
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-23 21:46:37 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Components/GameSceneManager.cs
+++ src/game-service/Game.Application/Components/GameSceneManager.cs
@@ -1,28 +1,33 @@
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using Common.ComponentModel;
 using Common.Components;
 
 namespace Game.Application.Components
 {
-    [ComponentSettings(ExposedState.Unexposable)]
-    public class GameSceneManager : ComponentBase
+    [ComponentSettings(ExposedState.Exposable)]
+    public class GameSceneManager : ComponentBase, IGameSceneManager
     {
-        private Dictionary<Map, IGameScene> collection;
+        private ConcurrentDictionary<Map, IGameScene> collection;
 
         protected override void OnAwake()
         {
             var idGenerator = Components.Get<IIdGenerator>();
 
-            collection = new Dictionary<Map, IGameScene>();
-            collection.Add(Map.Lobby, new LobbyGameScene(idGenerator));
-            collection.Add(Map.TheDarkForest, new TheDarkForestGameScene(idGenerator));
+            collection = new ConcurrentDictionary<Map, IGameScene>();
+            collection.TryAdd(Map.Lobby, new LobbyGameScene(idGenerator));
+            collection.TryAdd(Map.TheDarkForest, new TheDarkForestGameScene(idGenerator));
         }
 
         protected override void OnRemoved()
         {
-            collection[Map.Lobby].Dispose();
-            collection[Map.TheDarkForest].Dispose();
-            collection.Clear();
+            collection[Map.Lobby]?.Dispose();
+            collection[Map.TheDarkForest]?.Dispose();
+            collection?.Clear();
+        }
+
+        public bool TryGetGameScene(Map map, out IGameScene gameScene)
+        {
+            return collection.TryGetValue(map, out gameScene);
         }
     }
 }
