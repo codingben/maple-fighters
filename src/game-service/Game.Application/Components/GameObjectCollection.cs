a2d6c84e5b382b22f78ab0046479f439c4c055c1
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-23 22:12:16 +0300

    :alien: Implemented methods.

--- src/game-service/Game.Application/Components/GameObjectCollection.cs
+++ src/game-service/Game.Application/Components/GameObjectCollection.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using Common.ComponentModel;
 using Game.Application.Objects;
 
@@ -7,14 +9,31 @@ namespace Game.Application.Components
     [ComponentSettings(ExposedState.Exposable)]
     public class GameObjectCollection : ComponentBase, IGameObjectCollection
     {
-        public GameObjectCollection()
-        {
+        private ConcurrentDictionary<int, IGameObject> collection;
 
+        public GameObjectCollection(IEnumerable<IGameObject> gameObjects)
+        {
+            collection = new ConcurrentDictionary<int, IGameObject>();
+
+            foreach (var gameObject in gameObjects)
+            {
+                collection.TryAdd(gameObject.Id, gameObject);
+            }
+        }
+
+        protected override void OnRemoved()
+        {
+            foreach (var gameObject in collection.Values)
+            {
+                ((IDisposable)gameObject.Components)?.Dispose();
+            }
+
+            collection.Clear();
         }
 
         public bool TryGetGameObject(int id, out IGameObject gameObject)
         {
-            throw new NotImplementedException();
+            return collection.TryGetValue(id, out gameObject);
         }
     }
 }
