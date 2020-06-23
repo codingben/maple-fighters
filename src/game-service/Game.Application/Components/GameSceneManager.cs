c2b42f8f07c289ec4f689bea024161b64015ebf6
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-23 21:31:45 +0300

    :alien: Added todo.

--- src/game-service/Game.Application/Components/GameSceneManager.cs
+++ src/game-service/Game.Application/Components/GameSceneManager.cs
@@ -19,6 +19,8 @@ namespace Game.Application.Components
 
         protected override void OnRemoved()
         {
+            // TODO: Dispose scenes
+
             gameSceneCollection.RemoveScene(Map.Lobby);
             gameSceneCollection.RemoveScene(Map.TheDarkForest);
         }
