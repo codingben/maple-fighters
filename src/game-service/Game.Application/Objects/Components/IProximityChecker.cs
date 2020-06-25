f587add9bcdc42b3a6a631f5d5ebcb38a82e4728
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-25 20:06:40 +0300

    :alien: Added parameter.

--- src/game-service/Game.Application/Objects/Components/IProximityChecker.cs
+++ src/game-service/Game.Application/Objects/Components/IProximityChecker.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using Game.Application.Components;
 using InterestManagement;
 
 namespace Game.Application.Objects.Components
 {
     public interface IProximityChecker
     {
-        void ChangeScene();
+        void ChangeScene(IGameScene gameScene);
 
         IEnumerable<IGameObject> GetNearbyGameObjects();
 
