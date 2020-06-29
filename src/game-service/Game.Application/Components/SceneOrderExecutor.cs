e103d53fa735668054463a583229aa14e7f64f68
Ben Ukhanov <ben1zuk321@gmail.com>
2020-06-29 18:08:10 +0300

    :alien: New script.

---
+++ src/game-service/Game.Application/Components/SceneOrderExecutor.cs
@@ -0,0 +1,10 @@
+using Common.ComponentModel;
+
+namespace Game.Application.Components
+{
+    [ComponentSettings(ExposedState.Exposable)]
+    public class SceneOrderExecutor : ComponentBase, ISceneOrderExecutor
+    {
+        // TODO: Implement
+    }
+}
