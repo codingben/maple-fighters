6fb704ed6bf4823cfcd959d3fd3f75d1f24df770
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-05 21:28:52 +0300

    :alien: Changed to 25Hz.

--- lib/physics/box2d/DefaultSettings.cs
+++ lib/physics/box2d/DefaultSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class DefaultSettings
     {
-        public const float TimeStep = 1.0f / 30.0f; // (30Hz)
+        public const float TimeStep = 1.0f / 25.0f; // (30Hz)
 
         public const int SleepTime = 10;
 
