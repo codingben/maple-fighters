c245960d809884333f97b7aa9ef7b031b5e4b058
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-06 20:29:16 +0300

    :alien: New script to create player.

---
+++ src/game-service/Game.Application/Components/GamePlayerCreator.cs
@@ -0,0 +1,33 @@
+using Common.ComponentModel;
+using Common.Components;
+using Game.Application.Objects;
+
+namespace Game.Application.Components
+{
+    [ComponentSettings(ExposedState.Exposable)]
+    public class GamePlayerCreator : ComponentBase, IGamePlayerCreator
+    {
+        private IIdGenerator idGenerator;
+        private IPlayerSpawnData playerSpawnData;
+        private IGameObjectCollection gameObjectCollection;
+
+        protected override void OnAwake()
+        {
+            idGenerator = Components.Get<IIdGenerator>();
+            playerSpawnData = Components.Get<IPlayerSpawnData>();
+        }
+
+        public IGameObject Create()
+        {
+            var id = idGenerator.GenerateId();
+            var player = new PlayerGameObject(id);
+
+            player.Transform.SetPosition(playerSpawnData.GetPosition());
+            player.Transform.SetSize(playerSpawnData.GetSize());
+
+            gameObjectCollection.AddGameObject(player);
+
+            return player;
+        }
+    }
+}
