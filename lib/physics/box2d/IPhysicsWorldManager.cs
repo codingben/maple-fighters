ecb7a6bc9c4e722fdc22e5e107451cf4108b47bb
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-07 21:36:02 +0300

    :alien: New scripts.

---
+++ lib/physics/box2d/IPhysicsWorldManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Box2DX.Dynamics;
+
+namespace Physics.Box2D
+{
+    public interface IPhysicsWorldManager : IDisposable
+    {
+        void AddBody(NewBodyData newBodyData);
+
+        void RemoveBody(int id);
+
+        bool GetBody(int id, out BodyData bodyData);
+
+        void UpdateBodies();
+
+        void Step(float timeStep, int velocityIterations, int positionIterations);
+
+        void SetDebugDraw(DebugDraw debugDraw);
+    }
+}
---
+++ lib/physics/box2d/PhysicsWorldManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using Box2DX.Collision;
+using Box2DX.Dynamics;
+using Common.MathematicsHelper;
+
+namespace Physics.Box2D
+{
+    public class PhysicsWorldManager : IPhysicsWorldManager
+    {
+        private readonly World world;
+        private readonly LinkedList<NewBodyData> addBodies;
+        private readonly LinkedList<BodyData> removeBodies;
+        private readonly Dictionary<int, BodyData> bodies;
+
+        public PhysicsWorldManager(
+            Vector2 lowerBound,
+            Vector2 upperBound,
+            Vector2 gravity,
+            bool doSleep = false,
+            bool continuousPhysics = false)
+        {
+            var worldAabb = new AABB
+            {
+                LowerBound = lowerBound.FromVector2(),
+                UpperBound = upperBound.FromVector2()
+            };
+
+            world = new World(worldAabb, gravity.FromVector2(), doSleep);
+            addBodies = new LinkedList<NewBodyData>();
+            removeBodies = new LinkedList<BodyData>();
+            bodies = new Dictionary<int, BodyData>();
+
+            // TODO: Move out
+            world.SetContactFilter(new GroupContactFilter());
+            world.SetContactListener(new BodyContactListener());
+            world.SetContinuousPhysics(continuousPhysics);
+        }
+
+        public void Dispose()
+        {
+            world.Dispose();
+        }
+
+        public void UpdateBodies()
+        {
+            if (removeBodies.Count > 0)
+            {
+                RemoveBodies();
+            }
+
+            if (addBodies.Count > 0)
+            {
+                AddBodies();
+            }
+        }
+
+        private void AddBodies()
+        {
+            foreach (var newBodyData in addBodies)
+            {
+                var id = newBodyData.Id;
+                var body =
+                    CreateBody(newBodyData.BodyDef, newBodyData.PolygonDef);
+                var bodyData = new BodyData(id, body);
+
+                bodies.Add(newBodyData.Id, bodyData);
+            }
+
+            addBodies.Clear();
+        }
+
+        private void RemoveBodies()
+        {
+            foreach (var bodyData in removeBodies)
+            {
+                world.DestroyBody(bodyData.Body);
+
+                bodies.Remove(bodyData.Id);
+            }
+
+            removeBodies.Clear();
+        }
+
+        public void AddBody(NewBodyData newBodyData)
+        {
+            addBodies.AddLast(newBodyData);
+        }
+
+        public void RemoveBody(int id)
+        {
+            var body = bodies[id];
+
+            removeBodies.AddLast(body);
+        }
+
+        public void Step(float timeStep, int velocityIterations, int positionIterations)
+        {
+            world.Step(timeStep, velocityIterations, positionIterations);
+        }
+
+        public void SetDebugDraw(DebugDraw debugDraw)
+        {
+            world.SetDebugDraw(debugDraw);
+        }
+
+        public bool GetBody(int id, out BodyData bodyData)
+        {
+            return bodies.TryGetValue(id, out bodyData);
+        }
+
+        private Body CreateBody(BodyDef bodyDef, PolygonDef polygonDef)
+        {
+            var body = world.CreateBody(bodyDef);
+
+            body.SetUserData(bodyDef.UserData);
+            body.CreateFixture(polygonDef);
+            body.SetMassFromShapes();
+
+            return body;
+        }
+    }
+}
