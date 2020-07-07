f36db8659185b0d0be202e2b8e45b1a1735fdd55
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-07 14:15:41 +0300

    :alien: Code refactoring.

---
+++ src/game-service/Game.Application/Components/GameSceneCollection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using Common.ComponentModel;
+
+namespace Game.Application.Components
+{
+    [ComponentSettings(ExposedState.Exposable)]
+    public class GameSceneCollection : ComponentBase, IGameSceneCollection
+    {
+        private ConcurrentDictionary<Map, IGameScene> collection;
+
+        public GameSceneCollection()
+        {
+            collection = new ConcurrentDictionary<Map, IGameScene>();
+        }
+
+        public bool Add(Map map, IGameScene gameScene)
+        {
+            return collection.TryAdd(map, gameScene);
+        }
+
+        public bool Remove(Map map)
+        {
+            return collection.TryRemove(map, out _);
+        }
+
+        public bool TryGet(Map map, out IGameScene gameScene)
+        {
+            return collection.TryGetValue(map, out gameScene);
+        }
+    }
+}
