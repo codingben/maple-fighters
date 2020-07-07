7dd2ea166962f5c707725a333fd36d3b15ea95bd
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-07 15:12:09 +0300

    :alien: New script to create and remove scenes.

---
+++ src/game-service/Game.Application/Components/GameSceneManager.cs
@@ -0,0 +1,49 @@
+﻿using Common.ComponentModel;
+
+namespace Game.Application.Components
+{
+    [ComponentSettings(ExposedState.Exposable)]
+    public class GameSceneManager : ComponentBase
+    {
+        private IGameSceneCollection gameSceneCollection;
+
+        protected override void OnAwake()
+        {
+            gameSceneCollection = Components.Get<IGameSceneCollection>();
+
+            var lobby = CreateMap(Map.Lobby);
+            var theDarkForest = CreateMap(Map.TheDarkForest);
+
+            gameSceneCollection.Add(Map.Lobby, lobby);
+            gameSceneCollection.Add(Map.TheDarkForest, theDarkForest);
+        }
+
+        protected override void OnRemoved()
+        {
+            gameSceneCollection.Remove(Map.Lobby);
+            gameSceneCollection.Remove(Map.TheDarkForest);
+        }
+
+        private IGameScene CreateMap(Map map)
+        {
+            IGameScene gameScene = null;
+
+            switch (map)
+            {
+                case Map.Lobby:
+                {
+                    gameScene = new Lobby();
+                    break;
+                }
+
+                case Map.TheDarkForest:
+                {
+                    gameScene = new TheDarkForest();
+                    break;
+                }
+            }
+
+            return gameScene;
+        }
+    }
+}
