68c22d8637c83bd230b95d5f891d6056506ecb6d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-07 21:36:45 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Components/IPhysicsExecutor.cs
+++ src/game-service/Game.Application/Components/IPhysicsExecutor.cs
@@ -3,7 +3,7 @@ using Coroutines;
 
 namespace Game.Application.Components
 {
-    public interface IGameScenePhysicsExecutor : IDisposable
+    public interface IPhysicsExecutor : IDisposable
     {
         CoroutineRunner GetCoroutineRunner();
     }
