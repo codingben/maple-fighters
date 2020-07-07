da743efc82d6e59915b8aa50713065db1c9fd360
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-07 21:14:38 +0300

    :alien: Added methods.

--- lib/physics/box2d/IWorldManager.cs
+++ lib/physics/box2d/IWorldManager.cs
@@ -1,10 +1,17 @@
+using System;
 using Box2DX.Dynamics;
 
 namespace Physics.Box2D
 {
-    public interface IWorldManager
+    public interface IWorldManager : IDisposable
     {
-        void Update();
+        void AddBody(NewBodyData newBodyData);
+
+        void RemoveBody(int id);
+
+        bool GetBody(int id, out BodyData bodyData);
+
+        void UpdateBodies();
 
         void Step(float timeStep, int velocityIterations, int positionIterations);
 
