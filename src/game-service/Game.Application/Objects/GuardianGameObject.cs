1cd22085a37e5f2df07a2efcc23653611c57fb48
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-08 15:13:15 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Objects/GuardianGameObject.cs
+++ src/game-service/Game.Application/Objects/GuardianGameObject.cs
@@ -1,6 +1,5 @@
 using Common.MathematicsHelper;
 using Game.Application.Objects.Components;
-using InterestManagement;
 
 namespace Game.Application.Objects
 {
@@ -10,14 +9,6 @@ namespace Game.Application.Objects
             : base(id, name: "Guardian")
         {
             Transform.SetPosition(position);
-
-            Components.Add(new GameObjectGetter(this));
-        }
-
-        public void AddProximityChecker(IMatrixRegion<IGameObject> matrixRegion)
-        {
-            var proximityChecker = Components.Add(new ProximityChecker());
-            proximityChecker.SetMatrixRegion(matrixRegion);
         }
 
         public void AddBubbleNotification(string text, int time)
