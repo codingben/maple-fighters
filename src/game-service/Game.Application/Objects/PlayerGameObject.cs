fef4510c2ec8710e9bb762f23afc3c9eaa46b63d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-08 15:23:27 +0300

    :alien: Removed method.

--- src/game-service/Game.Application/Objects/PlayerGameObject.cs
+++ src/game-service/Game.Application/Objects/PlayerGameObject.cs
@@ -1,5 +1,4 @@
 using Common.MathematicsHelper;
-using Game.Application.Objects.Components;
 
 namespace Game.Application.Objects
 {
@@ -9,17 +8,6 @@ namespace Game.Application.Objects
             : base(id, name: "Player")
         {
             Transform.SetPosition(position);
-
-            AddComponents();
-        }
-
-        private void AddComponents()
-        {
-            Components.Add(new AnimationData());
-            // Components.Add(new MessageSender(SendMessageToMySession, SendMessageToSession));
-            Components.Add(new PositionChangedMessageSender());
-            Components.Add(new AnimationStateChangedMessageSender());
-            Components.Add(new CharacterData());
         }
     }
 }
