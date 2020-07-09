795b900364e1d1521279f028aaf8e743cb580bf9
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-09 23:46:17 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Components/GameSceneManager.cs
+++ src/game-service/Game.Application/Components/GameSceneManager.cs
@@ -40,18 +40,22 @@ namespace Game.Application.Components
                 {
                     gameScene = new GameScene(new Vector2(40, 5), new Vector2(40, 5));
 
+                    var region = gameScene.MatrixRegion;
+
                     // Guardian Game Object
                     var guardianId = idGenerator.GenerateId();
-                    var guardian = new GuardianGameObject(guardianId, new Vector2(-14.24f, -2.025f));
-                    guardian.AddProximityChecker(gameScene.MatrixRegion);
+                    var guardianPosition = new Vector2(-14.24f, -2.025f);
+                    var guardian = 
+                        new GuardianGameObject(guardianId, guardianPosition, region);
                     guardian.AddBubbleNotification("Hello", 1);
 
                     gameScene.GameObjectCollection.Add(guardian);
 
                     // Portal Game Object
                     var portalId = idGenerator.GenerateId();
-                    var portal = new PortalGameObject(portalId, new Vector2(-17.125f, -1.5f));
-                    portal.AddProximityChecker(gameScene.MatrixRegion);
+                    var portalPosition = new Vector2(-14.24f, -2.025f);
+                    var portal = 
+                        new PortalGameObject(portalId, portalPosition, region);
                     portal.AddPortalData((byte)Map.TheDarkForest);
 
                     gameScene.GameObjectCollection.Add(portal);
@@ -62,17 +66,21 @@ namespace Game.Application.Components
                 {
                     gameScene = new GameScene(new Vector2(30, 30), new Vector2(40, 5));
 
+                    var region = gameScene.MatrixRegion;
+
                     // Blue Snail Game Object
                     var guardianId = idGenerator.GenerateId();
-                    var blueSnail = new BlueSnailGameObject(guardianId, new Vector2(-2f, -8.2f));
-                    blueSnail.AddProximityChecker(gameScene.MatrixRegion);
+                    var blueSnailPosition = new Vector2(-2f, -8.2f);
+                    var blueSnail = 
+                        new BlueSnailGameObject(guardianId, blueSnailPosition, region);
 
                     gameScene.GameObjectCollection.Add(blueSnail);
 
                     // Portal Game Object
                     var portalId = idGenerator.GenerateId();
-                    var portal = new PortalGameObject(portalId, new Vector2(12.5f, -1.125f));
-                    portal.AddProximityChecker(gameScene.MatrixRegion);
+                    var portalPosition = new Vector2(12.5f, -1.125f);
+                    var portal = 
+                        new PortalGameObject(portalId, portalPosition, region);
                     portal.AddPortalData((byte)Map.Lobby);
 
                     gameScene.GameObjectCollection.Add(portal);
