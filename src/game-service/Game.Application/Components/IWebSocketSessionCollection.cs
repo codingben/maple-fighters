87f95a65ceefacf9bbb4b4ccf50e7fdbb3ae364a
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-10 18:24:43 +0300

    :alien: Rename to IWebSocketSessionCollection.

---
+++ src/game-service/Game.Application/Components/IWebSocketSessionCollection.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.Application.Components
+{
+    public interface IWebSocketSessionCollection
+    {
+        bool AddSessionData(int id, WebSocketSessionData webSocketSessionData);
+
+        bool RemoveSessionData(int id);
+
+        bool GetSessionData(int id, out WebSocketSessionData webSocketSessionData);
+    }
+}
