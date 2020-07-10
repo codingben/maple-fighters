40d0e7a822b9cf6cd9a15e0607f67f75e330e1e0
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-10 18:25:04 +0300

    :alien: Rename to WebSocketSessionCollection.

---
+++ src/game-service/Game.Application/Components/WebSocketSessionCollection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using Common.ComponentModel;
+
+namespace Game.Application.Components
+{
+    [ComponentSettings(ExposedState.Exposable)]
+    public class WebSocketSessionCollection : ComponentBase, IWebSocketSessionCollection
+    {
+        private readonly ConcurrentDictionary<int, WebSocketSessionData> collection;
+
+        public WebSocketSessionCollection()
+        {
+            collection = new ConcurrentDictionary<int, WebSocketSessionData>();
+        }
+
+        public bool AddSessionData(int id, WebSocketSessionData webSocketSessionData)
+        {
+            return collection.TryAdd(id, webSocketSessionData);
+        }
+
+        public bool RemoveSessionData(int id)
+        {
+            return collection.TryRemove(id, out _);
+        }
+
+        public bool GetSessionData(int id, out WebSocketSessionData webSocketSessionData)
+        {
+            return collection.TryGetValue(id, out webSocketSessionData);
+        }
+    }
+}
