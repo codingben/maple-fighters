d86c8203aa4d3bc0939e9f46a81da32b2a32d7ff
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-11 17:59:39 +0300

    :alien: Completed TODOs.

--- src/game-service/Game.Application/Handlers/EnterSceneMessageHandler.cs
+++ src/game-service/Game.Application/Handlers/EnterSceneMessageHandler.cs
@@ -1,3 +1,4 @@
+using Game.Application.Components;
 using Game.Application.Messages;
 using Game.Application.Network;
 using Game.Application.Objects;
@@ -8,13 +9,19 @@ namespace Game.Application.Handlers
     public class EnterSceneMessageHandler : IMessageHandler
     {
         private readonly IGameObject player;
+        private readonly IGameSceneCollection gameSceneCollection;
         private readonly ICharacterData characterData;
+        private readonly IPresenceMapProvider presenceMapProvider;
+        private readonly IProximityChecker proximityChecker;
         private readonly IMessageSender messageSender;
 
-        public EnterSceneMessageHandler(IGameObject player)
+        public EnterSceneMessageHandler(IGameObject player, IGameSceneCollection gameSceneCollection)
         {
             this.player = player;
+            this.gameSceneCollection = gameSceneCollection;
             this.characterData = player.Components.Get<CharacterData>();
+            this.presenceMapProvider = player.Components.Get<IPresenceMapProvider>();
+            this.proximityChecker = player.Components.Get<IProximityChecker>();
             this.messageSender = player.Components.Get<MessageSender>();
         }
 
@@ -22,14 +29,29 @@ namespace Game.Application.Handlers
         {
             var message =
                 MessageUtils.DeserializeMessage<EnterSceneMessage>(rawData);
+            var map = message.Map;
             var name = message.CharacterName;
             var characterType = message.CharacterType;
 
             characterData.SetName(name);
             characterData.SetCharacterType(characterType);
 
-            // TODO: Add to game scene
-            // TODO: Set matrix region
+            if (gameSceneCollection.TryGet((Map)map, out var gameScene))
+            {
+                // Set's player position
+                var position = gameScene.GamePlayerSpawnData.GetSpawnPosition();
+                player.Transform.SetPosition(position);
+
+                // Set's map
+                presenceMapProvider.SetMap(map);
+
+                // Set's region
+                var region = gameScene.MatrixRegion;
+                proximityChecker.SetMatrixRegion(region);
+
+                // Adds to this game scene
+                gameScene.GameObjectCollection.Add(player);
+            }
 
             SendEnteredSceneMessage();
         }
