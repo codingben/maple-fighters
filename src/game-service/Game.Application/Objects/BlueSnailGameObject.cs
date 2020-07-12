acb69e41a562f2cc5cd8a240700c257a80ce7b06
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-12 20:30:03 +0300

    :alien: Creating body data.

--- src/game-service/Game.Application/Objects/BlueSnailGameObject.cs
+++ src/game-service/Game.Application/Objects/BlueSnailGameObject.cs
@@ -1,5 +1,7 @@
+using Box2DX.Dynamics;
 using Common.MathematicsHelper;
 using InterestManagement;
+using Physics.Box2D;
 
 namespace Game.Application.Objects
 {
@@ -9,6 +11,19 @@ namespace Game.Application.Objects
             : base(id, name: "BlueSnail", region)
         {
             Transform.SetPosition(position);
+        }
+
+        public NewBodyData CreateBodyData()
+        {
+            var bodyDef = new BodyDef();
+            bodyDef.Position.Set(Transform.Position.X, Transform.Position.Y);
+
+            var polygonDef = new PolygonDef();
+            polygonDef.SetAsBox(5, 5);
+            polygonDef.Density = 0.0f;
+            polygonDef.Filter = new FilterData();
+
+            return new NewBodyData(Id, bodyDef, polygonDef);
         }
     }
 }
