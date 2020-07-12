356b1ab86db32863d19d581be8bdfc0db2787c84
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-12 18:00:40 +0300

    :alien: Creating physics simulation window.

--- src/game-service/Game.PhysicsTests/Program.cs
+++ src/game-service/Game.PhysicsTests/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading;
+using Box2D.Window;
+using static Box2DX.Dynamics.DebugDraw;
 
 namespace Game.PhysicsTests
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var thread = new Thread(new ThreadStart(CreateWindow))
+            {
+                Priority = ThreadPriority.Lowest,
+                IsBackground = true
+            };
+
+            thread.Start();
+
+            Console.ReadKey();
+        }
+
+        private static void CreateWindow()
+        {
+            var simulationWindow = new SimulationWindow("Physics Tests", 800, 600);
+
+            simulationWindow.SetView(new CameraView());
+
+            var physicsDrawer = new DrawPhysics(simulationWindow);
+
+            physicsDrawer.AppendFlags(DrawFlags.Aabb);
+            physicsDrawer.AppendFlags(DrawFlags.Shape);
+            physicsDrawer.AppendFlags(DrawFlags.Pair);
+            physicsDrawer.AppendFlags(DrawFlags.Joint);
+
+            simulationWindow.VSync = OpenTK.VSyncMode.On;
+            simulationWindow.Run(25.0);
         }
     }
 }
