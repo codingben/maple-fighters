c78a44a0624e48aaf888284b2d6bf9ecd3582268
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-12 20:34:00 +0300

    :alien: Wrote several tests.

--- src/game-service/Game.PhysicsTests/Program.cs
+++ src/game-service/Game.PhysicsTests/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Threading;
 using Box2D.Window;
+using Common.ComponentModel;
+using Common.Components;
+using Game.Application.Components;
 using static Box2DX.Dynamics.DebugDraw;
 
 namespace Game.PhysicsTests
@@ -22,16 +25,25 @@ namespace Game.PhysicsTests
 
         private static void CreateWindow()
         {
-            var simulationWindow = new SimulationWindow("Physics Tests", 800, 600);
+            IComponents components = new ComponentsContainer();
+            components.Add(new IdGenerator());
+            components.Add(new GameSceneCollection());
+            components.Add(new GameSceneManager());
 
+            var simulationWindow = new SimulationWindow("Physics Tests", 800, 600);
             simulationWindow.SetView(new CameraView());
 
             var physicsDrawer = new DrawPhysics(simulationWindow);
-
             physicsDrawer.AppendFlags(DrawFlags.Aabb);
             physicsDrawer.AppendFlags(DrawFlags.Shape);
             physicsDrawer.AppendFlags(DrawFlags.Pair);
             physicsDrawer.AppendFlags(DrawFlags.Joint);
+
+            var gameSceneCollection = components.Get<IGameSceneCollection>();
+            if (gameSceneCollection.TryGet(Map.TheDarkForest, out var gameScene))
+            {
+                gameScene.PhysicsWorldManager.SetDebugDraw(physicsDrawer);
+            }
 
             simulationWindow.VSync = OpenTK.VSyncMode.On;
             simulationWindow.Run(25.0);
