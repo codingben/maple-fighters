202c109736dcd650bebbc7bb0761f29ef729a0fc
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-13 17:43:57 +0300

    :alien: Added body data.

--- src/game-service/Game.Application/Objects/PlayerGameObject.cs
+++ src/game-service/Game.Application/Objects/PlayerGameObject.cs
@@ -1,3 +1,6 @@
+using Box2DX.Dynamics;
+using Physics.Box2D;
+
 namespace Game.Application.Objects
 {
     public class PlayerGameObject : GameObject
@@ -6,6 +9,19 @@ namespace Game.Application.Objects
             : base(id, name: "Player")
         {
             // Left blank intentionally
+        }
+
+        public NewBodyData CreateBodyData()
+        {
+            var bodyDef = new BodyDef();
+            bodyDef.Position.Set(Transform.Position.X, Transform.Position.Y);
+
+            var polygonDef = new PolygonDef();
+            polygonDef.SetAsBox(5, 5);
+            polygonDef.Density = 0.0f;
+            polygonDef.Filter = new FilterData();
+
+            return new NewBodyData(Id, bodyDef, polygonDef);
         }
     }
 }
