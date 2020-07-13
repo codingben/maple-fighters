40473206f76ce79ee2d804f03756a141b0a17db1
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-13 21:11:26 +0300

    :alien: New script to handle contact events.

---
+++ src/game-service/Game.Application/Objects/Components/MobContactEvents.cs
@@ -0,0 +1,27 @@
+﻿using Box2DX.Dynamics;
+using Physics.Box2D;
+
+namespace Game.Application.Objects.Components
+{
+    public class MobContactEvents : IContactEvents
+    {
+        private int id;
+
+        public MobContactEvents(int id)
+        {
+            this.id = id;
+        }
+
+        public void OnBeginContact(Body body)
+        {
+            var player = body.GetUserData() as IGameObject;
+            var playerAttacked = player?.Components.Get<IPlayerAttacked>();
+            playerAttacked?.Attack(id);
+        }
+
+        public void OnEndContact(Body body)
+        {
+            // Left blank intentionally
+        }
+    }
+}
