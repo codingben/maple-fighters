69a39f96c7286fba69c46b98c0878d7208255280
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-15 22:43:55 +0300

    :alien: Removed functions.

--- lib/common/Common.ComponentModel/Utils.cs
+++ lib/common/Common.ComponentModel/Utils.cs
@@ -1,40 +1,9 @@
-﻿using System;
-using Common.ComponentModel.Exceptions;
+﻿using Common.ComponentModel.Exceptions;
 
 namespace Common.ComponentModel
 {
     internal static class Utils
     {
-        public static ComponentSettingsAttribute GetComponentSettings<TComponent>()
-            where TComponent : IComponent
-        {
-            var componentSettings =
-                (ComponentSettingsAttribute)Attribute.GetCustomAttribute(
-                    typeof(TComponent),
-                    typeof(ComponentSettingsAttribute));
-            if (componentSettings == null)
-            {
-                if (typeof(IDisposable).IsAssignableFrom(typeof(TComponent)))
-                {
-                    throw new ComponentSettingsMissingException(typeof(TComponent).Name);
-                }
-            }
-
-            return componentSettings;
-        }
-
-        public static ExposedState GetExposedState<TComponent>()
-            where TComponent : IComponent
-        {
-            var componentSettings = GetComponentSettings<TComponent>();
-            if (componentSettings != null)
-            {
-                return componentSettings.ExposedState;
-            }
-
-            throw new InvalidOperationException();
-        }
-
         public static void ThrowExceptionIfNotInterface<TComponent>()
             where TComponent : class
         {
@@ -43,7 +12,7 @@ namespace Common.ComponentModel
                 return;
             }
 
-            throw new ComponentSettingsMissingException(typeof(TComponent).Name);
+            throw new ComponentInterfaceExpectedException(typeof(TComponent).Name);
         }
     }
 }
