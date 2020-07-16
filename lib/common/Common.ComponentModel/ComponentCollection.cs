33f6d66dd90fe6c81bfecf6acb9c20efb53e1651
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-16 17:39:32 +0300

    :alien: New script to get components.

---
+++ lib/common/Common.ComponentModel/ComponentCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.ComponentModel
+{
+    /// <summary>
+    /// Collection of components.
+    /// </summary>
+    public sealed class ComponentCollection : IComponents
+    {
+        private readonly IReadOnlyList<IComponent> components;
+
+        public ComponentCollection(IEnumerable<IComponent> collection)
+        {
+            components = new List<IComponent>(collection);
+
+            foreach (var component in components)
+            {
+                component.Awake(this);
+            }
+        }
+
+        /// <summary>
+        /// See <see cref="IComponents.Get{T}"/> for more information.
+        /// </summary>
+        TComponent IComponents.Get<TComponent>()
+        {
+            if (typeof(TComponent).IsInterface == false)
+            {
+                var name = typeof(TComponent).Name;
+                var message =
+                    $"The called component {name} is not through an interface.";
+
+                throw new Exception(message);
+            }
+
+            return components.OfType<TComponent>().FirstOrDefault();
+        }
+
+        /// <summary>
+        /// See <see cref="IDisposable.Dispose"/> for more information.
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var component in components)
+            {
+                component?.Dispose();
+            }
+        }
+    }
+}
