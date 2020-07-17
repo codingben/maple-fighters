1989f0c1e71f88ab10d2c3f40ce1832969e4d785
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-17 21:11:39 +0300

    :alien: Code refactoring.

--- src/game-service/Game.Application/Objects/PortalGameObject.cs
+++ src/game-service/Game.Application/Objects/PortalGameObject.cs
@@ -1,19 +1,14 @@
+using Common.ComponentModel;
 using Common.MathematicsHelper;
-using Game.Application.Objects.Components;
 
 namespace Game.Application.Objects
 {
     public class PortalGameObject : GameObject
     {
-        public PortalGameObject(int id, Vector2 position)
+        public PortalGameObject(int id, Vector2 position, IComponent[] components)
             : base(id, name: "Portal")
         {
             Transform.SetPosition(position);
-        }
-
-        public void AddPortalData(byte map)
-        {
-            Components.Add(new PortalData(map));
         }
     }
 }
