769de78da00d29bab40e197642008b172a59b6c1
Ben Ukhanov <ben1zuk321@gmail.com>
2020-07-31 18:25:11 +0300

    :alien: Started implementing the game api.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/GameApi.cs
@@ -1,28 +1,58 @@
 using Game.Messages;
+using NativeWebSocket;
 using UnityEngine;
 
 namespace Scripts.Services.Game
 {
     public class GameApi : MonoBehaviour, IGameApi
     {
-        public void ChangeAnimationState(ChangeAnimationStateMessage message)
+        private WebSocket webSocket;
+
+        private async void Start()
+        {
+            webSocket = new WebSocket("ws://localhost:50060");
+
+            await webSocket.Connect();
+        }
+
+        private void Update()
+        {
+#if !UNITY_WEBGL || UNITY_EDITOR
+            webSocket.DispatchMessageQueue();
+#endif
+        }
+
+        private async void OnApplicationQuit()
+        {
+            await webSocket.Close();
+        }
+
+        public async void ChangeAnimationState(ChangeAnimationStateMessage message)
         {
             throw new System.NotImplementedException();
         }
 
-        public void ChangePosition(ChangePositionMessage message)
+        public async void ChangePosition(ChangePositionMessage message)
         {
             throw new System.NotImplementedException();
         }
 
-        public void ChangeScene(ChangeSceneMessage message)
+        public async void ChangeScene(ChangeSceneMessage message)
         {
             throw new System.NotImplementedException();
         }
 
-        public void EnterScene(EnterSceneMessage message)
+        public async void EnterScene(EnterSceneMessage message)
         {
             throw new System.NotImplementedException();
+        }
+
+        private async void SendMessage(byte[] rawData)
+        {
+            if (webSocket.State == WebSocketState.Open)
+            {
+                await webSocket.Send(rawData);
+            }
         }
     }
 }
