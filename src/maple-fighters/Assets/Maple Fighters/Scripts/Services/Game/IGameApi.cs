739c6c78242155a6f540ec3033ae2dfccc7343d9
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-01 22:30:04 +0300

    :alien: Added game events.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/IGameApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Game/IGameApi.cs
@@ -1,9 +1,24 @@
 using System;
+using Game.Messages;
 
 namespace Scripts.Services.Game
 {
     public interface IGameApi
     {
+        Action<EnteredSceneMessage> SceneEntered { get; }
+
+        Action<GameObjectsAddedMessage> SceneObjectsAdded { get; }
+
+        Action<GameObjectsRemovedMessage> SceneObjectsRemoved { get; }
+
+        Action<PositionChangedMessage> PositionChanged { get; }
+
+        Action<AnimationStateChangedMessage> AnimationStateChanged { get; }
+
+        Action<AttackedMessage> Attacked { get; }
+
+        Action<BubbleNotificationMessage> BubbleMessageReceived { get; }
+
         void SendMessage<TCode, TMessage>(TCode code, TMessage message)
             where TCode : IComparable, IFormattable, IConvertible
             where TMessage : class;
