b968dabd17510ae8076e4f74d5c5ce3cfcf4417f
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-08 20:03:07 +0300

    :alien: Added character datas.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Character/CharacterData.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Character/CharacterData.cs
@@ -2,6 +2,10 @@ namespace Scripts.Gameplay.Player
 {
     public class CharacterData
     {
+        public int Id { get; set; }
 
+        public string CharacterName { get; set; }
+
+        public byte CharacterType { get; set; }
     }
 }
