b1f9d7b9554f329981538da7e3599ec2ae9d9108
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-09 14:58:11 +0300

    :alien: New character data script.

---
+++ src/game-service/Game.Application/Messages/CharacterData.cs
@@ -0,0 +1,11 @@
+namespace Game.Messages
+{
+    public class CharacterData
+    {
+        public string CharacterName { get; set; }
+
+        public byte CharacterType { get; set; }
+
+        public bool HasCharacter { get; set; }
+    }
+}
