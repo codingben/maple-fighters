150b8241f403486eaec0f0d3b92876ac07e979b4
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-11 23:28:11 +0300

    :alien: Added TODOs.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Controller/CharacterViewInteractor.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Controller/CharacterViewInteractor.cs
@@ -31,129 +31,27 @@ namespace Scripts.UI.CharacterSelection
 
         public void ConnectToGameServer()
         {
-            // TODO: Connect
-            // onConnectionFinishedListener.OnConnectionSucceed()
-            // onConnectionFinishedListener.OnConnectionFailed()
+            // TODO: Implement
         }
 
         public void GetCharacters()
         {
-            /*var parameters =
-                await characterSelectorApi.GetCharactersAsync(yield);
-            var characters = parameters.Characters;
-
-            foreach (var character in characters)
-            {
-                var characterDetails = new CharacterDetails(
-                    character.Name,
-                    character.Index.ToUiCharacterIndex(),
-                    character.CharacterType.ToUiCharacterClass(),
-                    character.LastMap.ToString(),
-                    character.HasCharacter);
-
-                onCharacterReceivedListener.OnCharacterReceived(
-                    characterDetails);
-            }
-
-            onCharacterReceivedListener.OnAfterCharacterReceived();*/
+            // TODO: Implement
         }
 
         public void ValidateCharacter(int characterIndex)
         {
-            /*var parameters =
-                new ValidateCharacterRequestParameters(characterIndex);
-
-            var responseParameters =
-                await characterSelectorApi.ValidateCharacterAsync(
-                    yield,
-                    parameters);
-            var status = responseParameters.Status;
-            var map = responseParameters.Map;
-            var mapName = map.ToString();
-
-            switch (status)
-            {
-                case CharacterValidationStatus.Ok:
-                    {
-                        onCharacterValidationFinishedListener
-                            .OnCharacterValidated(mapName);
-                        break;
-                    }
-
-                case CharacterValidationStatus.Wrong:
-                    {
-                        onCharacterValidationFinishedListener
-                            .OnCharacterUnvalidated();
-                        break;
-                    }
-            }*/
+            // TODO: Implement
         }
 
         public void RemoveCharacter(int characterIndex)
         {
-            /*var parameters =
-                new RemoveCharacterRequestParameters(characterIndex);
-
-            var responseParameters =
-                await characterSelectorApi.RemoveCharacterAsync(
-                    yield,
-                    parameters);
-            var status = responseParameters.Status;
-
-            switch (status)
-            {
-                case RemoveCharacterStatus.Succeed:
-                    {
-                        onCharacterDeletionFinishedListener
-                            .OnCharacterDeletionSucceed();
-                        break;
-                    }
-
-                case RemoveCharacterStatus.Failed:
-                    {
-                        onCharacterDeletionFinishedListener
-                            .OnCharacterDeletionFailed();
-                        break;
-                    }
-            }*/
+            // TODO: Implement
         }
 
         public void CreateCharacter(UICharacterDetails characterDetails)
         {
-            /*var parameters = new CreateCharacterRequestParameters(
-                characterDetails.GetCharacterClass().FromUiCharacterClass(),
-                characterDetails.GetCharacterName(),
-                characterDetails.GetCharacterIndex().FromUiCharacterIndex());
-
-            var responseParameters =
-                await characterSelectorApi.CreateCharacterAsync(
-                    yield,
-                    parameters);
-            var status = responseParameters.Status;
-
-            switch (status)
-            {
-                case CharacterCreationStatus.Succeed:
-                    {
-                        onCharacterCreationFinishedListener
-                            .OnCharacterCreated();
-                        break;
-                    }
-
-                case CharacterCreationStatus.Failed:
-                    {
-                        onCharacterCreationFinishedListener
-                            .OnCreateCharacterFailed(CharacterCreationFailed.Unknown);
-                        break;
-                    }
-
-                case CharacterCreationStatus.NameUsed:
-                    {
-                        onCharacterCreationFinishedListener
-                            .OnCreateCharacterFailed(CharacterCreationFailed.NameAlreadyInUse);
-                        break;
-                    }
-            }*/
+            // TODO: Implement
         }
     }
 }
