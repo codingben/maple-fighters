dba56e213b0cf4258c3fe770aadf3d25f4d34446
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-14 19:31:18 +0300

    :alien: Fake interactor.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Controller/CharacterViewInteractor.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/UI/CharacterSelection/Controller/CharacterViewInteractor.cs
@@ -15,6 +15,9 @@ namespace Scripts.UI.CharacterSelection
         private IOnCharacterCreationFinishedListener onCharacterCreationFinishedListener;
         private IOnCharacterDeletionFinishedListener onCharacterDeletionFinishedListener;
 
+        // TODO: Remove
+        private UICharacterDetails[] characters;
+
         private void Awake()
         {
             onConnectionFinishedListener =
@@ -29,29 +32,58 @@ namespace Scripts.UI.CharacterSelection
                 GetComponent<IOnCharacterDeletionFinishedListener>();
         }
 
+        private void Start()
+        {
+            // TODO: Get this data from server
+            characters = new UICharacterDetails[]
+            {
+                new UICharacterDetails("Knight", UICharacterIndex.First, UICharacterClass.Knight, "Map_1", false),
+                new UICharacterDetails("Arrow", UICharacterIndex.Second, UICharacterClass.Arrow, "Map_1", false),
+                new UICharacterDetails("Wizard", UICharacterIndex.Third, UICharacterClass.Wizard, "Map_1", false)
+            };
+        }
+
         public void ConnectToGameServer()
         {
-            // TODO: Implement
+            // TODO: Get this data from server
+            onConnectionFinishedListener.OnConnectionSucceed();
         }
 
         public void GetCharacters()
         {
-            // TODO: Implement
+            // TODO: Get this data from server
+            foreach (var character in characters)
+            {
+                onCharacterReceivedListener.OnCharacterReceived(character);
+            }
+
+            onCharacterReceivedListener.OnAfterCharacterReceived();
+
         }
 
         public void ValidateCharacter(int characterIndex)
         {
-            // TODO: Implement
+            // TODO: Get this data from server
+            onCharacterValidationFinishedListener.OnCharacterValidated(mapName: "Map_1");
         }
 
         public void RemoveCharacter(int characterIndex)
         {
-            // TODO: Implement
+            characters[characterIndex].SetHasCharacter(false);
+
+            // TODO: Get this data from server
+            onCharacterDeletionFinishedListener.OnCharacterDeletionSucceed();
         }
 
         public void CreateCharacter(UICharacterDetails characterDetails)
         {
-            // TODO: Implement
+            characterDetails.SetHasCharacter(true);
+
+            var index = (int)characterDetails.GetCharacterIndex();
+            characters[index] = characterDetails;
+
+            // TODO: Get this data from server
+            onCharacterCreationFinishedListener.OnCharacterCreated();
         }
     }
 }
