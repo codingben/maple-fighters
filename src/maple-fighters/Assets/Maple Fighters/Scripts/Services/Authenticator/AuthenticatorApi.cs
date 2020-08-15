6efc6190901e4ed5baf802c5a874d362bfa73b37
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-15 14:29:08 +0300

    :alien: New authenticator api.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/AuthenticatorApi.cs
@@ -0,0 +1,15 @@
+namespace Scripts.Services.Authenticator
+{
+    public class AuthenticatorApi : IAuthenticatorApi
+    {
+        public void Authenticate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Register()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
