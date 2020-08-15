cb4e54557334eb8a0367eee8789b9c610deb2470
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-15 14:34:18 +0300

    :alien: New game provider api.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/GameServerProviderApi.cs
@@ -0,0 +1,10 @@
+namespace Scripts.Services.GameServerProvider
+{
+    public class GameServerProviderApi : IGameServerProviderApi
+    {
+        public void ProvideGameServer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
