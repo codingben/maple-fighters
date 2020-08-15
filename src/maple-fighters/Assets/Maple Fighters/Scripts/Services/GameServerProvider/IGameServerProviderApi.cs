3508eb4ed515b8520c58d97e9bd802bcc2f3ef26
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-15 14:33:28 +0300

    :alien: Code refactoring.

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/IGameServerProviderApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameServerProvider/IGameServerProviderApi.cs
@@ -1,11 +1,7 @@
-﻿using System.Threading.Tasks;
-using CommonTools.Coroutines;
-using GameServerProvider.Client.Common;
-
-namespace Scripts.Services.GameServerProvider
+﻿namespace Scripts.Services.GameServerProvider
 {
     public interface IGameServerProviderApi
     {
-        Task<GameServersProviderResponseParameters> ProvideGameServersAsync(IYield yield);
+        void ProvideGameServer();
     }
 }
