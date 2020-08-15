98ac6ce2e8d44990049c5e78102d01d3b30f3447
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-15 18:29:11 +0300

    :alien: New api scripts.

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/CharacterProvider/CharacterProviderApi.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Scripts.Services.CharacterProvider
+{
+    public class CharacterProviderApi : MonoBehaviour, ICharacterApi
+    {
+        // TODO: Implement
+    }
+}
---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/CharacterProvider/ICharacterProviderApi.cs
@@ -0,0 +1,7 @@
+﻿namespace Scripts.Services.CharacterProvider
+{
+    interface ICharacterProviderApi
+    {
+        // TODO: Implement
+    }
+}
