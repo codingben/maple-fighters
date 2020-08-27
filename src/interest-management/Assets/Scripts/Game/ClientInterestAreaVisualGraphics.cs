6374a2aef5ac0e1d078540e962f78678bc786c9c
Ben Ukhanov <ben1zuk321@gmail.com>
2020-08-27 23:54:39 +0300

    :alien: Some changes.

--- src/interest-management/Assets/Scripts/Game/ClientInterestAreaVisualGraphics.cs
+++ src/interest-management/Assets/Scripts/Game/ClientInterestAreaVisualGraphics.cs
@@ -10,9 +10,7 @@ namespace Game.InterestManagement.Simulation
         private void Update()
         {
             GraphicsUtils.DrawRectangle(
-                transform.position, 
-                transform.localScale, 
-                color);
+                transform.position, transform.localScale, color);
         }
     }
 }
