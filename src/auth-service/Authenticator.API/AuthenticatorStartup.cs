21fde6ea3140b88f3809d72f0f67e2fbfb999bab
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-03 23:52:23 +0300

    :alien: Removed grpc from startup.

--- src/auth-service/Authenticator.API/AuthenticatorStartup.cs
+++ src/auth-service/Authenticator.API/AuthenticatorStartup.cs
@@ -1,12 +1,10 @@
 ﻿using Authenticator.API.Controllers;
-using Authenticator.API.Services;
 using Authenticator.Domain.Aggregates.User;
 using Authenticator.Domain.Aggregates.User.Services;
 using Authenticator.Infrastructure.Repository;
 using Common.MongoDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,8 +15,6 @@ namespace Authenticator.API
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
-
             // services.AddSingleton<IDatabaseProvider>(new MongoDatabaseProvider(url: "mongodb://localhost:27017/maple_fighters"));
             // services.AddSingleton<IAccountRepository, MongoAccountRepository>();
             services.AddSingleton<IAccountRepository, InMemoryAccountRepository>();
@@ -41,10 +37,7 @@ namespace Authenticator.API
             applicationBuilder.UseRouting();
             applicationBuilder.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<AuthenticatorService>();
-                endpoints.MapGet(
-                    "/",
-                    async c => await c.Response.WriteAsync("Please use a gRPC client."));
+                // TODO: Implement
             });
 
             logger.LogInformation("AuthenticatorStartup::Configure()");
