37ece2781e2f700be30865e7ee246d908ce3351d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-04 23:12:17 +0300

    :alien: Rename to LoginData.

--- src/auth-service/Authenticator.API/Datas/LoginData.cs
+++ src/auth-service/Authenticator.API/Datas/LoginData.cs
@@ -1,12 +1,12 @@
 ﻿namespace Authenticator.API.Datas
 {
-    public struct AuthenticationData
+    public struct LoginData
     {
         public string Email { get; }
 
         public string Password { get; }
 
-        public AuthenticationData(string email, string password)
+        public LoginData(string email, string password)
         {
             Email = email;
             Password = password;
