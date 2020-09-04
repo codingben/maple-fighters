9bf69d813153c02868b40511b4fb8cb216543e58
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-04 20:50:12 +0300

    :alien: Removed comments.

--- src/auth-service/Authenticator.API/Program.cs
+++ src/auth-service/Authenticator.API/Program.cs
@@ -8,10 +8,6 @@ namespace Authenticator.API
     {
         public static void Main() => CreateHostBuilder().Build().Run();
 
-        /*
-         * Additional configuration is required to successfully run gRPC on macOS.
-         * For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        */
         public static IHostBuilder CreateHostBuilder() =>
             Host.CreateDefaultBuilder()
                 .ConfigureLogging(x =>
