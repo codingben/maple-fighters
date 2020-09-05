b690729bfdadc44fb30b9f9c2262189fc638177c
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-05 20:19:02 +0300

    :alien: Added more error messages.

--- src/auth-service/Authenticator.API/Constants/ErrorMessages.cs
+++ src/auth-service/Authenticator.API/Constants/ErrorMessages.cs
@@ -2,6 +2,10 @@ namespace Authenticator.API.Constants
 {
     public static class ErrorMessages
     {
+        public const string InvalidEmail = "Invalid email address.";
         public const string EmailAlreadyExists = "This email address is already being used.";
+        public const string PasswordRequired = "Password is required.";
+        public const string FirstNameRequired = "First name is required.";
+        public const string LastNameRequired = "Last name is required.";
     }
 }
