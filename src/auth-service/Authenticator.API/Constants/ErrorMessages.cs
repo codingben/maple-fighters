f40d477b58b3e05365b19412b64bf49e87bfaab8
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-05 20:14:22 +0300

    :alien: New script to get error messages.

---
+++ src/auth-service/Authenticator.API/Constants/ErrorMessages.cs
@@ -0,0 +1,7 @@
+namespace Authenticator.API.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string EmailAlreadyExists = "This email address is already being used.";
+    }
+}
