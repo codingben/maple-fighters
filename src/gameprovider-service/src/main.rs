d08659b3e07202f680cd63778aacc8eb9177d06c
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-07 22:24:47 +0300

    :alien: Code refactoring.

--- src/gameprovider-service/src/main.rs
+++ src/gameprovider-service/src/main.rs
@@ -1,33 +1,24 @@
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use std::env;
+
 mod database;
 mod endpoints;
 mod models;
-mod game_provider {
-    tonic::include_proto!("game_provider");
-}
 
-use dotenv::dotenv;
-use endpoints::GameProviderService;
-use game_provider::game_provider_server::GameProviderServer;
-use std::{env, error::Error};
-use tonic::transport::Server;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+#[actix_rt::main]
+async fn main() -> Result<()> {
     dotenv().expect("Could not find .env file");
 
     let address = env::var("IP_ADDRESS").expect("IP_ADDRESS not found");
     let data_path = env::var("DATABASE_PATH").expect("DATABASE_PATH not found");
     let address_parsed = address.parse()?;
-    let game_provider_service = GameProviderService {
-        game_collection: database::load(&data_path),
-    };
+    // TODO: Use: database::load(&data_path)
 
     println!("Server is running {}", address);
 
-    Server::builder()
-        .add_service(GameProviderServer::new(game_provider_service))
-        .serve(address_parsed)
-        .await?;
-
-    Ok(())
+    HttpServer::new(move || App::new())
+        .bind(address_parsed)?
+        .run()
+        .await
 }
