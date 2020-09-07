405ad4c0287a97814275ab9eb687e35fb545e8bd
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-07 22:17:54 +0300

    :alien: Removed build script.

--- src/gameprovider-service/build.rs
+++
@@ -1,9 +0,0 @@
-use std::error::Error;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(false)
-        .compile(&["proto/gameprovider.proto"], &["proto/"])?;
-    Ok(())
-}
