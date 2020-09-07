b0c081137adec4c5dfdcd9e7e2057abefe1d9e14
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-07 23:39:53 +0300

    :alien: Removed old code.

--- src/gameprovider-service/src/endpoints.rs
+++ src/gameprovider-service/src/endpoints.rs
@@ -1,18 +1,3 @@
-use super::game_provider::{game_provider_server::GameProvider, *};
-use tonic::{Request, Response, Status};
-
-pub struct GameProviderService {
-    pub game_collection: Vec<Game>,
-}
-
-#[tonic::async_trait]
-impl GameProvider for GameProviderService {
-    async fn get_game_servers(
-        &self,
-        _request: Request<()>,
-    ) -> Result<Response<GetGameServersResponse>, Status> {
-        Ok(Response::new(GetGameServersResponse {
-            game_collection: self.game_collection.to_vec(),
-        }))
-    }
+pub fn get_game_servers() {
+    // TODO: Implement
 }
