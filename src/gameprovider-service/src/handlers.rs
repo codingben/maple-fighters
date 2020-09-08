2f6248d43d2ca98b58bde1102c5327eae35253b9
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-08 23:09:29 +0300

    :alien: Implemented handler.

--- src/gameprovider-service/src/handlers.rs
+++ src/gameprovider-service/src/handlers.rs
@@ -1,5 +1,9 @@
-use actix_web::Responder;
+use crate::models::GameCollection;
+use actix_web::web::Data;
+use actix_web::HttpResponse;
+use std::sync::Arc;
 
-pub fn get_game_servers() {
-    // TODO: Implement
+pub fn get_game_servers(data: Data<Arc<GameCollection>>) -> HttpResponse {
+    let data = data.clone();
+    HttpResponse::Ok().json(&data.game_collection)
 }
