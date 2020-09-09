af4507502bbabe95ae973d370ad0b8101ee30d72
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-09 23:47:40 +0300

    :alien: Code refactoring.

--- src/character-service/src/main.rs
+++ src/character-service/src/main.rs
@@ -2,22 +2,14 @@
 extern crate diesel;
 extern crate dotenv;
 
-mod endpoints;
-mod models;
-mod schema;
-mod character {
-    tonic::include_proto!("character");
-}
-
-use character::character_server::CharacterServer;
 use diesel::{pg::PgConnection, r2d2::ConnectionManager, r2d2::Pool};
 use dotenv::dotenv;
-use endpoints::CharacterService;
-use std::{env, error::Error};
-use tonic::transport::Server;
+use std::env;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+mod models;
+mod schema;
+
+fn main() {
     dotenv().expect("Could not find .env file");
 
     let address = env::var("IP_ADDRESS").expect("IP_ADDRESS not found");
@@ -27,15 +19,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .build(manager)
         .expect("Failed to create pool");
 
-    let character_service = CharacterService { pool: r2d2_pool };
-    let address_parsed = address.parse()?;
-
     println!("Server is running {}", address);
-
-    Server::builder()
-        .add_service(CharacterServer::new(character_service))
-        .serve(address_parsed)
-        .await?;
-
-    Ok(())
 }
