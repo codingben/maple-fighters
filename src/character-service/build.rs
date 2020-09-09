5cd350267e2a5f0322cf0f6690eedc01ce457081
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-09 23:47:20 +0300

    :alien: Removed build script.

--- src/character-service/build.rs
+++
@@ -1,9 +0,0 @@
-use std::error::Error;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(false)
-        .compile(&["proto/character.proto"], &["proto/"])?;
-    Ok(())
-}
