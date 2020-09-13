a82e0abb4796f28d77e36910c0af92fd26e576f2
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-14 01:04:10 +0300

    :alien: Code refactoring.

--- src/character-service/src/handlers.rs
+++ src/character-service/src/handlers.rs
@@ -4,12 +4,12 @@ use actix_web::{web, web::Json, web::Path, HttpResponse};
 
 pub fn create_new(db: web::Data<Pool>, character: Json<NewCharacter>) -> HttpResponse {
     let conn = db.get().unwrap();
-    let data = character.into_inner();
+    let new_character = character.into_inner();
 
-    if is_character_name_used(data.userid, &data.charactername, &conn) {
+    if is_character_name_used(new_character.userid, &new_character.charactername, &conn) {
         HttpResponse::BadRequest().json("Please choose a different character name.")
     } else {
-        if insert_character(data, &conn) {
+        if insert_character(new_character, &conn) {
             HttpResponse::Created().finish()
         } else {
             HttpResponse::InternalServerError().finish()
