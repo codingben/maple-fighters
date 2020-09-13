cbd15992a9d0c0cb00403fc45f25bd7274c0ece3
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-14 01:32:25 +0300

    :alien: Code refactoring.

--- src/gameprovider-service/src/database.rs
+++ src/gameprovider-service/src/database.rs
@@ -1,12 +1,11 @@
-use super::models::Game;
+use crate::models::Game;
 use serde_json;
 use std::{fs::File, path::Path};
 
 pub fn load(path: &str) -> Vec<Game> {
     let json_file_path = Path::new(path);
     let json_file = File::open(json_file_path).expect("File not found");
-    let json_data: Vec<super::models::Game> =
-        serde_json::from_reader(&json_file).expect("Could not read json");
+    let json_data: Vec<Game> = serde_json::from_reader(&json_file).expect("Could not read json");
 
     json_data
         .into_iter()
