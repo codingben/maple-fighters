7fe9fd73a1162a7d690fa0c79e73449ff3815e18
Ben Ukhanov <ben1zuk321@gmail.com>
2020-09-14 00:57:52 +0300

    :alien: Implemented create_new handler.

--- src/character-service/src/handlers.rs
+++ src/character-service/src/handlers.rs
@@ -1,9 +1,20 @@
-use crate::database::Pool;
+use crate::database::*;
 use crate::models::NewCharacter;
 use actix_web::{web, web::Json, web::Path, HttpResponse};
 
 pub fn create_new(db: web::Data<Pool>, character: Json<NewCharacter>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+    let conn = db.get().unwrap();
+    let data = character.into_inner();
+
+    if is_character_name_used(data.userid, &data.charactername, &conn) {
+        HttpResponse::BadRequest().json("Please choose a different character name.")
+    } else {
+        if insert_character(data, &conn) {
+            HttpResponse::Created().finish()
+        } else {
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
 
 pub fn remove_by_id(db: web::Data<Pool>, id: Path<i32>) -> HttpResponse {
