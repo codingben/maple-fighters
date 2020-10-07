0b1fd15718bcba4fc92500ada7f9940b886ff00d
Ben Ukhanov <ben1zuk321@gmail.com>
2020-10-07 22:54:17 +0300

    :alien: Added a process exit event handler.

--- src/game-service/Game.AppStarter/GameStarter.cs
+++ src/game-service/Game.AppStarter/GameStarter.cs
@@ -5,21 +5,38 @@ namespace Game.AppStarter
 {
     public static class GameStarter
     {
-        public static void Main()
+        private static IServerApplication serverApplication;
+
+        static GameStarter()
         {
-            // TODO: Docker
-            var serverApplication = GetServerApplication();
-            serverApplication.Startup();
-
-            // TODO: Remove
-            Console.ReadLine();
-
-            // TODO: Remove
-            serverApplication.Shutdown();
+            AddProcessExitEventHandler();
         }
 
-        private static IServerApplication GetServerApplication()
+        public static void Main()
         {
+            StartApplication();
+        }
+
+        private static void StartApplication()
+        {
+            serverApplication = CreateServerApplication();
+            serverApplication.Startup();
+        }
+
+        private static void StopApplication()
+        {
+            serverApplication?.Shutdown();
+        }
+
+        private static void AddProcessExitEventHandler()
+        {
+            // It's supposed to work with a docker container:
+            AppDomain.CurrentDomain.ProcessExit += (x, y) => StopApplication();
+        }
+
+        private static IServerApplication CreateServerApplication()
+        {
+            // TODO: Get IP address from config
             return new GameApplication("ws://localhost:50051");
         }
     }
