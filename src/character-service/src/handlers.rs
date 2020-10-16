59ed4c3cc283848c951f0bf0e87090a0cac256a2
Ben Ukhanov <ben1zuk321@gmail.com>
2020-10-16 21:49:42 +0300

    :alien: Code refactoring.

--- src/character-service/src/handlers.rs
+++ src/character-service/src/handlers.rs
@@ -2,8 +2,8 @@ use crate::database::*;
 use crate::models::NewCharacter;
 use actix_web::{web, web::Json, web::Path, Error, HttpResponse};
 
-pub async fn create(db: web::Data<Pool>, data: Json<NewCharacter>) -> Result<HttpResponse, Error> {
-    let conn = db.get().unwrap();
+pub async fn create(pool: web::Data<Pool>, data: Json<NewCharacter>) -> Result<HttpResponse, Error> {
+    let conn = pool.get().unwrap();
     let character_data = data.into_inner();
     let is_inserted = web::block(move || insert(character_data, &conn))
         .await
@@ -16,8 +16,8 @@ pub async fn create(db: web::Data<Pool>, data: Json<NewCharacter>) -> Result<Htt
     }
 }
 
-pub async fn delete_by_id(db: web::Data<Pool>, id: Path<i32>) -> Result<HttpResponse, Error> {
-    let conn = db.get().unwrap();
+pub async fn delete_by_id(pool: web::Data<Pool>, id: Path<i32>) -> Result<HttpResponse, Error> {
+    let conn = pool.get().unwrap();
     let character_id = id.into_inner();
     let is_deleted = web::block(move || delete(character_id, &conn))
         .await
@@ -30,8 +30,8 @@ pub async fn delete_by_id(db: web::Data<Pool>, id: Path<i32>) -> Result<HttpResp
     }
 }
 
-pub async fn get_all(db: web::Data<Pool>, id: Path<i32>) -> Result<HttpResponse, Error> {
-    let conn = db.get().unwrap();
+pub async fn get_all(pool: web::Data<Pool>, id: Path<i32>) -> Result<HttpResponse, Error> {
+    let conn = pool.get().unwrap();
     let user_id = id.into_inner();
     let characters = web::block(move || get_all_by_user_id(user_id, &conn))
         .await
