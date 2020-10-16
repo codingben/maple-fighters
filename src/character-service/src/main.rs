27f101d8216d40088da248efc252915055335f10
Ben Ukhanov <ben1zuk321@gmail.com>
2020-10-16 20:51:02 +0300

    :alien: Added openssl crate.

--- src/character-service/src/main.rs
+++ src/character-service/src/main.rs
@@ -1,3 +1,7 @@
+// As of Rust 1.34.0, these dependencies need to be declared in this order using
+// `extern crate` in your `main.rs` file. See
+// https://github.com/emk/rust-musl-builder/issues/69.
+extern crate openssl;
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
