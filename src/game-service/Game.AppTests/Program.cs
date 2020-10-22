56cfdb5c339cc5f651a50933c1be952f1679fadc
Ben Ukhanov <ben1zuk321@gmail.com>
2020-10-22 23:16:39 +0300

    :alien: Added new project.

---
+++ src/game-service/Game.AppTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game.AppTests
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
