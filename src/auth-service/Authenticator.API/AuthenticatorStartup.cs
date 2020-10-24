946af56fec3dc94204799f33163ef6ba6abe7043
Ben Ukhanov <ben1zuk321@gmail.com>
2020-10-24 22:23:57 +0300

    :alien: Getting the url of the database.

--- src/auth-service/Authenticator.API/AuthenticatorStartup.cs
+++ src/auth-service/Authenticator.API/AuthenticatorStartup.cs
@@ -1,11 +1,11 @@
-﻿using Authenticator.Domain.Aggregates.User;
+﻿using System;
+using Authenticator.Domain.Aggregates.User;
 using Authenticator.Domain.Aggregates.User.Services;
 using Authenticator.Infrastructure.Repository;
 using Common.MongoDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Authenticator.API
@@ -14,9 +14,15 @@ namespace Authenticator.API
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddSingleton<IDatabaseProvider>(new MongoDatabaseProvider(url: "mongodb://localhost:27017/maple_fighters"));
-            // services.AddSingleton<IAccountRepository, MongoAccountRepository>();
-            services.AddSingleton<IAccountRepository, InMemoryAccountRepository>();
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            if (databaseUrl == null)
+            {
+                throw new NullReferenceException("Please set DATABASE_URL");
+            }
+
+            services.AddSingleton<IDatabaseProvider>(new MongoDatabaseProvider(databaseUrl));
+            services.AddSingleton<IAccountRepository, MongoAccountRepository>();
+            // services.AddSingleton<IAccountRepository, InMemoryAccountRepository>();
             services.AddTransient<ILoginService, LoginService>();
             services.AddTransient<IRegistrationService, RegistrationService>();
             services.AddControllers();
