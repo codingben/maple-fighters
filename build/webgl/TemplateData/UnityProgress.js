a699e7be5d8286c7610eaf83a68d7472e639e448
Ben Ukhanov <ben1zuk321@gmail.com>
2020-11-23 23:54:57 +0200

    :alien: Added build.

---
+++ build/webgl/TemplateData/UnityProgress.js
@@ -0,0 +1,17 @@
+function UnityProgress(unityInstance, progress) {
+  if (!unityInstance.Module)
+    return;
+  if (!unityInstance.progress) {    
+    unityInstance.progress = document.createElement("div");
+    unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
+    unityInstance.progress.empty = document.createElement("div");
+    unityInstance.progress.empty.className = "empty";
+    unityInstance.progress.appendChild(unityInstance.progress.empty);
+    unityInstance.progress.full = document.createElement("div");
+    unityInstance.progress.full.className = "full";
+    unityInstance.progress.appendChild(unityInstance.progress.full);
+    unityInstance.container.appendChild(unityInstance.progress);
+  }
+  unityInstance.progress.full.style.width = (100 * progress) + "%";
+  unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+}
