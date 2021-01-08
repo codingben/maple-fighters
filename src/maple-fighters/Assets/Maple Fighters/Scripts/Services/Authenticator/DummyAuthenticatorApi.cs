ab7bfe1a2910b7431b7726a2fdb34999504512ea
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-08 17:02:11 +0200

    feat: add dummy authenticator api

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/DummyAuthenticatorApi.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Scripts.Services.AuthenticatorApi
+{
+    public class DummyAuthenticatorApi : MonoBehaviour, IAuthenticatorApi
+    {
+        public static DummyAuthenticatorApi GetInstance()
+        {
+            if (instance == null)
+            {
+                var authenticatorApi =
+                    new GameObject("Dummy Authenticator Api");
+                instance =
+                    authenticatorApi.AddComponent<DummyAuthenticatorApi>();
+            }
+
+            return instance;
+        }
+
+        private static DummyAuthenticatorApi instance;
+
+        public void Authenticate(string email, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Register()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
