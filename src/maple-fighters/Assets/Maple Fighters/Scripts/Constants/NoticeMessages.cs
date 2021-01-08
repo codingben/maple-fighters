f6b40ce25438984d84ba2f42dafb73b1085c94f1
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-08 16:30:36 +0200

    refactor: remove unused messages

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/NoticeMessages.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/NoticeMessages.cs
@@ -16,10 +16,6 @@
             public const string ShortLastName = "The last name is too short.";
             public const string WrongPassword = "The password is incorrect.";
             public const string WrongEmailAddress = "The email address does not exist.";
-            public const string UnknownError = "An unknown error has occurred.";
-            public const string EmailAddressExists = "The email address is already in use.";
-            public const string NonAuthorized = "The user is not authorized to log in.";
-            public const string ConnectionFailed = "Failed to connect to authenticator server.";
             public const string RegistrationSucceed = "Registration completed successfully!";
         }
 
