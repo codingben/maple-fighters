b257da909e0dae8c13075f01951a42d68c86f010
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-09 16:06:55 +0200

    feat: add authenticator api events

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/IAuthenticatorApi.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/Authenticator/IAuthenticatorApi.cs
@@ -1,7 +1,13 @@
-﻿namespace Scripts.Services.AuthenticatorApi
+﻿using System;
+
+namespace Scripts.Services.AuthenticatorApi
 {
     public interface IAuthenticatorApi
     {
+        Action<long, string> Authentication { get; set; }
+
+        Action<long, string> Registration { get; set; }
+
         void Authenticate(string email, string password);
 
         void Register(string email, string password, string firstname, string lastname);
