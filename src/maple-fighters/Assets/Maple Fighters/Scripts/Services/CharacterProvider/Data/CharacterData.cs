60890558e80ac397c2001ff9eeda59fc40f9c1d0
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-10 15:42:45 +0200

    feat: add character data

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/CharacterProvider/Data/CharacterData.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+namespace Scripts.Services.CharacterProviderApi
+{
+    [Serializable]
+    public class CharacterData
+    {
+        public int id;
+
+        public int userid;
+
+        public string charactername;
+
+        public int index;
+
+        public int classindex;
+    }
+}
---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/CharacterProvider/Data/NewCharacterData.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Scripts.Services.CharacterProviderApi
+{
+    [Serializable]
+    public class NewCharacterData
+    {
+        public int userid;
+
+        public string charactername;
+
+        public int index;
+
+        public int classindex;
+    }
+}
