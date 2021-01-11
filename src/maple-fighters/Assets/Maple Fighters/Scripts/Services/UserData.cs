ae4f5da13e38845e50698c9336535fa27a28761a
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-11 19:13:56 +0200

    feat: add user data

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/UserData.cs
@@ -0,0 +1,19 @@
+namespace Scripts.Services
+{
+    // TODO: Don't use user data as a static object
+    public static class UserData
+    {
+        public static class CharacterData
+        {
+            public static byte Type;
+
+            public static string Name;
+        }
+
+        // TODO: JWT
+
+        public static int Id;
+
+        public static string GameServerUrl;
+    }
+}
