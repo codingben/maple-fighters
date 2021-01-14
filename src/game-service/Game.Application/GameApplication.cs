5696b61412f80334335cb3d4ac3053fc296950de
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-14 20:26:09 +0200

    refactor: game app uses a new websocket server

--- src/game-service/Game.Application/GameApplication.cs
+++ src/game-service/Game.Application/GameApplication.cs
@@ -1,18 +1,18 @@
 using Common.ComponentModel;
 using Common.Components;
+using Fleck;
 using Game.Application.Components;
-using WebSocketSharp;
-using WebSocketSharp.Server;
 
 namespace Game.Application
 {
-    public class GameApplication : WebSocketServer, IServerApplication
+    public class GameApplication : IServerApplication
     {
+        private readonly IWebSocketServer webSocketServer;
         private readonly IComponents components;
 
         public GameApplication(string url)
-            : base(url)
         {
+            webSocketServer = new WebSocketServer(url);
             components = new ComponentCollection(new IComponent[]
             {
                 new IdGenerator(),
@@ -20,32 +20,16 @@ namespace Game.Application
                 new GameSceneCollection(),
                 new GameSceneManager()
             });
-
-            Log.Level = LogLevel.Info;
         }
 
         public void Startup()
         {
-            AddWebSocketService("/game", () => new GameService(components));
-
-            Start();
-
-            if (IsListening)
-            {
-                Log.Info($"Server is running {Address:Port}");
-            }
+            webSocketServer?.Start((config) => new GameService(config, components));
         }
 
         public void Shutdown()
         {
-            RemoveComponents();
-            RemoveWebSocketService("/game");
-
-            Stop();
-        }
-
-        private void RemoveComponents()
-        {
+            webSocketServer?.Dispose();
             components?.Dispose();
         }
     }
