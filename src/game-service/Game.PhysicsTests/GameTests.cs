4d999dc82fb55a3025ac4e1ea29e30ba3612c268
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-22 00:51:57 +0200

    refactor: much simpler game physics tests

--- src/game-service/Game.PhysicsTests/GameTests.cs
+++ src/game-service/Game.PhysicsTests/GameTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading;
-using Box2D.Window;
+﻿using Box2D.Window;
 using Common.ComponentModel;
 using Common.Components;
 using Game.Application.Components;
@@ -8,78 +6,52 @@ using Game.Application.Objects;
 using Game.Application.Objects.Components;
 using static Box2DX.Dynamics.DebugDraw;
 
-namespace Game.PhysicsTests
+CreateSimulationWindow();
+
+void CreateSimulationWindow()
 {
-    public class GameTests
+    var window =
+        new SimulationWindow(title: "Physics Tests", width: 800, height: 600)
+        {
+            VSync = OpenTK.VSyncMode.On
+        };
+
+    window.SetView(new CameraView());
+
+    CreateMap(new DrawPhysics(window)
     {
-        private static void Main()
-        {
-            var windowThread = new Thread(new ThreadStart(RunWindow))
-            {
-                Priority = ThreadPriority.Lowest,
-                IsBackground = true
-            };
+        Flags = DrawFlags.Aabb | DrawFlags.Shape
+    });
 
-            windowThread.Start();
-            Console.ReadKey();
-        }
+    window.Run(updateRate: 25.0);
+}
 
-        private static void RunWindow()
-        {
-            var simulationWindow = CreateSimulationWindow();
-            var physicsDrawer = new DrawPhysics(simulationWindow)
-            {
-                Flags = DrawFlags.Aabb | DrawFlags.Shape
-            };
+void CreateMap(DrawPhysics drawPhysics)
+{
+    var gameSceneCollection = new ComponentCollection(new IComponent[]
+    {
+        new IdGenerator(),
+        new GameSceneCollection(),
+        new GameSceneManager()
+    }).Get<IGameSceneCollection>();
 
-            var gameScene = CreateMap(Map.TheDarkForest);
-            if (gameScene != null)
-            {
-                gameScene.PhysicsWorldManager.SetDebugDraw(physicsDrawer);
+    if (gameSceneCollection.TryGet(map: Map.TheDarkForest, out var gameScene))
+    {
+        gameScene.PhysicsWorldManager.SetDebugDraw(drawPhysics);
 
-                CreatePlayer(gameScene);
-            }
-
-            simulationWindow.Run(25.0);
-        }
-
-        private static SimulationWindow CreateSimulationWindow()
-        {
-            var simulationWindow = new SimulationWindow("Physics Tests", 800, 600);
-
-            simulationWindow.SetView(new CameraView());
-            simulationWindow.VSync = OpenTK.VSyncMode.On;
-
-            return simulationWindow;
-        }
-
-        private static IGameScene CreateMap(Map map)
-        {
-            var components = new ComponentCollection(new IComponent[]
-            {
-                new IdGenerator(),
-                new GameSceneCollection(),
-                new GameSceneManager()
-            });
-
-            var gameSceneCollection = components.Get<IGameSceneCollection>();
-            gameSceneCollection.TryGet(map, out var gameScene);
-
-            return gameScene;
-        }
-
-        private static void CreatePlayer(IGameScene gameScene)
-        {
-            var playerId = 1;
-            var player = new PlayerGameObject(playerId, new IComponent[]
-            {
-                new MessageSender(),
-                new PlayerAttackedMessageSender()
-            });
-            var bodyData = player.CreateBodyData();
-
-            gameScene.GameObjectCollection.Add(player);
-            gameScene.PhysicsWorldManager.AddBody(bodyData);
-        }
+        CreatePlayer(gameScene);
     }
+}
+
+void CreatePlayer(IGameScene gameScene)
+{
+    var player = new PlayerGameObject(id: 1, new IComponent[]
+    {
+        new MessageSender(),
+        new PlayerAttackedMessageSender()
+    });
+    var bodyData = player.CreateBodyData();
+
+    gameScene.GameObjectCollection.Add(player);
+    gameScene.PhysicsWorldManager.AddBody(bodyData);
 }
