da4c0a7e9fccc1f15e13062be5d7320a2047d17a
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-22 19:21:52 +0200

    refactor: remove functions

--- src/game-service/Game.PhysicsTests/GameTests.cs
+++ src/game-service/Game.PhysicsTests/GameTests.cs
@@ -6,45 +6,30 @@ using Game.Application.Objects;
 using Game.Application.Objects.Components;
 using static Box2DX.Dynamics.DebugDraw;
 
-CreateSimulationWindow();
+var window =
+    new SimulationWindow(title: "Physics Tests", width: 800, height: 600)
+    {
+        VSync = OpenTK.VSyncMode.On
+    };
 
-void CreateSimulationWindow()
+window.SetView(new CameraView());
+
+var gameSceneCollection = new ComponentCollection(new IComponent[]
 {
-    var window =
-        new SimulationWindow(title: "Physics Tests", width: 800, height: 600)
-        {
-            VSync = OpenTK.VSyncMode.On
-        };
+    new IdGenerator(),
+    new GameSceneCollection(),
+    new GameSceneManager()
+})
+.Get<IGameSceneCollection>()
+.TryGet(map: Map.TheDarkForest, out var gameScene);
 
-    window.SetView(new CameraView());
-
-    CreateMap(new DrawPhysics(window)
+if (gameScene != null)
+{
+    gameScene.PhysicsWorldManager.SetDebugDraw(new DrawPhysics(window)
     {
         Flags = DrawFlags.Aabb | DrawFlags.Shape
     });
 
-    window.Run(updateRate: 25.0);
-}
-
-void CreateMap(DrawPhysics drawPhysics)
-{
-    var gameSceneCollection = new ComponentCollection(new IComponent[]
-    {
-        new IdGenerator(),
-        new GameSceneCollection(),
-        new GameSceneManager()
-    }).Get<IGameSceneCollection>();
-
-    if (gameSceneCollection.TryGet(map: Map.TheDarkForest, out var gameScene))
-    {
-        gameScene.PhysicsWorldManager.SetDebugDraw(drawPhysics);
-
-        CreatePlayer(gameScene);
-    }
-}
-
-void CreatePlayer(IGameScene gameScene)
-{
     var player = new PlayerGameObject(id: 1, new IComponent[]
     {
         new MessageSender(),
@@ -55,3 +40,5 @@ void CreatePlayer(IGameScene gameScene)
     gameScene.GameObjectCollection.Add(player);
     gameScene.PhysicsWorldManager.AddBody(bodyData);
 }
+
+window.Run(updateRate: 25.0);
