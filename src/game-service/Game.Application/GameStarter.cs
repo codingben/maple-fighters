58a9022353bd1613effacafb52af328e2930a7ba
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-22 17:15:34 +0200

    refactor: remove Env.Load() from game starter

--- src/game-service/Game.Application/GameStarter.cs
+++ src/game-service/Game.Application/GameStarter.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using DotNetEnv;
 using Game.Application;
 
-Env.Load();
+var gameApplication = new GameApplication();
+gameApplication.Startup();
 
-var ipAddress = Environment.GetEnvironmentVariable("IP_ADDRESS");
-if (ipAddress == null)
-{
-    throw new Exception("Please set IP_ADDRESS");
-}
-
-var serverApplication = new GameApplication(ipAddress);
-serverApplication.Startup();
-
-AppDomain.CurrentDomain.ProcessExit += (x, y) => serverApplication?.Shutdown();
+AppDomain.CurrentDomain.ProcessExit += (s, e) => gameApplication?.Shutdown();
