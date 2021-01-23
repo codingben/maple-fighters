874e9dfbcf2da6c8c44c8b1c62745c67c10086da
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-23 19:31:25 +0200

    refactor: make the game application shorter

--- src/game-service/Game.Application/GameApplication.cs
+++ src/game-service/Game.Application/GameApplication.cs
@@ -3,40 +3,19 @@ using Common.ComponentModel;
 using Common.Components;
 using DotNetEnv;
 using Fleck;
+using Game.Application;
 using Game.Application.Components;
 
-namespace Game.Application
+Env.Load();
+
+var url = Environment.GetEnvironmentVariable("URL");
+var components = new ComponentCollection(new IComponent[]
 {
-    public class GameApplication
-    {
-        private readonly IWebSocketServer webSocketServer;
-        private readonly IComponents components;
+    new IdGenerator(),
+    new WebSocketSessionCollection(),
+    new GameSceneCollection(),
+    new GameSceneManager()
+});
 
-        public GameApplication()
-        {
-            Env.Load();
-
-            var url = Environment.GetEnvironmentVariable("URL");
-
-            webSocketServer = new WebSocketServer(url);
-            components = new ComponentCollection(new IComponent[]
-            {
-                new IdGenerator(),
-                new WebSocketSessionCollection(),
-                new GameSceneCollection(),
-                new GameSceneManager()
-            });
-        }
-
-        public void Startup()
-        {
-            webSocketServer?.Start((config) => new GameService(config, components));
-        }
-
-        public void Shutdown()
-        {
-            webSocketServer?.Dispose();
-            components?.Dispose();
-        }
-    }
-}
+var webSocketServer = new WebSocketServer(url);
+webSocketServer.Start((connection) => new GameService(connection, components));
