ab340b87a5137dab9ba41b4e8ccb5bce5debf172
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-25 00:45:50 +0200

    feat: add dummy chat api

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/ChatApi/DummyChatApi.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace Scripts.Services.ChatApi
+{
+    public class DummyChatApi : MonoBehaviour, IChatApi
+    {
+        public static DummyChatApi GetInstance()
+        {
+            if (instance == null)
+            {
+                var chatApi = new GameObject("Dummy Chat Api");
+                instance = chatApi.AddComponent<DummyChatApi>();
+            }
+
+            return instance;
+        }
+
+        private static DummyChatApi instance;
+
+        public Action<string> ChatMessageReceived { get; set; }
+
+        public void SendChatMessage(string message)
+        {
+            ChatMessageReceived?.Invoke("MapleStory: Hello World!");
+        }
+    }
+}
