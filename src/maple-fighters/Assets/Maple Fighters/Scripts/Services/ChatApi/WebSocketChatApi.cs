26195995e972542531d1c5f55ab2218bf0f2ab51
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-25 00:45:33 +0200

    src: rename ChatApi to WebSocketChatApi

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/ChatApi/WebSocketChatApi.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace Scripts.Services.ChatApi
+{
+    public class WebSocketChatApi : MonoBehaviour, IChatApi
+    {
+        public static WebSocketChatApi GetInstance()
+        {
+            if (instance == null)
+            {
+                var chatApi = new GameObject("WebSocket Chat Api");
+                instance = chatApi.AddComponent<WebSocketChatApi>();
+            }
+
+            return instance;
+        }
+
+        private static WebSocketChatApi instance;
+
+        public Action<string> ChatMessageReceived { get; set; }
+
+        public void SendChatMessage(string message)
+        {
+            // TODO: Implement this
+        }
+    }
+}
