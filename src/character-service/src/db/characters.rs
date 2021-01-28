64a99a3ca8d5f9eb25b2b857ce165213b0a09ed6
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-28 21:52:56 +0200

    refactor: remove unnecessary functions

--- src/character-service/src/db/characters.rs
+++ src/character-service/src/db/characters.rs
@@ -2,31 +2,22 @@ use crate::models::character::Character;
 use crate::models::new_character::NewCharacter;
 use crate::schema::characters;
 use crate::schema::characters::dsl::characters as db_characters;
+
 use diesel::{pg::PgConnection, prelude::*, r2d2, r2d2::ConnectionManager, result::Error};
 
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 pub fn create(character: NewCharacter, conn: &PgConnection) -> Result<bool, Error> {
-    if is_name_used(character.userid, &character.charactername, &conn)? {
-        return Ok(false);
-    }
-
-    let is_ok = diesel::insert_into(characters::table)
+    let is_inserted = diesel::insert_into(characters::table)
         .values(&character)
         .execute(conn)
         .is_ok();
 
-    Ok(is_ok)
+    Ok(is_inserted)
 }
 
 pub fn delete_by_id(id: i32, conn: &PgConnection) -> Result<bool, Error> {
-    if get_by_id(id, conn)?.is_empty() {
-        return Ok(false);
-    };
-
-    let is_ok = diesel::delete(db_characters.find(id)).execute(conn).is_ok();
-
-    Ok(is_ok)
+    Ok(diesel::delete(db_characters.find(id)).execute(conn).is_ok())
 }
 
 pub fn get_by_id(id: i32, conn: &PgConnection) -> Result<Vec<Character>, Error> {
@@ -37,14 +28,4 @@ pub fn get_all_by_user_id(userid: i32, conn: &PgConnection) -> Result<Vec<Charac
     db_characters
         .filter(characters::userid.eq(userid))
         .load::<Character>(conn)
-}
-
-pub fn is_name_used<'a>(userid: i32, name: &'a str, conn: &PgConnection) -> Result<bool, Error> {
-    for character in get_all_by_user_id(userid, &conn)? {
-        if character.charactername == name {
-            return Ok(true);
-        }
-    }
-
-    Ok(false)
 }
