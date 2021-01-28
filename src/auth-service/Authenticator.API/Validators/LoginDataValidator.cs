a1b95ddd3399ac8235bca3e4aa2958a3da4d8098
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-28 23:41:58 +0200

    feat: add length rules

--- src/auth-service/Authenticator.API/Validators/LoginDataValidator.cs
+++ src/auth-service/Authenticator.API/Validators/LoginDataValidator.cs
@@ -9,7 +9,9 @@ namespace Authenticator.API.Validators
         public LoginDataValidator()
         {
             RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage(ErrorMessages.InvalidEmail);
+            RuleFor(x => x.Email).Length(3, 50).WithMessage(ErrorMessages.EmailLength);
             RuleFor(x => x.Password).NotEmpty().WithMessage(ErrorMessages.PasswordRequired);
+            RuleFor(x => x.Password).Length(8, 16).WithMessage(ErrorMessages.PasswordLength);
         }
     }
 }
