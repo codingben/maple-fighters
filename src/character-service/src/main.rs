f7673e1bea08f8814c9ebeb839ab4e3566e39020
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-28 18:05:25 +0200

    refactor: main cleanup

--- src/character-service/src/main.rs
+++ src/character-service/src/main.rs
@@ -2,44 +2,39 @@
 // `extern crate` in your `main.rs` file. See
 // https://github.com/emk/rust-musl-builder/issues/69.
 extern crate openssl;
-
 #[macro_use]
 extern crate diesel;
-
 #[macro_use]
 extern crate diesel_migrations;
+extern crate dotenv;
 
 embed_migrations!("./migrations");
-
-extern crate dotenv;
 
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use diesel::{pg::PgConnection, r2d2::ConnectionManager, r2d2::Pool};
 use dotenv::dotenv;
 use embedded_migrations::run;
-use std::{env::var, io::Result};
+use std::{env, io::Result};
 
 mod database;
 mod handlers;
 mod models;
 mod schema;
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> Result<()> {
     dotenv().ok();
 
     env_logger::init();
 
-    let address = var("IP_ADDRESS").expect("IP_ADDRESS not found");
-    let database_url = var("DATABASE_URL").expect("DATABASE_URL not found");
+    let ip_address = env::var("IP_ADDRESS").expect("IP_ADDRESS not found");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not found");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = Pool::builder()
         .build(manager)
         .expect("Failed to create pool");
 
     run(&*pool.clone().get().expect("No db connection")).expect("Could not run migrations");
-
-    println!("Server is running {}", address);
 
     HttpServer::new(move || {
         App::new()
@@ -49,7 +44,7 @@ async fn main() -> Result<()> {
             .route("/characters/{id}", web::delete().to(handlers::delete_by_id))
             .route("/characters/{id}", web::get().to(handlers::get_all))
     })
-    .bind(address)?
+    .bind(ip_address)?
     .run()
     .await
 }
