b51989fd9b1d396cd3b8c99dcb7f2eaf2427141a
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-28 17:10:35 +0200

    refactor: remove unnecessary functions

--- src/gameprovider-service/src/handlers.rs
+++
@@ -1,9 +0,0 @@
-use crate::models::GameCollection;
-use actix_web::web::Data;
-use actix_web::HttpResponse;
-use std::sync::Arc;
-
-pub fn get_game_servers(data: Data<Arc<GameCollection>>) -> HttpResponse {
-    let game_collection = data.clone();
-    HttpResponse::Ok().json(&game_collection.collection)
-}
