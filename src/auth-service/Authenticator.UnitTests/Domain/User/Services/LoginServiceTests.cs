14a40e72c6a6b72083054a4b2ac443af92aecb47
Ben Ukhanov <ben1zuk321@gmail.com>
2021-02-04 01:44:08 +0200

    fix: login tests

--- src/auth-service/Authenticator.UnitTests/Domain/User/Services/LoginServiceTests.cs
+++ src/auth-service/Authenticator.UnitTests/Domain/User/Services/LoginServiceTests.cs
@@ -11,29 +11,14 @@ namespace Authenticator.UnitTests.Domain.User.Services
     public class LoginServiceTests
     {
         [Fact]
-        public void Authenticate_Returns_NotFound()
-        {
-            // Arrange
-            var accountRepository = Substitute.For<IAccountRepository>();
-            var loginService = new LoginService(accountRepository);
-
-            // Act
-            var account =
-                loginService.Authenticate("benzuk@gmail.com", "benzuk");
-
-            // Assert
-            account.ShouldBeNull();
-        }
-
-        [Fact]
-        public void Authenticate_Returns_Account()
+        public void FindAccount_Returns_Account()
         {
             // Arrange
             var dummyAccount = Account.Create(
-                "benzuk@gmail.com",
-                "benzuk",
-                "Ben",
-                "Ben Ukhanov");
+                email: "benzuk@gmail.com",
+                password: "benzuk",
+                firstName: "Ben",
+                lastName: "Ukhanov");
             var accountRepository = Substitute.For<IAccountRepository>();
             accountRepository
                 .Read(Arg.Any<Expression<Func<Account, bool>>>())
@@ -41,31 +26,21 @@ namespace Authenticator.UnitTests.Domain.User.Services
             var loginService = new LoginService(accountRepository);
 
             // Act
-            var account =
-                loginService.Authenticate("benzuk@gmail.com", "benzuk");
+            var account = loginService.FindAccount("benzuk@gmail.com");
 
             // Assert
             account.ShouldNotBeNull();
         }
 
         [Fact]
-        public void Authenticate_Returns_No_Account()
+        public void FindAccount_Returns_No_Account()
         {
             // Arrange
-            var dummyAccount = Account.Create(
-                "benzuk@gmail.com",
-                "benzuk",
-                "Ben",
-                "Ben Ukhanov");
             var accountRepository = Substitute.For<IAccountRepository>();
-            accountRepository
-                .Read(Arg.Any<Expression<Func<Account, bool>>>())
-                .Returns(dummyAccount);
             var loginService = new LoginService(accountRepository);
 
             // Act
-            var account =
-                loginService.Authenticate("benzuk@gmail.com", "ben");
+            var account = loginService.FindAccount("benzuk@gmail.com");
 
             // Assert
             account.ShouldBeNull();
