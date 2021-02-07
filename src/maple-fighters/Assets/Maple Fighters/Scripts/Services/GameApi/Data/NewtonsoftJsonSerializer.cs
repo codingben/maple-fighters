4cf8c5428533ed67fe7ce17c4deea4e849176b6e
Ben Ukhanov <ben1zuk321@gmail.com>
2021-02-08 00:39:31 +0200

    feat: add newtonsoft json serializer

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameApi/Data/NewtonsoftJsonSerializer.cs
@@ -0,0 +1,20 @@
+using Game.Network;
+using Newtonsoft.Json;
+
+namespace Scripts.Services.GameApi
+{
+    public class NewtonsoftJsonSerializer : IJsonSerializer
+    {
+        public string Serialize<T>(T data)
+            where T : class
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        public T Deserialize<T>(string json)
+            where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
