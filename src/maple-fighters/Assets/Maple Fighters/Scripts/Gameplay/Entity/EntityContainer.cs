ff23235f6cb14c9ec85601e7131b3ac5ecd87ae7
Ben Ukhanov <ben1zuk321@gmail.com>
2021-02-08 00:42:59 +0200

    refactor: set entity position

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Entity/EntityContainer.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Entity/EntityContainer.cs
@@ -51,8 +51,8 @@ namespace Scripts.Gameplay.Entity
             // TODO: Remove "LocalPlayer"
             var name = "LocalPlayer";
             var id = message.GameObjectId;
-            var x = message.SpawnData.X;
-            var y = message.SpawnData.Y;
+            var x = message.X;
+            var y = message.Y;
             var position = new Vector2(x, y);
 
             localEntity = AddEntity(id, name, position);
