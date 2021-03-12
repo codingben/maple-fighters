d19c438f7f3b77237dcaffbe5ec9c9ad6bd4eb77
Ben Ukhanov <ben1zuk321@gmail.com>
2021-03-12 12:38:04 +0200

    docs: add summaries for interest area

--- lib/interest-management/IInterestArea.cs
+++ lib/interest-management/IInterestArea.cs
@@ -6,12 +6,28 @@ namespace InterestManagement
     public interface IInterestArea<TSceneObject> : IDisposable
         where TSceneObject : ISceneObject
     {
+        /// <summary>
+        /// Sets the source of regions.
+        /// </summary>
+        /// <param name="matrixRegion">The matrix region.</param>
         void SetMatrixRegion(IMatrixRegion<TSceneObject> matrixRegion);
 
+        /// <summary>
+        /// Gets all nearby regions.
+        /// </summary>
+        /// <returns>The regions.</returns>
         IEnumerable<IRegion<TSceneObject>> GetRegions();
 
+        /// <summary>
+        /// Gets all nearby objects in the regions.
+        /// </summary>
+        /// <returns>The scene objects.</returns>
         IEnumerable<TSceneObject> GetNearbySceneObjects();
 
+        /// <summary>
+        /// Gets the notifier of events for nearby scene objects.
+        /// </summary>
+        /// <returns>The event notifier.</returns>
         INearbySceneObjectsEvents<TSceneObject> GetNearbySceneObjectsEvents();
     }
 }
