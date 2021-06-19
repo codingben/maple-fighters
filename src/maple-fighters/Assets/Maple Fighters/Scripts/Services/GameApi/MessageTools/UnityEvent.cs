eff185cae165ecf3efc7d4a65662cb5377732d66
Ben Ukhanov <ben1zuk321@gmail.com>
2021-06-19 19:13:02 +0300

    feat(maple-fighters): add unity event

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/GameApi/MessageTools/UnityEvent.cs
@@ -0,0 +1,10 @@
+namespace Game.MessageTools
+{
+    public class UnityEvent<T> : UnityEngine.Events.UnityEvent<T>
+    {
+        internal void AddListener()
+        {
+            // Left blank intentionally
+        }
+    }
+}
