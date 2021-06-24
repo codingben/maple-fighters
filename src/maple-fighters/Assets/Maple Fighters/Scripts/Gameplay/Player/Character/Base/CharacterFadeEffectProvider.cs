f7571935bb7325c03bebcbc78afba24d08c5cdf3
Ben Ukhanov <ben1zuk321@gmail.com>
2021-06-24 18:53:29 +0300

    feat(maple-fighters): character fade effect provider

---
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Character/Base/CharacterFadeEffectProvider.cs
@@ -0,0 +1,47 @@
+using Scripts.Gameplay.Graphics;
+using UnityEngine;
+
+namespace Scripts.Gameplay.Player
+{
+    [RequireComponent(typeof(SpawnCharacter))]
+    public class CharacterFadeEffectProvider : MonoBehaviour, IFadeEffectProvider
+    {
+        private FadeEffect fadeEffect;
+        private ISpawnedCharacter spawnedCharacter;
+
+        private void Awake()
+        {
+            spawnedCharacter = GetComponent<ISpawnedCharacter>();
+        }
+
+        private void Start()
+        {
+            spawnedCharacter.CharacterSpawned += OnCharacterSpawned;
+        }
+
+        private void OnDestroy()
+        {
+            spawnedCharacter.CharacterSpawned -= OnCharacterSpawned;
+        }
+
+        private void OnCharacterSpawned()
+        {
+            var characterSprite = spawnedCharacter.GetCharacterSprite();
+            if (characterSprite != null)
+            {
+                fadeEffect = characterSprite.GetComponent<FadeEffect>();
+                fadeEffect?.SetOwner(gameObject);
+            }
+        }
+
+        public FadeEffect Provide()
+        {
+            if (fadeEffect == null)
+            {
+                Debug.LogWarning("Character fade effect provider is null.");
+            }
+
+            return fadeEffect;
+        }
+    }
+}
