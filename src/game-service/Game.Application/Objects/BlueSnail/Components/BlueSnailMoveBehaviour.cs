d47715b63072783494b92b7ec339d9135a6bd57f
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-04 23:01:56 +0300

    refactor(game-service): remove set body position

--- src/game-service/Game.Application/Objects/BlueSnail/Components/BlueSnailMoveBehaviour.cs
+++ src/game-service/Game.Application/Objects/BlueSnail/Components/BlueSnailMoveBehaviour.cs
@@ -3,7 +3,6 @@ using System.Timers;
 using Common.ComponentModel;
 using Common.MathematicsHelper;
 using Coroutines;
-using Game.Physics;
 using Game.Messages;
 
 namespace Game.Application.Objects.Components
@@ -11,21 +10,17 @@ namespace Game.Application.Objects.Components
     public class BlueSnailMoveBehaviour : ComponentBase
     {
         private readonly ICoroutineRunner coroutineRunner;
-        private readonly IPhysicsWorldManager physicsWorldManager;
+        private readonly Timer positionSenderTimer;
+        private readonly Timer directionSetterTimer;
 
         private IProximityChecker proximityChecker;
         private IGameObject blueSnail;
 
-        private BodyData? bodyData;
         private float direction = 0.1f;
 
-        private readonly Timer positionSenderTimer;
-        private readonly Timer directionSetterTimer;
-
-        public BlueSnailMoveBehaviour(ICoroutineRunner coroutineRunner, IPhysicsWorldManager physicsWorldManager)
+        public BlueSnailMoveBehaviour(ICoroutineRunner coroutineRunner)
         {
             this.coroutineRunner = coroutineRunner;
-            this.physicsWorldManager = physicsWorldManager;
 
             positionSenderTimer = new Timer(100);
             directionSetterTimer = new Timer(3500);
@@ -59,21 +54,9 @@ namespace Game.Application.Objects.Components
             var position = blueSnail.Transform.Position;
             var speed = 0.75f;
 
-            if (physicsWorldManager.GetBody(id, out var value))
-            {
-                bodyData = value;
-            }
-
             while (true)
             {
                 position += new Vector2(direction, 0) * speed;
-
-                var body = bodyData?.Body;
-                if (body != null)
-                {
-                    body.SetXForm(position.FromVector2(), body.GetAngle());
-                }
-
                 blueSnail.Transform.SetPosition(position);
                 yield return null;
             }
