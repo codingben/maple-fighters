a85619d13a6d910ec938e033b7061dc54e12cc26
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-10 13:03:57 +0300

    refactor(maple-fighters): player attacked behaviour

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Controller/Behaviours/PlayerAttackedBehaviour.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Controller/Behaviours/PlayerAttackedBehaviour.cs
@@ -15,12 +15,8 @@ namespace Scripts.Gameplay.Player.Behaviours
 
         public void OnPlayerAttacked(Vector3 direction)
         {
-            if (playerController.GetPlayerState() != PlayerStates.Attacked)
-            {
-                playerController.SetPlayerState(PlayerStates.Attacked);
-
-                StartCoroutine(WaitFrameAndBounce(direction));
-            }
+            // NOTE: Waiting one frame prevent an exception in the game server.
+            StartCoroutine(WaitFrameAndBounce(direction));
         }
 
         private IEnumerator WaitFrameAndBounce(Vector3 direction)
