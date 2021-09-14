983224d5ddfc40926c8410aeaf53181bfa047383
Ben Ukhanov <ben1zuk321@gmail.com>
2021-09-14 21:50:51 +0300

    feat(chat-service): add message listener

--- src/chat-service/src/server.js
+++ src/chat-service/src/server.js
@@ -2,8 +2,24 @@ const WebSocket = require("ws");
 
 require("dotenv").config();
 
-const server = new WebSocket.Server({ port: process.env.PORT });
+var server = new WebSocket.Server({ port: process.env.PORT });
+server.on("connection", onConnection);
+server.on("close", onClose);
 
-server.on("connection", function connection(_) {
+function onConnection(webSocket) {
+  webSocket.on("message", (data, isBinary) => {
+    server.clients.forEach((client) => {
+      if (client === webSocket || client.readyState !== WebSocket.OPEN) {
+        return;
+      }
+
+      client.send(data, { binary: isBinary });
+    });
+  });
+
   console.log("A new client connected.");
-});
+}
+
+function onClose() {
+  console.log("The client has been disconnected.");
+}
