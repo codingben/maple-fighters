41480f07c3e914845e2a30ec3b44ca78317ce178
Ben Ukhanov <ben1zuk321@gmail.com>
2021-09-22 12:35:09 +0300

    fix(maple-fighters): delay after player attacked

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Controller/Behaviours/PlayerAttackedBehaviour.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Player/Controller/Behaviours/PlayerAttackedBehaviour.cs
@@ -6,7 +6,11 @@ namespace Scripts.Gameplay.Player.Behaviours
     [RequireComponent(typeof(PlayerController))]
     public class PlayerAttackedBehaviour : MonoBehaviour, IAttackPlayer
     {
+        [SerializeField]
+        private float attackDelay = 2;
+
         private PlayerController playerController;
+        private bool isAttacked;
 
         private void Awake()
         {
@@ -15,15 +19,31 @@ namespace Scripts.Gameplay.Player.Behaviours
 
         public void OnPlayerAttacked(Vector3 direction)
         {
+            if (isAttacked)
+            {
+                return;
+            }
+
             // NOTE: Waiting one frame prevent an exception in the game server.
             StartCoroutine(WaitFrameAndBounce(direction));
         }
 
         private IEnumerator WaitFrameAndBounce(Vector3 direction)
         {
-            yield return null;
+            yield return new WaitForSeconds(0.1f);
 
             playerController.Bounce(direction);
+
+            StartCoroutine(WaitAfterAttacked());
+        }
+
+        private IEnumerator WaitAfterAttacked()
+        {
+            isAttacked = true;
+
+            yield return new WaitForSeconds(attackDelay);
+
+            isAttacked = false;
         }
     }
 }
