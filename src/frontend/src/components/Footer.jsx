f35bd5ad16e1f691edf54b555e866217bd078313
Ben Ukhanov <ben1zuk321@gmail.com>
2021-10-01 22:30:29 +0300

    feat(frontend): add footer

---
+++ src/frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h4>
+        Made with ❤️ by{" "}
+        <a href="https://ukben.dev" target="_blank">
+          Ben Ukhanov
+        </a>
+      </h4>
+    </div>
+  );
+}
+
+export default Footer;
