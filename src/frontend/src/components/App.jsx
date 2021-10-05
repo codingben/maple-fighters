3b0d47a32faadfc4d4c8d15a5cb126f52df078e2
Ben Ukhanov <ben1zuk321@gmail.com>
2021-10-05 23:43:39 +0300

    feat(frontend): improve device message view

--- src/frontend/src/components/App.jsx
+++ src/frontend/src/components/App.jsx
@@ -3,6 +3,7 @@ import { isBrowser } from "react-device-detect";
 import Header from "./Header";
 import Footer from "./Footer";
 import Loader from "./Loader";
+import logo from "../assets/logo.png";
 import "./app.css";
 
 export const context = new UnityContext({
@@ -15,8 +16,14 @@ export const context = new UnityContext({
 function App() {
   if (!isBrowser) {
     return (
-      <div className="center">
-        <h1>This game is not compatible with your device.</h1>
+      <div className="warning">
+        <img src={logo} alt="logo" />
+        <h1>
+          <a href="https://github.com/benukhanov/maple-fighters">
+            Maple Fighters
+          </a>{" "}
+          is not compatible with your device.
+        </h1>
       </div>
     );
   }
