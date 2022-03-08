57882b44a3abd3292f7a75838c4ec565d424bfa1
Ben Ukhanov <ben1zuk321@gmail.com>
2022-03-08 22:42:53 +0200

    fix(maple-fighters): set entity direction on creation

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Entity/EntityContainer.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Gameplay/Entity/EntityContainer.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using Game.Messages;
 using Scripts.Gameplay.Graphics;
+using Scripts.Gameplay.Player;
 using Scripts.Services;
 using Scripts.Services.GameApi;
 using UnityEngine;
@@ -48,7 +50,6 @@ namespace Scripts.Gameplay.Entity
 
         private void OnSceneEntered(EnteredSceneMessage message)
         {
-            // TODO: Remove "LocalPlayer"
             var name = "LocalPlayer";
             var id = message.GameObjectId;
             var x = message.X;
@@ -56,9 +57,7 @@ namespace Scripts.Gameplay.Entity
             var position = new Vector2(x, y);
             var direction = message.Direction;
 
-            // TODO: Set direction
-
-            localEntity = AddEntity(id, name, position);
+            localEntity = AddEntity(id, name, position, direction);
         }
 
         private void OnGameObjectsAdded(GameObjectsAddedMessage message)
@@ -70,8 +69,7 @@ namespace Scripts.Gameplay.Entity
                 var id = gameObject.Id;
                 var name = gameObject.Name;
                 var position = new Vector2(gameObject.X, gameObject.Y);
-
-                // TODO: Set direction
+                var direction = gameObject.Direction;
 
                 if (collection.ContainsKey(id))
                 {
@@ -79,7 +77,7 @@ namespace Scripts.Gameplay.Entity
                 }
                 else
                 {
-                    AddEntity(id, name, position);
+                    AddEntity(id, name, position, direction);
                 }
             }
         }
@@ -97,7 +95,11 @@ namespace Scripts.Gameplay.Entity
             }
         }
 
-        private IEntity AddEntity(int id, string name, Vector2 position)
+        private IEntity AddEntity(
+            int id,
+            string name,
+            Vector2 position,
+            float direction)
         {
             IEntity entity = null;
 
@@ -113,6 +115,8 @@ namespace Scripts.Gameplay.Entity
                     collection.Add(id, entity);
 
                     Debug.Log($"Added a new entity with id #{id}");
+
+                    StartCoroutine(SetEntityDirection(gameObject, direction));
                 }
             }
 
@@ -155,6 +159,38 @@ namespace Scripts.Gameplay.Entity
         public bool GetRemoteEntity(int id, out IEntity entity)
         {
             return collection.TryGetValue(id, out entity);
+        }
+
+        private IEnumerator SetEntityDirection(GameObject entity, float direction)
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            if (entity == null || direction == 0)
+            {
+                yield break;
+            }
+
+            if (entity.name == "LocalPlayer" || entity.name == "RemotePlayer")
+            {
+                var spawnedCharacter = entity.GetComponent<ISpawnedCharacter>();
+                var character = spawnedCharacter?.GetCharacter();
+                if (character != null)
+                {
+                    var x = direction;
+                    var y = character.transform.localScale.y;
+                    var z = character.transform.localScale.z;
+
+                    character.transform.localScale = new Vector3(x, y, z);
+                }
+            }
+            else
+            {
+                var x = direction;
+                var y = entity.transform.localScale.y;
+                var z = entity.transform.localScale.z;
+
+                entity.transform.localScale = new Vector3(x, y, z);
+            }
         }
     }
 }
