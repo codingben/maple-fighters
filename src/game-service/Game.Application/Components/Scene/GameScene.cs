7dd6fe31776a16bfa0cce087639ee58dc10dcf8f
Ben Ukhanov <ben1zuk321@gmail.com>
2022-04-19 00:04:22 +0300

    refactor(game-service): add scene components

--- src/game-service/Game.Application/Components/Scene/GameScene.cs
+++ src/game-service/Game.Application/Components/Scene/GameScene.cs
@@ -1,56 +1,22 @@
-using Game.Application.Objects;
 using InterestManagement;
-using Game.Physics;
 
 namespace Game.Application.Components
 {
     public class GameScene : IGameScene
     {
-        public IMatrixRegion<IGameObject> MatrixRegion { get; }
-
-        public IGameObjectCollection GameObjectCollection { get; }
-
-        public IPlayerSpawnData PlayerSpawnData { get; }
-
-        public IPhysicsExecutor PhysicsExecutor { get; }
-
-        public IPhysicsWorldManager PhysicsWorldManager { get; }
+        public IComponents Components { get; }
 
         public GameScene(Vector2 sceneSize, Vector2 regionSize)
         {
-            var log = InterestManagementLogger.GetLogger();
-
-            MatrixRegion = new MatrixRegion<IGameObject>(sceneSize, regionSize, log);
-            GameObjectCollection = new GameObjectCollection();
-            PlayerSpawnData = new PlayerSpawnData();
-
-            var lowerBound = new Vector2(-100, -100);
-            var upperBound = new Vector2(100, 100);
-            var gravity = new Vector2(0, 0 /* -9.81f */);
-
-            PhysicsWorldManager = new PhysicsWorldManager(lowerBound, upperBound, gravity);
-            PhysicsExecutor = new PhysicsExecutor(OnUpdateBodies, OnSimulatePhysics);
+            Components.Add(new SceneRegionCreator(sceneSize, regionSize));
+            Components.Add(new SceneObjectCollection());
+            Components.Add(new ScenePlayerSpawnData());
+            Components.Add(new ScenePhysicsCreator());
         }
 
         public void Dispose()
         {
-            MatrixRegion?.Dispose();
-            PhysicsExecutor?.Dispose();
-            PhysicsWorldManager?.Dispose();
-        }
-
-        private void OnUpdateBodies()
-        {
-            PhysicsWorldManager?.UpdateBodies();
-        }
-
-        private void OnSimulatePhysics()
-        {
-            var timeStep = PhysicsSettings.TimeStep;
-            var velocityIterations = PhysicsSettings.VelocityIterations;
-            var positionIterations = PhysicsSettings.PositionIterations;
-
-            PhysicsWorldManager?.Step(timeStep, velocityIterations, positionIterations);
+            Components?.Dispose();
         }
     }
 }
--- src/game-service/Game.Application/Components/Scene/IGameScene.cs
+++ src/game-service/Game.Application/Components/Scene/IGameScene.cs
@@ -1,20 +1,9 @@
 using System;
-using Game.Application.Objects;
-using InterestManagement;
-using Game.Physics;
 
 namespace Game.Application.Components
 {
     public interface IGameScene : IDisposable
     {
-        IMatrixRegion<IGameObject> MatrixRegion { get; }
-
-        IGameObjectCollection GameObjectCollection { get; }
-
-        IPlayerSpawnData PlayerSpawnData { get; }
-
-        IPhysicsExecutor PhysicsExecutor { get; }
-
-        IPhysicsWorldManager PhysicsWorldManager { get; }
+        IComponents Components { get; }
     }
 }
