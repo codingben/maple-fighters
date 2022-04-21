6a0dbd92eacd2f5967b0fb676df711a16c15a21f
Ben Ukhanov <ben1zuk321@gmail.com>
2022-04-21 20:30:02 +0300

    refactor(game-service): add data to player config

--- src/game-service/Game.Application/Objects/Player/Components/PlayerConfigData.cs
+++ src/game-service/Game.Application/Objects/Player/Components/PlayerConfigData.cs
@@ -7,5 +7,11 @@ namespace Game.Application.Objects.Components
         public float BodyHeight { get; set; }
 
         public float BodyDensity { get; set; }
+
+        public string CharacterName { get; set; }
+
+        public byte CharacterType { get; set; }
+
+        public float CharacterSpawnDirection { get; set; }
     }
 }
