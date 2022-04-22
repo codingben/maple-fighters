810651a24b6a1ed7511736e8c1bb0dee381076a9
Ben Ukhanov <ben1zuk321@gmail.com>
2022-04-22 22:32:38 +0300

    style(game-service): organize local variables

--- src/game-service/Game.Application/Objects/Player/Components/PlayerPhysicsBodyCreator.cs
+++ src/game-service/Game.Application/Objects/Player/Components/PlayerPhysicsBodyCreator.cs
@@ -27,19 +27,19 @@ namespace Game.Application.Objects.Components
         private NewBodyData CreateBodyData()
         {
             var id = gameObject.Id;
-            var bodyDef = new BodyDef();
             var x = gameObject.Transform.Position.X;
             var y = gameObject.Transform.Position.Y;
-            bodyDef.Position.Set(x, y);
-            bodyDef.UserData = gameObject;
-
-            var playerConfigDataProvider =
-                Components.Get<IPlayerConfigDataProvider>();
+            var playerConfigDataProvider = Components.Get<IPlayerConfigDataProvider>();
             var playerConfigData = playerConfigDataProvider.Provide();
             var bodyWidth = playerConfigData.BodyWidth;
             var bodyHeight = playerConfigData.BodyHeight;
             var bodyDensity = playerConfigData.BodyDensity;
+            var bodyDef = new BodyDef();
             var polygonDef = new PolygonDef();
+
+            bodyDef.Position.Set(x, y);
+            bodyDef.UserData = gameObject;
+
             polygonDef.SetAsBox(bodyWidth, bodyHeight);
             polygonDef.Density = bodyDensity;
             polygonDef.Filter = new FilterData()
