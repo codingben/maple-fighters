f69eeaf878be4beeebe2a602cdc10dd03af03066
Ben Ukhanov <ben1zuk321@gmail.com>
2022-04-27 00:07:40 +0300

    refactor(game-service): add game client components

--- src/game-service/Game.Application/GameClient.cs
+++ src/game-service/Game.Application/GameClient.cs
@@ -1,142 +1,32 @@
 using System;
 using Game.Application.Components;
-using Game.Application.Handlers;
-using Game.Messages;
-using Game.MessageTools;
 using Fleck;
-using Game.Application.Objects;
-using Game.Application.Objects.Components;
 
 namespace Game.Application
 {
-    public class GameClient
+    public class GameClient : IDisposable
     {
-        private readonly int id;
-        private readonly IWebSocketConnection connection;
-        private readonly IWebSocketSessionCollection sessionCollection;
-        private readonly IGameSceneCollection gameSceneCollection;
-        private readonly IMessageHandlerCollection handlerCollection;
-        private readonly IJsonSerializer jsonSerializer;
-        private readonly IGameObject player;
+        private IComponents Components { get; }
 
         public GameClient(IWebSocketConnection connection, IComponents serverComponents)
         {
-            this.connection = connection;
+            var id = serverComponents.Get<IIdGenerator>().GenerateId();
+            var sessionCollection = serverComponents.Get<IWebSocketSessionCollection>();
+            var gameSceneCollection = serverComponents.Get<IGameSceneCollection>();
 
-            connection.OnOpen += OnConnectionEstablished;
-            connection.OnClose += OnConnectionClosed;
-            connection.OnError += OnErrorOccurred;
-            connection.OnMessage += OnMessageReceived;
-
-            id = serverComponents.Get<IIdGenerator>().GenerateId();
-            sessionCollection = serverComponents.Get<IWebSocketSessionCollection>();
-            gameSceneCollection = serverComponents.Get<IGameSceneCollection>();
-
-            jsonSerializer = new NativeJsonSerializer();
-            handlerCollection = new MessageHandlerCollection(jsonSerializer);
-
-            player = CreatePlayerGameObject();
-
-            var messageSender = player.Components.Get<IMessageSender>();
-            messageSender.SetJsonSerializer(jsonSerializer);
+            Components = new ComponentCollection();
+            Components.Add(new WebSocketConnectionProvider(id, connection, sessionCollection));
+            Components.Add(new WebSocketConnectionErrorHandler());
+            Components.Add(new RemotePlayerProvider());
+            Components.Add(new RemotePlayerMessageSender(sessionCollection));
+            Components.Add(new MessageHandlerManager(gameSceneCollection));
 
             Console.WriteLine($"A new client #{id} is connected.");
         }
 
-        private void OnConnectionEstablished()
+        public void Dispose()
         {
-            // General
-            SubscribeToMessageSender();
-            AddWebSocketSessionData();
-
-            // Handlers
-            handlerCollection.Set(MessageCodes.ChangePosition, new ChangePositionMessageHandler(player));
-            handlerCollection.Set(MessageCodes.ChangeAnimationState, new ChangeAnimationStateHandler(player));
-            handlerCollection.Set(MessageCodes.EnterScene, new EnterSceneMessageHandler(player, gameSceneCollection));
-            handlerCollection.Set(MessageCodes.ChangeScene, new ChangeSceneMessageHandler(player, gameSceneCollection));
-        }
-
-        private void OnConnectionClosed()
-        {
-            // General
-            UnsubscribeFromMessageSender();
-            RemovePlayer();
-            RemoveWebSocketSessionData();
-
-            // Handlers
-            handlerCollection.Unset(MessageCodes.ChangePosition);
-            handlerCollection.Unset(MessageCodes.ChangeAnimationState);
-            handlerCollection.Unset(MessageCodes.EnterScene);
-            handlerCollection.Unset(MessageCodes.ChangeScene);
-        }
-
-        private void OnErrorOccurred(Exception exception)
-        {
-            Console.WriteLine($"OnErrorOccurred() -> {exception.Message}");
-        }
-
-        private void OnMessageReceived(string json)
-        {
-            var messageData = jsonSerializer.Deserialize<MessageData>(json);
-            var code = messageData.Code;
-            var data = messageData.Data;
-
-            if (handlerCollection.TryGet(code, out var handler))
-            {
-                handler?.Invoke(data);
-            }
-        }
-
-        private void SubscribeToMessageSender()
-        {
-            var messageSender = player.Components.Get<IMessageSender>();
-            messageSender.SendMessageCallback += SendMessageCallback;
-            messageSender.SendToMessageCallback += SendMessageCallback;
-        }
-
-        private void UnsubscribeFromMessageSender()
-        {
-            var messageSender = player.Components.Get<IMessageSender>();
-            messageSender.SendMessageCallback -= SendMessageCallback;
-            messageSender.SendToMessageCallback -= SendMessageCallback;
-        }
-
-        private void SendMessageCallback(string data)
-        {
-            connection.Send(data);
-        }
-
-        private void SendMessageCallback(string data, int id)
-        {
-            if (sessionCollection.TryGet(id, out var sessionData))
-            {
-                sessionData.Connection.Send(data);
-            }
-        }
-
-        private void RemovePlayer()
-        {
-            var map = player.Components.Get<IPresenceMapProvider>().GetMap();
-            var sceneObjectCollection =
-                map?.Components.Get<ISceneObjectCollection>();
-            sceneObjectCollection?.Remove(id);
-
-            player.Dispose();
-        }
-
-        private void AddWebSocketSessionData()
-        {
-            sessionCollection.Add(id, new WebSocketSessionData(id, connection));
-        }
-
-        private void RemoveWebSocketSessionData()
-        {
-            sessionCollection.Remove(id);
-        }
-
-        private IGameObject CreatePlayerGameObject()
-        {
-            return new PlayerGameObject(id, name: "RemotePlayer");
+            Components?.Dispose();
         }
     }
 }
