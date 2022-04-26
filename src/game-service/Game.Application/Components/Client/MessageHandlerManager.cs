3133fb19b38237e55fc4123e26d33480850c3f9e
Ben Ukhanov <ben1zuk321@gmail.com>
2022-04-27 00:05:16 +0300

    refactor(game-service): add message handler manager

---
+++ src/game-service/Game.Application/Components/Client/MessageHandlerManager.cs
@@ -0,0 +1,55 @@
+using Game.Application.Handlers;
+using Game.Messages;
+using Game.MessageTools;
+
+namespace Game.Application.Components
+{
+    public class MessageHandlerManager : ComponentBase
+    {
+        private readonly IJsonSerializer jsonSerializer;
+        private readonly IMessageHandlerCollection handlerCollection;
+        private readonly IGameSceneCollection gameSceneCollection;
+
+        public MessageHandlerManager(IGameSceneCollection gameSceneCollection)
+        {
+            this.gameSceneCollection = gameSceneCollection;
+
+            jsonSerializer = new NativeJsonSerializer();
+            handlerCollection = new MessageHandlerCollection(jsonSerializer);
+        }
+
+        protected override void OnAwake()
+        {
+            var webSocketConnectionProvider =
+                Components.Get<IWebSocketConnectionProvider>();
+            var remotePlayerProvider =
+                Components.Get<IRemotePlayerProvider>();
+            var webSocketConnection =
+                webSocketConnectionProvider.ProvideConnection();
+            var remotePlayer =
+                remotePlayerProvider.Provide();
+
+            webSocketConnection.OnMessage += (json) =>
+            {
+                var messageData = jsonSerializer.Deserialize<MessageData>(json);
+                var code = messageData.Code;
+                var data = messageData.Data;
+
+                if (handlerCollection.TryGet(code, out var handler))
+                {
+                    handler?.Invoke(data);
+                }
+            };
+
+            handlerCollection.Set(MessageCodes.ChangePosition, new ChangePositionMessageHandler(remotePlayer));
+            handlerCollection.Set(MessageCodes.ChangeAnimationState, new ChangeAnimationStateHandler(remotePlayer));
+            handlerCollection.Set(MessageCodes.EnterScene, new EnterSceneMessageHandler(remotePlayer, gameSceneCollection));
+            handlerCollection.Set(MessageCodes.ChangeScene, new ChangeSceneMessageHandler(remotePlayer, gameSceneCollection));
+        }
+
+        protected override void OnRemoved()
+        {
+            handlerCollection?.Dispose();
+        }
+    }
+}
