be4155b7e2b0508f161e5f36585012f4b46a2631
Ben Ukhanov <ben1zuk321@gmail.com>
2022-05-08 23:59:04 +0300

    feat(game-service): initialize serilog logger

--- src/game-service/Game.Application/GameApplication.cs
+++ src/game-service/Game.Application/GameApplication.cs
@@ -2,8 +2,22 @@ using System;
 using DotNetEnv;
 using Fleck;
 using Game.Application.Components;
+using Serilog;
 
 Env.Load();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+FleckLog.LogAction = (level, message, exception) =>
+{
+    if (level >= LogLevel.Info)
+    {
+        if (exception == null) Log.Logger.Information("{0} [{1}] {2}", DateTime.Now, level, message);
+        else Log.Logger.Information("{0} [{1}] {2} {3}", DateTime.Now, level, message, exception);
+    }
+};
 
 var url = Environment.GetEnvironmentVariable("URL");
 var server = new WebSocketServer(url);
