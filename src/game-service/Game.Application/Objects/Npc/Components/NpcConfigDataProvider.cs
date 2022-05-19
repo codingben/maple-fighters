f7a37a2a26fed29a37332d1251601225ae28ca8b
Ben Ukhanov <ben1zuk321@gmail.com>
2022-05-19 16:34:44 +0300

    feat(game-service): load npc yaml config

--- src/game-service/Game.Application/Objects/Npc/Components/NpcConfigDataProvider.cs
+++ src/game-service/Game.Application/Objects/Npc/Components/NpcConfigDataProvider.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Game.Application.Components;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
@@ -6,21 +7,20 @@ namespace Game.Application.Objects.Components
 {
     public class NpcConfigDataProvider : ComponentBase, INpcConfigDataProvider
     {
-        private NpcConfigData npcConfigData;
+        private NpcConfigData configData;
 
         protected override void OnAwake()
         {
-            var config = GetConfig();
-
-            npcConfigData = ParseNpcConfigData(config);
+            var yamlConfig = LoadYamlConfig();
+            configData = ParseConfigData(yamlConfig);
         }
 
         public NpcConfigData Provide()
         {
-            return npcConfigData;
+            return configData;
         }
 
-        private NpcConfigData ParseNpcConfigData(string data)
+        private NpcConfigData ParseConfigData(string data)
         {
             var deserializer = new DeserializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
@@ -29,12 +29,18 @@ namespace Game.Application.Objects.Components
             return deserializer.Deserialize<NpcConfigData>(data);
         }
 
-        private string GetConfig()
+        private string LoadYamlConfig()
         {
-            return @"
-message: 'Hello World!'
-messageTime: 5
-            ";
+            var config = string.Empty;
+            var url = "https://raw.githubusercontent.com/benukhanov/maple-fighters-configs/main/{0}";
+            var yamlPath = string.Format(url, "npc.yml");
+
+            using (var client = new WebClient())
+            {
+                config = client.DownloadString(yamlPath);
+            }
+
+            return config;
         }
     }
 }
