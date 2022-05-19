d1875edcab15cf9c5881eb5884e487575f902f77
Ben Ukhanov <ben1zuk321@gmail.com>
2022-05-19 16:38:09 +0300

    feat(game-service): load physics yaml config

--- src/game-service/Game.Application/Components/Scene/Components/ScenePhysicsCreator.cs
+++ src/game-service/Game.Application/Components/Scene/Components/ScenePhysicsCreator.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using Game.Physics;
 using InterestManagement;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace Game.Application.Components
 {
@@ -10,9 +13,11 @@ namespace Game.Application.Components
 
         protected override void OnAwake()
         {
-            var lowerBound = new Vector2(-100, -100);
-            var upperBound = new Vector2(100, 100);
-            var gravity = new Vector2(0, 0 /* -9.81f */);
+            var yamlConfig = LoadYamlConfig();
+            var configData = ParseConfigData(yamlConfig);
+            var upperBound = new Vector2(configData.UpperBound.X, configData.UpperBound.Y);
+            var lowerBound = new Vector2(configData.LowerBound.X, configData.LowerBound.Y);
+            var gravity = new Vector2(configData.Gravity.X, configData.Gravity.Y);
 
             physicsWorldManager = new PhysicsWorldManager(lowerBound, upperBound, gravity);
             physicsExecutor = new PhysicsExecutor(OnUpdateBodies, OnSimulatePhysics);
@@ -46,6 +51,29 @@ namespace Game.Application.Components
         public IPhysicsExecutor GetPhysicsExecutor()
         {
             return physicsExecutor;
+        }
+
+        private PhysicsData ParseConfigData(string data)
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            return deserializer.Deserialize<PhysicsData>(data);
+        }
+
+        private string LoadYamlConfig()
+        {
+            var config = string.Empty;
+            var url = "https://raw.githubusercontent.com/benukhanov/maple-fighters-configs/main/{0}";
+            var yamlPath = string.Format(url, "physics.yml");
+
+            using (var client = new WebClient())
+            {
+                config = client.DownloadString(yamlPath);
+            }
+
+            return config;
         }
     }
 }
---
+++ src/game-service/Game.Application/Components/Scene/Data/PhysicsData.cs
@@ -0,0 +1,11 @@
+namespace Game.Application.Components
+{
+    public class PhysicsData
+    {
+        public Vector2Data UpperBound { get; set; }
+
+        public Vector2Data LowerBound { get; set; }
+
+        public Vector2Data Gravity { get; set; }
+    }
+}
