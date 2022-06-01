82a61d18c84f9443e84a135f72009701ada23e9a
Ben Oukhanov <ben1zuk321@gmail.com>
2022-06-01 23:39:58 +0300

    fix(frontend): change website url

--- src/frontend/src/components/Footer/Footer.jsx
+++ src/frontend/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="footer">
       <h4>
         Made with <span style={{ color: "#E91E63" }}>&#x2764;</span> by{" "}
-        <a href="https://hello@codingben.io" target="_blank">
+        <a href="https://codingben.io" target="_blank">
           Ben Oukhanov
         </a>
       </h4>
