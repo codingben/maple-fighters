8bf81681bf470f6242895d61638c5b9a2c202a6e
Ben Oukhanov <ben1zuk321@gmail.com>
2022-06-16 10:21:40 +0300

    fix(maple-fighters): change scene indexes

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/SceneIndexes.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Constants/SceneIndexes.cs
@@ -2,7 +2,7 @@ namespace Scripts.Constants
 {
     public static class SceneIndexes
     {
-        public const int Lobby = 3;
-        public const int TheDarkForest = 4;
+        public const int Lobby = 1;
+        public const int TheDarkForest = 2;
     }
 }
