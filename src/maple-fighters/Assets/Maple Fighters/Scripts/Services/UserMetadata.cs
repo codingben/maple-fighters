af6bf850be434b2a91994555ba6efce78ac379eb
Ben Oukhanov <ben1zuk321@gmail.com>
2022-06-27 22:32:32 +0300

    feat(maple-fighters): set user id on initialization

--- src/maple-fighters/Assets/Maple Fighters/Scripts/Services/UserMetadata.cs
+++ src/maple-fighters/Assets/Maple Fighters/Scripts/Services/UserMetadata.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using Scripts.Services.AuthenticatorApi;
+using System.Text.RegularExpressions;
+using System;
 
 namespace Scripts.Services
 {
@@ -8,14 +10,43 @@ namespace Scripts.Services
         public UserData UserData { get; set; }
 
         public string GameServerUrl { get; set; }
-        
+
         public int CharacterType { get; set; }
-        
+
         public string CharacterName { get; set; }
 
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            UserData = new UserData()
+            {
+                id = GetUserId()
+            };
+        }
+
+        private string GetUserId()
+        {
+            string userid;
+            const string key = "userid";
+
+            if (PlayerPrefs.HasKey(key))
+            {
+                userid = PlayerPrefs.GetString(key);
+            }
+            else
+            {
+                userid = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                userid = Regex.Replace(userid, "[/+=]", string.Empty);
+
+                PlayerPrefs.SetString(key, userid);
+                PlayerPrefs.Save();
+            }
+
+            return userid;
         }
     }
 }
