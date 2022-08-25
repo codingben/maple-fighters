2fe87555fa323f55da7b76739936a1be6501b030
Ben Oukhanov <ben1zuk321@gmail.com>
2022-08-25 18:51:58 +0300

    refactor(frontend): remove unnecessary components

--- src/frontend/src/components/App/App.jsx
+++ src/frontend/src/components/App/App.jsx
@@ -1,17 +1,28 @@
 import Unity, { UnityContext } from "react-unity-webgl";
 import { isMobile } from "react-device-detect";
-import Mobile from "../Mobile/Mobile.jsx";
-import Header from "../Header/Header.jsx";
-import Footer from "../Footer/Footer.jsx";
-import Loader from "../Loader/Loader.jsx";
+import FadeLoader from "react-spinners/FadeLoader";
 import "./app.css";
+import "./header.css";
+import "./footer.css";
+import "./loader.css";
+import "./mobile.css";
+import logo from "../../assets/logo.png";
+import { useState } from "react";
 
-export const context = new UnityContext({
-  loaderUrl: "files/WebGL.loader.js",
-  dataUrl: "files/WebGL.data",
-  frameworkUrl: "files/WebGL.framework.js",
-  codeUrl: "files/WebGL.wasm",
+const context = new UnityContext({
+  loaderUrl: "files/example-app.loader.js",
+  dataUrl: "files/example-app.data",
+  frameworkUrl: "files/example-app.framework.js",
+  codeUrl: "files/example-app.wasm",
 });
+
+function EnterFullScreen() {
+  context.setFullscreen(true);
+}
+
+function Login() {
+  context.send("Authenticator Controller", "ShowLoginWindow");
+}
 
 function OnSetEnv() {
   // Timeout is required because of the splash screen,
@@ -24,19 +35,72 @@ function OnSetEnv() {
   }, 2000);
 }
 
+function Mobile() {
+  return (
+    <div className="warning">
+      <img src={logo} alt="logo" />
+      <h1>
+        <a href="https://github.com/codingben/maple-fighters">Maple Fighters</a>{" "}
+        is not compatible with your device.
+      </h1>
+    </div>
+  );
+}
+
 function App() {
   if (isMobile) {
     return <Mobile />;
   }
 
+  let [loading, setLoading] = useState(true);
+
+  context.on("loaded", () => {
+    setLoading(false);
+  });
+
   context.on("SetEnv", OnSetEnv);
 
   return (
     <div>
-      <Header />
-      <Footer />
+      <div>
+        <img src={logo} className="logo" alt="logo" />
+        <div className="title">
+          <h2>Maple Fighters</h2>
+        </div>
+        <div>
+          {loading == false && (
+            <div>
+              <button onClick={EnterFullScreen} className="fullscreen-button">
+                Enter Full Screen
+              </button>
+              <button onClick={Login} className="login-button">
+                Login
+              </button>
+            </div>
+          )}
+          <a href="https://github.com/codingben/maple-fighters" target="_blank">
+            <button className="github-button">GitHub</button>
+          </a>
+        </div>
+      </div>
       <Unity className="container" unityContext={context} />
-      <Loader />
+      <div className="footer">
+        <h4>
+          Made with <span style={{ color: "#E91E63" }}>&#x2764;</span> by{" "}
+          <a href="https://codingben.io" target="_blank">
+            Ben Oukhanov
+          </a>
+        </h4>
+      </div>
+      <div className="loader">
+        <FadeLoader
+          css={"display: block;"}
+          size={50}
+          color={"white"}
+          loading={loading}
+          speedMultiplier={1.5}
+        />
+      </div>
     </div>
   );
 }
