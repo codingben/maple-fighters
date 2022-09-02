f7ad955d16639a0c0145ad27c5d84bebd2305c6a
Ben Oukhanov <ben1zuk321@gmail.com>
2022-09-02 17:58:35 +0300

    feat(game-service): implement mob attacked behaviour

--- src/game-service/Game.Application/Objects/Mob/Components/MobAttackedBehaviour.cs
+++ src/game-service/Game.Application/Objects/Mob/Components/MobAttackedBehaviour.cs
@@ -1,17 +1,32 @@
+using System.Timers;
 using Game.Application.Components;
 
 namespace Game.Application.Objects.Components
 {
     public class MobAttackedBehaviour : ComponentBase, IMobAttackedBehaviour
     {
+        private readonly Timer attackedTimer;
+
+        public MobAttackedBehaviour()
+        {
+            attackedTimer = new Timer(2500);
+            attackedTimer.Elapsed += (s, e) => OnAttackedTimeOver();
+        }
+
         public void Start()
         {
-            // TODO: Implement
+            attackedTimer.Start();
         }
 
         public void Stop()
         {
-            // TODO: Implement
+            attackedTimer.Stop();
+        }
+
+        private void OnAttackedTimeOver()
+        {
+            var mobBehaviourManager = Components.Get<IMobBehaviourManager>();
+            mobBehaviourManager.ChangeBehaviour(type: MobBehaviourType.Move);
         }
     }
 }
