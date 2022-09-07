a95629e2712ea0c41ec0e1e16ddeb69042619ea3
Ben Oukhanov <ben1zuk321@gmail.com>
2022-09-07 22:57:42 +0300

    fix(game-service): reorder config data provider

--- src/game-service/Game.Application/Objects/Mob/MobGameObject.cs
+++ src/game-service/Game.Application/Objects/Mob/MobGameObject.cs
@@ -10,11 +10,11 @@ namespace Game.Application.Objects
             Components.Add(new GameObjectGetter(this));
             Components.Add(new ProximityChecker());
             Components.Add(new PresenceSceneProvider());
+            Components.Add(new MobConfigDataProvider());
             Components.Add(new MobMoveBehaviour());
             Components.Add(new MobAttackedBehaviour());
             Components.Add(new MobBehaviourManager());
             Components.Add(new MobAttackPlayerHandler(id));
-            Components.Add(new MobConfigDataProvider());
             Components.Add(new MobPhysicsBodyCreator());
             Components.Add(new PhysicsBodyPositionSetter());
         }
