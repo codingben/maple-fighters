dea0fe93021df2ffcaf7474b89e14cdb0034a8dd
Ben Oukhanov <ben1zuk321@gmail.com>
2022-09-18 14:23:25 +0300

    feat(game-service): respawn game scene objects

--- src/game-service/Game.Application/Components/Scene/GameSceneManager.cs
+++ src/game-service/Game.Application/Components/Scene/GameSceneManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Net;
+using System.Timers;
 using Game.Application.Objects;
 using InterestManagement;
 using YamlDotNet.Serialization;
@@ -9,22 +10,36 @@ namespace Game.Application.Components
 {
     public class GameSceneManager : ComponentBase
     {
-        private IIdGenerator idGenerator;
+        private const float MOB_RESPAWN_TIME = 20000;
+
+        private readonly Timer timer;
         private IGameSceneCollection gameSceneCollection;
+
+        public GameSceneManager()
+        {
+            timer = new Timer
+            {
+                Interval = MOB_RESPAWN_TIME,
+                AutoReset = true,
+                Enabled = true
+            };
+        }
 
         protected override void OnAwake()
         {
-            idGenerator = Components.Get<IIdGenerator>();
             gameSceneCollection = Components.Get<IGameSceneCollection>();
 
             var yamlConfig = LoadYamlConfig();
             var configData = ParseConfigData(yamlConfig);
 
             CreateGameScene(configData);
+
+            timer.Elapsed += (_, _) => RespawnGameObjects(configData.Scenes);
         }
 
         protected override void OnRemoved()
         {
+            timer.Dispose();
             gameSceneCollection.Dispose();
         }
 
@@ -70,26 +85,64 @@ namespace Game.Application.Components
             }
         }
 
+        private void AddGameObject(IGameScene gameScene, ObjectData objectData)
+        {
+            var gameObject = CreateGameObject(objectData);
+            var presenceSceneProvider = gameObject.Components.Get<IPresenceSceneProvider>();
+            presenceSceneProvider.SetScene(gameScene);
+
+            var sceneObjectCollection = gameScene.Components.Get<ISceneObjectCollection>();
+            sceneObjectCollection.Add(gameObject);
+        }
+
+        private void RespawnGameObjects(Dictionary<string, SceneData> scenes)
+        {
+            foreach (var sceneName in scenes.Keys)
+            {
+                if (gameSceneCollection.TryGet(sceneName, out var gameScene))
+                {
+                    var objects = scenes[sceneName].Objects;
+
+                    foreach (var objectData in objects)
+                    {
+                        var gameObjectCollection =
+                            gameScene.Components.Get<ISceneObjectCollection>();
+                        if (gameObjectCollection.Exists(objectData.Id))
+                        {
+                            continue;
+                        }
+
+                        AddGameObject(gameScene, objectData);
+                    }
+                }
+            }
+        }
+
         private IEnumerable<IGameObject> CreateGameObjects(ObjectData[] objects)
         {
             foreach (var objectData in objects)
             {
-                var name = objectData.Name;
-                var type = (ObjectTypes)objectData.Type;
-                var position = new Vector2(objectData.Position.X, objectData.Position.Y);
-                var size = new Vector2(objectData.Size.X, objectData.Size.Y);
-                var customData = objectData.CustomData;
-                var id = idGenerator.GenerateId();
-                var gameObject = CreateGameObject(type, id, name, customData);
-
-                gameObject.Transform.SetPosition(position);
-                gameObject.Transform.SetSize(size);
-
-                yield return gameObject;
+                yield return CreateGameObject(objectData);
             }
         }
 
-        private IGameObject CreateGameObject(
+        private IGameObject CreateGameObject(ObjectData objectData)
+        {
+            var name = objectData.Name;
+            var id = objectData.Id;
+            var type = (ObjectTypes)objectData.Type;
+            var position = new Vector2(objectData.Position.X, objectData.Position.Y);
+            var size = new Vector2(objectData.Size.X, objectData.Size.Y);
+            var customData = objectData.CustomData;
+            var gameObject = GetGameObject(type, id, name, customData);
+
+            gameObject.Transform.SetPosition(position);
+            gameObject.Transform.SetSize(size);
+
+            return gameObject;
+        }
+
+        private IGameObject GetGameObject(
           ObjectTypes type,
           int id,
           string name,
