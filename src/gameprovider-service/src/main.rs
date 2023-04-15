88c78c1a8a9afb8191ac28d4404cdc633444a30e
Ben Oukhanov <ben1zuk321@gmail.com>
2023-04-15 23:49:24 +0300

    refactor(gameprovider-service): read game services config

--- src/gameprovider-service/src/main.rs
+++ src/gameprovider-service/src/main.rs
@@ -1,32 +1,51 @@
-use actix_web::{middleware::Logger, web, App, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, web::Data, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
-use std::{env, io::Result};
+use serde::{Serialize, Deserialize};
+use std::{env, io};
 
-struct AppData {
-    game_services: String,
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Config {
+    game_services: Vec<GameService>,
 }
 
-async fn get_game_services(data: web::Data<AppData>) -> impl Responder {
-    data.get_ref().game_services.clone()
+#[derive(Serialize, Deserialize)]
+struct GameService {
+    name: String,
+    protocol: String,
+    url: String,
+}
+
+async fn read_config_from_remote() -> Result<Config, reqwest::Error> {
+    let config_url = format!(
+        "https://raw.githubusercontent.com/codingben/maple-fighters-configs/{}/game-services.yml",
+        env::var("CONFIG_SOURCE").unwrap()
+    );
+    let config_text = reqwest::get(config_url).await?.text().await?;
+    let config = serde_yaml::from_str(&config_text).unwrap();
+
+    Ok(config)
+}
+
+#[get("/games")]
+async fn get_game_services(config: Data<Config>) -> HttpResponse {
+    HttpResponse::Ok()
+        .json(config.as_ref())
 }
 
 #[actix_web::main]
-async fn main() -> Result<()> {
+async fn main() -> io::Result<()> {
     dotenv().ok();
-
     env_logger::init();
 
     let ip_address = env::var("IP_ADDRESS").expect("IP_ADDRESS not found");
-    let game_services = env::var("GAME_SERVICES").expect("GAME_SERVICES not found");
-    let data = web::Data::new(AppData {
-        game_services: game_services,
-    });
+    let config = Data::new(read_config_from_remote().await.unwrap());
 
     HttpServer::new(move || {
         App::new()
-            .app_data(data.clone())
+            .app_data(config.clone())
             .wrap(Logger::default())
-            .route("/games", web::get().to(get_game_services))
+            .service(get_game_services)
     })
     .bind(ip_address)?
     .run()
