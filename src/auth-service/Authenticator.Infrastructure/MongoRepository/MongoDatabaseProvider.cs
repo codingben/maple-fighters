af31304c550ab36c11e65758aee9e4b6a4a72cfd
Ben Oukhanov <ben1zuk321@gmail.com>
2023-05-06 20:23:36 +0300

    fix(auth-service): ping to establish db connection

--- src/auth-service/Authenticator.Infrastructure/MongoRepository/MongoDatabaseProvider.cs
+++ src/auth-service/Authenticator.Infrastructure/MongoRepository/MongoDatabaseProvider.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver;
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace Authenticator.Infrastructure.MongoRepository
 {
@@ -13,8 +15,25 @@ namespace Authenticator.Infrastructure.MongoRepository
         {
             var mongoUrl = new MongoUrl(url);
             var mongoClient = new MongoClient(mongoUrl);
-
             database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+
+            Ping(url);
+        }
+
+        /// <summary>
+        /// Force the connection to be established.
+        /// </summary>
+        private void Ping(string url)
+        {
+            try
+            {
+                var pingCommand = new BsonDocument("ping", 1);
+                database.RunCommand<BsonDocument>(pingCommand);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"Failed to connect to MongoDB server at {url}. {exception.Message}", exception);
+            }
         }
 
         public IMongoDatabase Provide()
